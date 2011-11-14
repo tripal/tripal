909eabab93c6d42bd66f3e33222f95903ac7cbb5
laceysanderson <laceyanne_sanderson@shaw.ca>
2011-11-14 17:03:37 +0000

    Tripal Pub: Added Add Authors to create/edit form & added create nodes via pubmed search -Not ready for production sites

---
+++ tripal_pub/theme/node-tripal_pub.tpl.php
@@ -0,0 +1,13 @@
+<?php
+// Developed by: Chad N.A Krilow at The University of Saskatchewan
+//
+// Purpose: This template provides the layout of the Tripal Pub node (page)
+//   using the same templates used for the various Tripal blocks. 
+?> 
+
+<?php
+ //uncomment this line to see a full listing of the fields avail. to $node
+ //print '<pre>'.print_r($node,TRUE).'</pre>';
+?>
+
+
