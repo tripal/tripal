460ccaa61e3b709db2cd3b1594c01d3c6aad9e85
spficklin <spficklin@gmail.com>
2012-01-23 23:48:27 -0500

    Changed view setup form to support AJAX and a table style format for editing fields

---
+++ base/tripal_views_setup/tripal_views_setup_fields_form.tpl.php
@@ -0,0 +1,47 @@
+<style type="text/css">
+
+#tripal-views-setup-fields-form  .fields-new-row, .field-headers {
+   dislay: block;
+   margin: 0px;
+   border-bottom-style: solid;
+   border-bottom-width: 1px;
+}
+#tripal-views-setup-fields-form .form-item {
+   margin: 0px 0px 5px 0px;
+}
+#tripal-views-setup-fields-form .column-id,.fields-column-join,.fields-column-join-column, .fields-column-handler {
+   display: inline-block;
+   margin: 0px;
+   vertical-align: top;
+}
+#tripal-views-setup-fields-form  .field-headers {
+   font-weight: bold;  
+}
+#tripal-views-setup-fields-form  .field-headers div {
+   display: inline-block;
+   margin: 0px;
+   vertical-align: top;
+}
+#tripal-views-setup-fields-form .column-name {
+
+}
+#tripal-views-setup-fields-form .column-type {
+   font-style: italic;  
+}
+#tripal-views-setup-fields-form .column-id {
+   width: 25%;
+}
+#tripal-views-setup-fields-form .fields-column-join {
+   width: 25%;
+}
+#tripal-views-setup-fields-form .fields-column-join-column {
+   width: 25%;
+}
+#tripal-views-setup-fields-form .fields-column-handler {
+   width: 25%;
+}
+
+</style>
+
+<?php print drupal_render($form); ?>
+
