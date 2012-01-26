d79daee0205431c7b045afb33e6c980b527dc2c8
spficklin <spficklin@gmail.com>
2012-01-26 10:07:47 -0500

    Renamed module from tripal_views_setup to tripal_views_integration. Completed coding for integration of the ahah_helper module. Added permissions. Unified the menu items to match that with the rest of Tripal.  Add usage documentation to the main administrative link

---
+++ base/tripal_views_integration/tripal_views_integration_fields_form.tpl.php
@@ -0,0 +1,47 @@
+<style type="text/css">
+
+#tripal-views-integration-new-setup-form  .fields-new-row, .field-headers {
+   dislay: block;
+   margin: 0px;
+   border-bottom-style: solid;
+   border-bottom-width: 1px;
+}
+#tripal-views-integration-new-setup-form .form-item {
+   margin: 0px 0px 5px 0px;
+}
+#tripal-views-integration-new-setup-form .column-id,.fields-column-join,.fields-column-join-column, .fields-column-handler {
+   display: inline-block;
+   margin: 0px;
+   vertical-align: top;
+}
+#tripal-views-integration-new-setup-form  .field-headers {
+   font-weight: bold;  
+}
+#tripal-views-integration-new-setup-form  .field-headers div {
+   display: inline-block;
+   margin: 0px;
+   vertical-align: top;
+}
+#tripal-views-integration-new-setup-form .column-name {
+
+}
+#tripal-views-integration-new-setup-form .column-type {
+   font-style: italic;  
+}
+#tripal-views-integration-new-setup-form .column-id {
+   width: 25%;
+}
+#tripal-views-integration-new-setup-form .fields-column-join {
+   width: 25%;
+}
+#tripal-views-integration-new-setup-form .fields-column-join-column {
+   width: 25%;
+}
+#tripal-views-integration-new-setup-form .fields-column-handler {
+   width: 25%;
+}
+
+</style>
+
+<?php print drupal_render($form); ?>
+
