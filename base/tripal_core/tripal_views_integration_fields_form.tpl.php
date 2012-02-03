ccf56f0789aa6ff48e04ba626dc23175ca955ccd
spficklin <spficklin@gmail.com>
2012-02-02 22:05:53 -0500

    Forgot to add new views integration files in the core module

---
+++ base/tripal_core/tripal_views_integration_fields_form.tpl.php
@@ -0,0 +1,50 @@
+<style type="text/css">
+
+#tripal-core-views-integration-new-setup-form  .fields-new-row, .field-headers {
+   dislay: block;
+   margin: 0px;
+   border-bottom-style: solid;
+   border-bottom-width: 1px;
+}
+#tripal-core-views-integration-new-setup-form .form-item {
+   margin: 0px 0px 5px 0px;
+}
+#tripal-core-views-integration-new-setup-form .column-id, .fields-column-join,.fields-column-join-column, .fields-filter-handler, .fields-field-handler {
+   display: inline-block;
+   margin: 0px;
+   vertical-align: top;
+}
+#tripal-core-views-integration-new-setup-form  .field-headers {
+   font-weight: bold;
+}
+#tripal-core-views-integration-new-setup-form  .field-headers div {
+   display: inline-block;
+   margin: 0px;
+   vertical-align: top;
+}
+#tripal-core-views-integration-new-setup-form .column-name {
+
+}
+#tripal-core-views-integration-new-setup-form .column-type {
+   font-style: italic;
+}
+#tripal-core-views-integration-new-setup-form .column-id {
+   width: 15%;
+}
+#tripal-core-views-integration-new-setup-form .fields-column-join {
+   width: 30%;
+}
+#tripal-core-views-integration-new-setup-form .fields-column-join-column {
+   width: 30%;
+}
+#tripal-core-views-integration-new-setup-form .fields-filter-handler {
+   width: 10%;
+}
+#tripal-core-views-integration-new-setup-form .fields-field-handler {
+   width: 10%;
+}
+
+</style>
+
+<?php print drupal_render($form); ?>
+
