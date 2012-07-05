942a59cd6cdc09464597c16f063ee6b3ff43622f
Lacey Sanderson <laceyanne_sanderson@shaw.ca>
2012-07-05 16:31:02 -0500

    Small changes to views integration themeing

--- tripal_views/tripal_views_integration_fields_form.tpl.php
+++ tripal_views/tripal_views_integration_fields_form.tpl.php
@@ -13,34 +13,46 @@
    border-bottom-style: solid;
    border-bottom-width: 1px;
 }
+
 #tripal-views-integration-form .form-item {
    margin: 0px 0px 5px 0px;
 }
+
 #tripal-views-integration-form .column-one, .column-two, .column-three, .column-four {
    display: inline-block;
    margin: 0px;
    vertical-align: top;
    margin-left: 15px;
-   //border: 1px solid #000;
+   /**border: 1px solid #000;*/
 }
+
 #tripal-views-integration-form  .field-headers {
    font-weight: bold;
 }
+
 #tripal-views-integration-form  .field-headers div {
    display: inline-block;
    margin: 0px;
    vertical-align: top;
 }
+
 #tripal-views-integration-form .column-name {
    font-weight: bold;
 }
+
 #tripal-views-integration-form .column-type {
    font-style: italic;
 }
+
 #tripal-views-integration-form .column-one {
-   width: 20%;
+   width: 10%;
    height: 50px;
 }
+
+#tripal-views-integration-form .column-two, .column-three, .column-four {
+   width: 25%;
+}
+
 #tripal-views-integration-form  .fields-new-row {
    padding-bottom: 10px;
    margin-bottom: 5px;
