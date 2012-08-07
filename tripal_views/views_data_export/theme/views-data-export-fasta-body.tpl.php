83bb80cb485e513a97844e9ec9c58db83ca85fe7
spficklin <spficklin@gmail.com>
2012-08-06 22:50:26 -0400

    Completion of sequence handler

--- tripal_views/views_data_export/theme/views-data-export-fasta-body.tpl.php
+++ tripal_views/views_data_export/theme/views-data-export-fasta-body.tpl.php
@@ -4,33 +4,46 @@
  * @file
  * Renders the body portion of a FASTA views data export
  */
+$defline_tpl = $variables['options']['display']['defline_fields'];
+$num_bases_per_line = $variables['options']['display']['num_bases_per_line'];
+$use_residues = $variables['options']['display']['use_residues'];
+$residues_colname = $variables['options']['display']['residues_colname'];
 
-//print_r($themed_rows);
-
-// print the first FASTA record header
-// this is needed due to the order of the fields
-print $defline;
+if(!$num_bases_per_line){
+   $num_bases_per_line = 50;
+}
 
 // foreach row in the views table
 foreach ($themed_rows as $index => $fields) {
   $defline = array();
   $residues = '';
-  foreach ($fields as $key => $value) {
-
-    // wrap the sequence
-    if (strcmp($key, 'residues') == 0) {
-      $residues = wordwrap($value, 60, "\r\n", TRUE);
-    }
-
-    // set the FASTA header
-    if (strcmp($key, 'defline') == 0) {
-      $defline = $value;
-    }
+  
+  // if we're using the residues as is then we assume the residues are already
+  // formatted in FASTA format.   We just need to print
+  if ($use_residues) {
+     print "$fields[$residues_colname]\r\n";
   }
+  // if we're not using the residues as is then wrap the residues
+  // and generate a proper FASTA format
+  else {
+  
+    foreach ($fields as $key => $value) {
 
-  // print the FASTA record
-  print ">$defline\r\n";
-  print "$residues\r\n";
+      // if the setup indicates, wrap the sequence 
+      if (strcmp($key, $residues_colname) == 0) {
+        $residues = wordwrap($value, $num_bases_per_line, "\r\n", TRUE);
+      }
+
+      // set the FASTA header
+      if (strcmp($key, 'defline') == 0) {
+        $defline = $value;
+      }
+    }
+
+    // print the FASTA record
+    print ">$defline\r\n";
+    print "$residues\r\n";
+  }
 }
 
 
