78b078bbfc8b86e0c97bebb34c3e7238e99eb6f2
spficklin <spficklin@gmail.com>
2012-09-17 21:03:28 -0400

    Fixed bug in mviews

--- tripal_core/includes/mviews.php
+++ tripal_core/includes/mviews.php
@@ -207,8 +207,8 @@ function tripal_edit_mview($mview_id, $name, $modulename, $mv_table, $mv_specs,
 
     // re-create the table differently depending on if it the traditional method
     // or the Drupal Schema API method
-    if ($mv_schema) {
-      if (!tripal_create_chado_table ($ret, $mv_table, $schema_arr)) {
+    if ($mv_schema) {    	
+      if (!tripal_core_create_custom_table($ret, $mv_table, $schema_arr)) {
         drupal_set_message(t("Could not create the materialized view. Check Drupal error report logs."));
       }
       else {
