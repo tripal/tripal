47f94bbed472cd0d68738a74f61c7562f5b5ba43
Lacey Sanderson <laceysanderson@781094.no-reply.drupal.org>
2013-12-11 17:28:43 -0600

    Tripal Views: Removed Views Data Export. Now in it's own extension module (tripal_data_export)

--- tripal_views/views_data_export/theme/views-data-export-fasta-footer.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-/**
- * @file
- * Renders the footer of a FASTA views data export
- * FASTA files don't have a footer.
- */
--- tripal_views/views_data_export/theme/views-data-export-fasta-header.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-/**
- * @file
- * Renders the header of a FASTA views data export
- * FASTA files don't have a header.
- */
