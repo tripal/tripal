7e6e3a3f2141649b39c29b486aace1a55fc819bb
Stephen Ficklin <stephen.ficklin@allelesystems.com>
2014-01-15 15:32:42 -0500

    Contact module now syncs/insert/updates. Templates converted except relationships/publications haven't been tested. Added new Properties API to tripal_core to allow for easy addition of properties to a node form

--- tripal_contact/theme/tripal_contact/tripal_contact_base.tpl.php
+++ tripal_contact/theme/tripal_contact/tripal_contact_base.tpl.php
@@ -1,24 +1,60 @@
 <?php
-$node = $variables['node'];
-$contact = $variables['node']->contact;
+$contact = $variables['node']->contact; ?>
 
-?>
 <div id="tripal_contact-base-box" class="tripal_contact-info-box tripal-info-box">
   <div class="tripal_contact-info-box-title tripal-info-box-title">Details</div>
-  <div class="tripal_contact-info-box-desc tripal-info-box-desc"></div>   
+  <div class="tripal_contact-info-box-desc tripal-info-box-desc"></div> <?php
 
-  <table id="tripal_contact-table-base" class="tripal_contact-table tripal-table tripal-table-vert">
-    <tr class="tripal_contact-table-even-row tripal-table-even-row">
-      <th>Name</th>
-      <td><?php print $contact->name; ?></td>
-    </tr>
-    <tr class="tripal_contact-table-odd-row tripal-table-odd-row">
-      <th>Contact Type</th>
-      <td><?php print $contact->type_id->name; ?></td>
-    </tr>
-    <tr class="tripal_contact-table-even-row tripal-table-even-row">
-      <th>Description</th>
-      <td><?php print $contact->description; ?></td>
-    </tr>
-  </table> 
+  // the $headers array is an array of fields to use as the colum headers. 
+  // additional documentation can be found here 
+  // https://api.drupal.org/api/drupal/includes%21theme.inc/function/theme_table/7
+  // This table for the contact has a vertical header (down the first column)
+  // so we do not provide headers here, but specify them in the $rows array below.
+  $headers = array();
+  
+  // the $rows array contains an array of rows where each row is an array
+  // of values for each column of the table in that row.  Additional documentation
+  // can be found here:
+  // https://api.drupal.org/api/drupal/includes%21theme.inc/function/theme_table/7 
+  $rows = array();
+
+  // Contact Name row
+  $rows[] = array(
+    array(
+      'data' => 'Name',
+      'header' => TRUE
+    ),
+    $contact->name,
+  );
+  // Contact Type row
+  $rows[] = array(
+    array(
+      'data' => 'Type',
+      'header' => TRUE
+    ),
+    $contact->type_id->name,
+  );
+  
+  // the $table array contains the headers and rows array as well as other
+  // options for controlling the display of the table.  Additional
+  // documentation can be found here:
+  // https://api.drupal.org/api/drupal/includes%21theme.inc/function/theme_table/7
+  $table = array(
+    'header' => $headers,
+    'rows' => $rows,
+    'attributes' => array(
+      'id' => 'tripal_contact-table-base',
+    ),
+    'sticky' => FALSE,
+    'caption' => '',
+    'colgroups' => array(),
+    'empty' => '',
+  );
+  
+  // once we have our table array structure defined, we call Drupal's theme_table()
+  // function to generate the table.
+  print theme_table($table);
+  if (property_exists($contact, 'description')) {
+    print $contact->description;
+  } ?>
 </div>
--- tripal_contact/theme/tripal_contact/tripal_contact_properties.tpl.php
+++ tripal_contact/theme/tripal_contact/tripal_contact_properties.tpl.php
@@ -1,43 +1,63 @@
 <?php
-$contact = $node->contact;
 
 // expand the contact to include the properties.
+$contact = $variables['node']->contact;
 $contact = tripal_core_expand_chado_vars($contact,'table', 'contactprop', array('return_array' => 1));
 $contactprops = $contact->contactprop;
+
+// put the properties in an array for easier access
 $properties = array();
-if (is_array($contactprops)) {
-  foreach ($contactprops as $property) {
-    // we want to keep all properties but the contact_description as that
-    // property is shown on the base template page.
-    if($property->type_id->name != 'contact_description') {
-      $property = tripal_core_expand_chado_vars($property,'field','contactprop.value');
-      $properties[] = $property;
-    }
+foreach ($contactprops as $property) {
+  // we want to keep all properties but the contact_description as that
+  // property is shown on the base template page.
+  if($property->type_id->name != 'contact_description') {
+    $property = tripal_core_expand_chado_vars($property,'field','contactprop.value');
+    $properties[] = $property;
   }
 }
+
 
 if (count($properties) > 0) { ?>
   <div id="tripal_contact-properties-box" class="tripal_contact-info-box tripal-info-box">
     <div class="tripal_contact-info-box-title tripal-info-box-title">More Details</div>
-    <div class="tripal_contact-info-box-desc tripal-info-box-desc">Additional information about this contact:</div>
-    <table class="tripal_contact-table tripal-table tripal-table-horz">
-      <tr>
-        <th>Property Name</th>
-        <th>Value</th>
-      </tr> <?php
-      $i = 0;
-      foreach ($properties as $property) {
-        $class = 'tripal_contact-table-odd-row tripal-table-odd-row';
-        if ($i % 2 == 0 ) {
-           $class = 'tripal_contact-table-odd-row tripal-table-even-row';
-        }
-        $i++; 
-        ?>
-        <tr class="<?php print $class ?>">
-          <td><?php print ucfirst(preg_replace('/_/', ' ', $property->type_id->name)) ?></td>
-          <td><?php print $property->value ?></td>
-        </tr><?php 
-      } ?>
-    </table>
+    <div class="tripal_contact-info-box-desc tripal-info-box-desc">Additional information about this contact:</div><?php
+    // the $headers array is an array of fields to use as the colum headers.
+    // additional documentation can be found here
+    // https://api.drupal.org/api/drupal/includes%21theme.inc/function/theme_table/7
+    $headers = array('Property Name', 'Value');
+    
+    // the $rows array contains an array of rows where each row is an array
+    // of values for each column of the table in that row.  Additional documentation
+    // can be found here:
+    // https://api.drupal.org/api/drupal/includes%21theme.inc/function/theme_table/7
+    $rows = array();
+    
+    // add the properties as individual rows
+    foreach ($properties as $property) {
+      $rows[] = array(
+        ucfirst(preg_replace('/_/', ' ', $property->type_id->name)),
+        $property->value
+      );
+    } 
+    
+    // the $table array contains the headers and rows array as well as other
+    // options for controlling the display of the table.  Additional
+    // documentation can be found here:
+    // https://api.drupal.org/api/drupal/includes%21theme.inc/function/theme_table/7
+    $table = array(
+      'header' => $headers,
+      'rows' => $rows,
+      'attributes' => array(
+        'id' => 'tripal_contact-table-properties',
+      ),
+      'sticky' => FALSE,
+      'caption' => '',
+      'colgroups' => array(),
+      'empty' => '',
+    );
+    
+    // once we have our table array structure defined, we call Drupal's theme_table()
+    // function to generate the table.
+    print theme_table($table); ?>
   </div> <?php
 }
