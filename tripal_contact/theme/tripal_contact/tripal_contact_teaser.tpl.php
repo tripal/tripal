3066604b44074f7514860bc6719179cbcb4cfc5e
Stephen Ficklin <stephen.ficklin@allelesystems.com>
2014-01-15 15:35:57 -0500

    Forgot to commit contact teaser template

---
+++ tripal_contact/theme/tripal_contact/tripal_contact_teaser.tpl.php
@@ -0,0 +1,15 @@
+<?php
+$node = $variables['node'];
+$contact = $variables['node']->contact; ?>
+
+<div class="tripal_contact-teaser tripal-teaser"> 
+  <div class="tripal-contact-teaser-title tripal-teaser-title"><?php 
+    print l($node->title, "node/$node->nid", array('html' => TRUE));?>
+  </div>
+  <div class="tripal-contact-teaser-text tripal-teaser-text"><?php 
+    print substr($contact->description, 0, 650);
+    if (strlen($contact->description) > 650) {
+      print "... " . l("[more]", "node/$node->nid");
+    } ?>
+  </div>
+</div>
