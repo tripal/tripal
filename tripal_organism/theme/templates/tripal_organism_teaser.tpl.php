6eeaed80fe1f8301e7e2feacd468f1ecd4dde90f
Stephen Ficklin <spficklin@gmail.com>
2014-08-21 13:15:39 -0400

    Fixed FASTA loader to not overrun the memory. Also fixed a few other minor issues

--- tripal_organism/theme/templates/tripal_organism_teaser.tpl.php
+++ tripal_organism/theme/templates/tripal_organism_teaser.tpl.php
@@ -1,13 +1,16 @@
 <?php
 $organism  = $variables['node']->organism;
-$image_url = tripal_organism_get_image_url($organism, $node->nid); ?>
+$image_url  = tripal_get_organism_image($organism, $node->nid); 
+$image_path = tripal_get_organism_image($organism, $node->nid, 'path');?>
 
 <div class="tripal_organism-teaser tripal-teaser"> 
   <div class="tripal-organism-teaser-title tripal-teaser-title"><?php 
     print l("<i>$organism->genus $organism->species</i> ($organism->common_name)", "node/$node->nid", array('html' => TRUE));?>
   </div>
-  <div class="tripal-organism-teaser-text tripal-teaser-text">
-    <img class="tripal-teaser-img" src="<?php print $image_url ?>" ><?php
+  <div class="tripal-organism-teaser-text tripal-teaser-text"><?php 
+    if (file_exists($image_path)) { ?>
+      <img class="tripal-teaser-img" src="<?php print $image_url ?>" ><?php
+    } 
     print substr($organism->comment, 0, 650);
     if (strlen($organism->comment) > 650) {
       print "... " . l("[more]", "node/$node->nid");
