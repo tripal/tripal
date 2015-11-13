0a834b5eadaa249d31aaaff3067a3ac542503a69
Stephen Ficklin <spficklin@gmail.com>
2015-11-12 17:10:31 -0800

    Added separate CSS file

--- tripal_fields_layout/theme/templates/tripal_fields_layout_generic.tpl.php
+++ tripal_fields_layout/theme/templates/tripal_fields_layout_generic.tpl.php
@@ -5,10 +5,12 @@ $fields = $variables['element']['#fields'];
 
 // Group fields into panels
 foreach ($fields AS $panel_id => $field) { ?>
-  <div style="border:solid 1px #CCC;margin:15px 0px;padding:15px;"><?php
-    print '<h1>' . $panels[$panel_id] . '</h1>';
-    print render($field);
-  ?>
-  </div><?php
+  <div id="" class="tripal-biodata-panel"> <?php
+    if ($panels[$panel_id] != 'Base Content') {
+      print '<h2>' . $panels[$panel_id] . '</h2>';
+    }
+    print render($field); ?>
+  </div> <?php
 }
 
+print render($content);
