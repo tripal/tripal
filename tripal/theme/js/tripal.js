f1deb072415daaba9f4105123f60ddd3e018c507
Stephen Ficklin <spficklin@gmail.com>
2017-01-14 17:48:45 -0800

    Added field for the publications to display records they link to.  Added button to check for new fields for a bundle.  Fixed ajax loading so titles don't appear

--- tripal/theme/js/tripal.js
+++ tripal/theme/js/tripal.js
@@ -4,7 +4,7 @@
   Drupal.behaviors.myModuleSecureLink = {
     attach: function (context, settings) {
 
-      $(".tripal-entity-unattached .field-items").replaceWith('Loading... <img src="' + tripal_path + '/theme/images/ajax-loader.gif">');
+      $(".tripal-entity-unattached .field-items").replaceWith('<div class="field-items">Loading... <img src="' + tripal_path + '/theme/images/ajax-loader.gif"></div>');
       $(".tripal-entity-unattached").each(function() {
         id = $(this).attr('id');
         $.ajax({
@@ -14,18 +14,9 @@
           success: function(data){
             var content = data['content'];
             var id = data['id'];
-            $("#" + id).replaceWith(content);
+            $("#" + id + ' .field-items').replaceWith(content);
           }
         });
-      });
-      
-      $(".tripal-tooltip").each(function() {
-        var field = $(this).next('.field');
-        $(this).detach();
-
-        if (field) {
-          field.children('.field-label').append($(this));
-        }
       });
     }
   }
