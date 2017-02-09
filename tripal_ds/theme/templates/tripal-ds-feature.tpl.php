f5429b58f654cd09bd841a19d06e4eefb1245777
Shawna Spoor <shawna.spoor@fick-web.cahnrs.wsu.edu>
2017-02-09 13:30:03 -0800

    final changes before merge

--- tripal_ds/theme/templates/tripal-ds-feature.tpl.php
+++ tripal_ds/theme/templates/tripal-ds-feature.tpl.php
@@ -10,9 +10,6 @@
    <?php if (isset($title_suffix['contextual_links'])): ?>
    <?php print render($title_suffix['contextual_links']); ?>
    <?php endif; ?>
-   <<?php print $top_wrapper ?> class="ds-top<?php print $top_classes; ?>">
-     <?php print $top; ?>
-   </<?php print $top_wrapper ?>>
 
    <<?php print $left_wrapper ?> class="ds-left<?php print $left_classes; ?>">
      <?php print $left; ?>
@@ -22,9 +19,5 @@
     <div class='group-tripal-pane-content-top'></div>
      <?php print $right; ?>
    </<?php print $right_wrapper ?>>
-
-   <<?php print $bottom_wrapper ?> class="ds-bottom<?php print $bottom_wrapper; ?>">
-     <?php print $bottom; ?>
-   </<?php print $bottom_wrapper ?>>
 
  </<?php print $layout_wrapper ?>>
