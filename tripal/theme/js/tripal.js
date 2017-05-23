e11517cb3bae2e4a9ce8fd88181144e9cfeebe0f
Stephen Ficklin <spficklin@gmail.com>
2017-05-23 12:11:17 -0700

    Fixed issues with chaching.  Fields now only load via ajax when fields are not hidden

--- tripal/theme/js/tripal.js
+++ tripal/theme/js/tripal.js
@@ -7,16 +7,18 @@
       $(".tripal-entity-unattached .field-items").replaceWith('<div class="field-items">Loading... <img src="' + tripal_path + '/theme/images/ajax-loader.gif"></div>');
       $(".tripal-entity-unattached").each(function() {
         id = $(this).attr('id');
-        $.ajax({
-          url: baseurl + '/bio_data/ajax/field_attach/' + id,
-          dataType: 'json',
-          type: 'GET',
-          success: function(data){
-            var content = data['content'];
-            var id = data['id'];
-            $("#" + id + ' .field-items').replaceWith(content);
-          }
-        });
+        if (id) {
+          $.ajax({
+            url: baseurl + '/bio_data/ajax/field_attach/' + id,
+            dataType: 'json',
+            type: 'GET',
+            success: function(data){
+              var content = data['content'];
+              var id = data['id'];
+              $("#" + id + ' .field-items').replaceWith(content);
+            }
+          });
+        }
       });
     }
   }
