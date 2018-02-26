b358db30938a6fad731a630ea284c85ba63df9d9
Abdullah Almsaeed <almasaeed2010@gmail.com>
2018-02-26 11:37:33 -0500

    Fix bootstrap path error

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,15 +1,24 @@
 <?php
+// Set environment variables
 test_suite_read_and_set_environment_variables();
+
+// Get Drupal root path
 $drupal_root = getenv('DRUPAL_ROOT');
-define('DRUPAL_ROOT', $drupal_root ?: __DIR__.'/../../../../..');
+define('DRUPAL_ROOT', $drupal_root ?: '/var/www/html');
+
+// Get Drupal bootstrap functions
 require_once DRUPAL_ROOT.'/includes/bootstrap.inc';
 $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
+
+// Bootstrap Drupal.
 $current_dir = getcwd();
 chdir(DRUPAL_ROOT);
-// Bootstrap Drupal.
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 chdir($current_dir);
+
 /**
+ * Get and set environment variables from .env file if it exists.
+ *
  * @throws \Exception
  */
 function test_suite_read_and_set_environment_variables() {
