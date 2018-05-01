cfeb0f61e23e7b4aa131a8183cff3c01e60f91aa
Bradford Condon <bradford.condon@gmail.com>
2018-05-01 14:11:43 -0400

    reorder tests

---
+++ tests/tripal/api/TripalJobsApiTest.php
@@ -0,0 +1,21 @@
+<?php
+
+
+use StatonLab\TripalTestSuite\DBTransaction;
+use StatonLab\TripalTestSuite\TripalTestCase;
+
+class TripalJobsApiTest extends TripalTestCase {
+
+  use DBTransaction;
+
+  /**
+   * Tests the ability to create a tripal job.
+   *
+   * @test
+   */
+  public function should_create_a_tripal_job() {
+    $job_id = tripal_add_job('Test adding jobs', 'tripal', 'tripal_tripal_cron_notification', [], 1);
+    $this->assertTrue(is_numeric($job_id));
+
+  }
+}
