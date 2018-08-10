17a59bbafba2b52c8d6ad2cfb83790ce32011eab
Abdullah Almsaeed <almasaeed2010@gmail.com>
2018-08-10 16:28:20 -0400

    Bump testsuite version, use silent method and ad more tests for jobs

--- tests/tripal_chado/api/TripalChadoAPITest.php
+++ tests/tripal_chado/api/TripalChadoAPITest.php
@@ -1,11 +1,11 @@
 <?php
-namespace Tests\tripal_chado\api;
 
+namespace Tests\tripal_chado\api;
 
 use StatonLab\TripalTestSuite\DBTransaction;
 use StatonLab\TripalTestSuite\TripalTestCase;
 
-class TripalChadoAPITest extends TripalTestCase {
+class TripalChadoAPITest extends TripalTestCase{
 
   use DBTransaction;
 
@@ -17,32 +17,35 @@ class TripalChadoAPITest extends TripalTestCase {
    */
   public function test_tripal_chado_publish_records() {
     $genus_string = 'a_genius_genus';
-    //create an organism, publish it
+
+    // Create an organism, publish it
     $organism = factory('chado.organism')->create([
       'genus' => $genus_string,
       'species' => 'fake_species',
     ]);
-    //get bundle ID for organism
+
+    // Get bundle ID for organism
     $bundle = db_select('public.chado_bundle', 'CB')
       ->fields('CB', ['bundle_id'])
       ->condition('data_table', 'organism')
-      ->execute()->fetchField();
+      ->execute()
+      ->fetchField();
 
-    var_dump($bundle);
     $values = ['bundle_name' => 'bio_data_' . $bundle];
 
- //   ob_start();//dont display the job message
-    $bool = chado_publish_records($values);
-   // ob_end_clean();
+    // Don't display the job message
+    $bool = silent(function () use ($values) {
+      return chado_publish_records($values);
+    });
 
-    $this->assertTrue($bool, 'Publishing a fake organism record failed');
+    $this->assertTrue($bool->getReturnValue(), 'Publishing a fake organism record failed');
 
-    //ensure that our entity was created
-    $query = db_select('chado.organism', 'O')
-      ->fields('O', ['organism_id']);
+    // Ensure that our entity was created
+    $query = db_select('chado.organism', 'O')->fields('O', ['organism_id']);
     $query->join('public.chado_bio_data_' . $bundle, 'CBD', 'O.organism_id = CBD.record_id');
     $query->condition('O.genus', $genus_string);
     $organism_id = $query->execute()->fetchField();
+
     $this->assertNotNull($organism_id, 'Organism with record ID not found in chado_bio_data table.');
   }
 
@@ -52,10 +55,11 @@ class TripalChadoAPITest extends TripalTestCase {
    * @group api
    */
   public function test_tripal_chado_publish_records_false_with_bad_bundle() {
-    putenv("TRIPAL_SUPPRESS_ERRORS=TRUE");//this will fail, so we suppress the tripal error reporter
-    $bool = chado_publish_records(['bundle_name' => 'never_in_a_million_years']);
-    $this->assertFalse($bool);
-    putenv("TRIPAL_SUPPRESS_ERRORS");//unset
+    $bool = silent(function () {
+      return chado_publish_records(['bundle_name' => 'never_in_a_million_years']);
+    });
+
+    $this->assertFalse($bool->getReturnValue());
   }
 
   /**
