b5086ff1f2427ee9f5d0eec2f2b3d732279bb334
Lacey Sanderson <lacey.sanderson@usask.ca>
2018-09-13 22:09:51 -0600

    Change sidebar.

--- docs/conf.py
+++ docs/conf.py
@@ -86,12 +86,12 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_context = {
-    'css_files': [
-        '_static/theme_overrides.css',  # override wide tables in RTD theme
-        ],
-     }
-
+# Override the base theme.
+# We add the stylesheet this way so that it's loaded after the default.css
+# See https://docs.readthedocs.io/en/latest/guides/adding-custom-css.html
+def setup(app):
+    app.add_stylesheet('theme_overrides.css');  
+    
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
