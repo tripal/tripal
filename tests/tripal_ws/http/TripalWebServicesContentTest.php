d4a887e1e8f4d21d36c1106fe9f2cd89c17d5cd0
Reynold Tan <reynoldltan@gmail.com>
2019-11-14 15:00:24 -0600

    Existing test doesn't take user permissions into account -setting them to fix the test.

--- tests/tripal_ws/http/TripalWebServicesContentTest.php
+++ tests/tripal_ws/http/TripalWebServicesContentTest.php
@@ -38,10 +38,18 @@ class TripalWebServicesContentTest extends TripalTestCase {
    */
   public function testGettingListOfEntitiesInABundle() {
     // Get bundle label
-    $label = db_query('SELECT label FROM tripal_bundle LIMIT 1')->fetchField();
+    $label = db_query('SELECT label, name FROM tripal_bundle LIMIT 1')->fetchObject();
+
+    // Grant user permission to view bundle.
+    $bundle_name = 'view ' . $label->name;
+    if (!user_access($bundle_name)) {
+      // Either the user is anonymous or registered.
+      $role_id = (user_is_anonymous()) ? DRUPAL_ANONYMOUS_RID : DRUPAL_AUTHENTICATED_RID;
+      user_role_grant_permissions($role_id, array($bundle_name));
+    }
 
     // Call /web-services/content/v0.1/[label]
-    $response = $this->get("web-services/content/v0.1/$label");
+    $response = $this->get("web-services/content/v0.1/$label->label");
 
     // Verify the returned JSON matches the structure
     $response->assertSuccessful();
@@ -56,6 +64,6 @@ class TripalWebServicesContentTest extends TripalTestCase {
 
     // Verify the collection is of the correct type
     $json = $response->json();
-    $this->assertEquals($json['label'], "$label Collection");
+    $this->assertEquals($json['label'], "$label->label Collection");
   }
 }
