303a06cfaa9473b2e09b6f3e6b9c81aae3eff656
Lacey Sanderson <laceyannesanderson@gmail.com>
2020-12-06 21:29:33 -0600

    Start working on chadoStorage::preSaveEntity.

--- tripal_chado/src/Plugin/TripalStorage/ChadoStorage.php
+++ tripal_chado/src/Plugin/TripalStorage/ChadoStorage.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Drupal\tripal\Plugin\TripalStorage;
+namespace Drupal\tripal_chado\Plugin\TripalStorage;
 
 use Drupal\Core\Plugin\PluginBase;
 use Drupal\tripal\Plugin\TripalStorage\TripalStorageInterface;
@@ -33,8 +33,36 @@ class ChadoStorage extends PluginBase implements TripalStorageInterface {
 
 	/**
 	 * @{inheritdoc}
+	 *
+	 * NOTE: This is where we actually save the record to chado. It is done here
+	 * to ensure that the id is available for saving in the Drupal schema in
+	 * saveEntity(). This maps to the Tripal3 tripal_chado_field_storage_write().
 	 */
 	public function preSaveEntity(&$entity) {
+
+		// Get the Tripal Content Type.
+		$bundle = \Drupal\tripal\Entity\TripalEntityType::load($entity->getType());
+		// Then get the Tripal Term and Tripal Vocab.
+		$term = $bundle->getTerm();
+		$vocab = $term->getVocab();
+
+		// Use the Tripal Vocab/Term to get the chado dbxref/cvterm.
+		$term_accession = $term->getAccession();
+		$vocab_name = $vocab->getName();
+		// @debug dpm($vocab_name . ':' . $term_accession, 'term');
+		/* @todo upgrade
+		$dbxref = chado_get_dbxref([
+	    'accession' => $term_accession,
+	    'db_id' => ['name' => $vocab_name],
+	  ]);
+	  $cvterm = chado_get_cvterm(['dbxref_id' => $dbxref->dbxref_id]);
+		*/
+
+		// Get the value for each field in the current entity.
+		foreach ($entity->getFields() as $name => $field) {
+			// @debug dpm($field->getValue(), $name);
+		}
+
 		return FALSE; // Entities not altered.
 	}
 
