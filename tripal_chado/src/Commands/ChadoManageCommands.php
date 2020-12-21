7142c2a7d219243262b4130da26ca15c8b5c7f48
Lacey-Anne Sanderson <lacey.sanderson@usask.ca>
2020-12-21 14:28:10 -0600

    Add more chado commands.

--- tripal_chado/src/Commands/ChadoManageCommands.php
+++ tripal_chado/src/Commands/ChadoManageCommands.php
@@ -10,7 +10,7 @@ use Drush\Commands\DrushCommands;
 class ChadoManageCommands extends DrushCommands {
 
   /**
-   * Executes one or more jobs in the Tripal Jobs Queue.
+   * Install the Chado schema.
    *
    * @command tripal-chado:install-chado
    * @aliases trp-install-chado
@@ -30,6 +30,49 @@ class ChadoManageCommands extends DrushCommands {
     $success = $installer->install($options['chado-version']);
     if ($success) {
       $this->output()->writeln('<info>[Success]</info> Chado was successfully installed.');
+    }
+  }
+
+  /**
+   * Drops the Chado schema.
+   *
+   * @command tripal-chado:drop-chado
+   * @aliases trp-drop-chado
+   * @options schema-name
+   *   The name of the schema to drop.
+   * @usage drush trp-drop-chado --schema-name='testchado'
+   *   Removes the chado schema named "testchado".
+   */
+  public function dropChado($options = ['schema-name' => 'chado']) {
+
+    $installer = \Drupal::service('tripal.bulkPgSchemaInstaller');
+    $installer->dropSchema($options['schema-name']);
+    $present = $installer->checkSchema($options['schema-name']);
+    if (!$present) {
+      $this->output()->writeln('<info>[Success]</info> Chado was successfully dropped.');
+    }
+  }
+
+  /**
+   * Set-up the Tripal Chado test environment.
+   *
+   * @command tripal-chado:setup-tests
+   * @aliases trp-prep-tests
+   * @usage drush trp-prep-tests
+   *   Sets up the standard Tripal Chado test environment.
+   */
+  public function setupTests() {
+
+    $chado_version = 1.3;
+    $schema_name = 'testchado';
+
+    $this->output()->writeln('Installing chado version ' . $chado_version . ' in a schema named "' . $schema_name. '"');
+
+    $installer = \Drupal::service('tripal_chado.chadoInstaller');
+    $installer->setSchema($schema_name);
+    $success = $installer->install($chado_version);
+    if ($success) {
+      $this->output()->writeln('<info>[Success]</info> Chado was successfully installed for testing.');
     }
   }
 }
