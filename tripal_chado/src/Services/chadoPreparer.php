0e8683e92b92c38cf3da8673188444638aaa3930
Peter Richter <peter.richter@uconn.edu>
2021-01-12 12:04:21 -0500

    Implement constructor, setSchema, setJob, etc

--- tripal_chado/src/Services/chadoPreparer.php
+++ tripal_chado/src/Services/chadoPreparer.php
@@ -27,24 +27,65 @@ class ChadoPreparer {
   protected $job = NULL;
 
   /**
-   *
+   * Constructor: initialize connections.
    */
   public function __construct() {
+    $this->connection = \Drupal::database();
 
+    // Initialize the logger.
+    $this->logger = \Drupal::service('tripal.logger');
+  }
+
+  /**
+   * Set the schema name.
+   */
+  public function setSchema($schema_name) {
+    // Schema name must be all lowercase with no special characters.
+    // It should also be a single word.
+    if (preg_match('/^[a-z][a-z0-9]+$/', $schema_name) === 0) {
+      $this->logger->error('The schema name must be a single word containing only lower case letters or numbers and cannot begin with a number.');
+      return FALSE;
+    }
+    else {
+      $this->logger->info('Setting Schema to "' . $schema_name . '".');
+      $this->schemaName = $schema_name;
+      return TRUE;
+    }
+}
+
+  /**
+   * A setter for the job object if this class is being run using a Tripal job.
+   */
+  public function setJob(\Drupal\tripal\Services\TripalJob $job) {
+    $this->job = $job;
+    $this->logger->setJob($job);
+    return TRUE;
+  }
+
+  /**
+   * Retrieve the Drupal connection to the database.
+   *
+   * @return Drupal\database
+   *   Current Drupal connection.
+   */
+  public function getDrupalConnection() {
+    return $this->connection;
+  }
+
+  /**
+   * Retrieves the message logger.
+   *
+   * @return object
+   *
+   */
+  public function getLogger() {
+    return $this->logger;
   }
 
   /**
    *
    */
-  public function setSchema($schema_name) {}
+  public function prepare() {
 
-  /**
-   *
-   */
-  public function setJob(\Drupal\tripal\Services\TripalJob $job) {}
-
-  /**
-   *
-   */
-  public function prepare() {}
+  }
 }
