c8057c7f0ed7e33b9bcbeaaeb451a8c1f16c83ab
Josh B <4ctrl.alt.del@gmail.com>
2021-09-15 11:51:25 -0700

    Worked on tripal vocab interface.
    Added additional interface methods that provide visibility to the parent/child
    tree like structure of terms within a vocabulary.

--- tripal4/src/Plugin/TripalVocabInterface.php
+++ tripal4/src/Plugin/TripalVocabInterface.php
@@ -22,15 +22,44 @@ interface TripalVocabInterface extends PluginInspectionInterface {
   public function hasTerm(Term $term);
 
   /**
-   * Returns a list of valid terms based off matches from the given partial
-   * term name. A given max number of terms are returned.
-   * 
+   * Returns a list of valid terms based off matches from the given partial term
+   * name. A given max number of terms are returned.
+   *
    * @param string partial
    *   The partial term name.
+   *
+   * @param int max
+   *   The given max number returned.
    *
    * @return array
    *   An array of valid \Drupal\tripal4\Vocabulary\Term objects.
    */
-  public function suggestTerms(string $partial);
+  public function suggestTerms(string $partial, int $max = 10);
+
+  /**
+   * Gets the parent of the given term. The given term must be a valid term for
+   * this vocabulary. If the given term is a root of this vocabulary then NULL
+   * is returned.
+   *
+   * @param \Drupal\tripal4\Vocabulary\Term child
+   *   The given term.
+   *
+   * @return mixed
+   *   The parent term or NULL.
+   */
+  public function getParent($child);
+
+  /**
+   * Gets the children terms of the given term. The given term must be a valid
+   * term for this vocabulary or NULL. If the given term is NULL then the root
+   * children of this vocabulary is returned.
+   *
+   * @param mixed parent
+   *   The given term or NULL.
+   *
+   * @return array
+   *   An array of \Drupal\tripal4\Vocabulary\Term children objects.
+   */
+  public function getChildren($parent = NULL);
 
 }
