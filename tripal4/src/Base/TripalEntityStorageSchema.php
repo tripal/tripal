ad0090c6853d8701819f25ab05e61cab5c5ebc2d
Lacey-Anne Sanderson <laceyannesanderson@gmail.com>
2021-12-15 17:57:32 -0600

    added some comments.

--- tripal4/src/Base/TripalEntityStorageSchema.php
+++ tripal4/src/Base/TripalEntityStorageSchema.php
@@ -8,13 +8,28 @@ class TripalEntityStorageSchema extends SqlContentEntityStorageSchema {
   public function onFieldableEntityTypeCreate(EntityTypeInterface $entity_type, array $field_storage_definitions) {
     parent::onFieldableEntityTypeCreate($entity_type,$field_storage_definitions);
 
-    // build storage operations for creating types
+    // This is where we create the types each field describes in the
+    // associated TripalStorage instance.
     $storageOps = array()
     foreach ($field_storage_definitions as $storageDefinition) {
       $field = \Drupal::service("plugin.manager.field.field_type").getInstance($storageDefinition->getType());
       if ($field instanceof TripalFieldItemInterface) {
+        // Each field can define it's value as a single-depth array of
+        // key => value pairs where the key is a name for that data (e.g. accession )
+        // and the value is an implementation of StoragePropertyTypeBase setting the
+        // type of the value (e.g. string).
+        // Here we are retrieving that key-value array.
         $types = $field->tripalTypes();
+        // Additionally, each field will have a TripalStorage type associated
+        // with it that we are retrieving here.
+        // Note: this can be changed in the admin UI since all TripalStorage
+        // types are expected to be able to read this key-value structure.
         $tsid = $field->tripalStorageId();
+        // Since each StoragePropertyTypeBase implementation knows it's
+        // key (saved as id) and associated field, we can merge them all here
+        // without saving the key mapping.
+        // We're basically just compiling all field property types with the
+        // same storage for the entire entity here.
         if (array_key_exists($tsid,$storageOps)) {
           $storageOps[$tsid] = array_merge($storageOps[$tsid],$types);
         }
@@ -24,7 +39,9 @@ class TripalEntityStorageSchema extends SqlContentEntityStorageSchema {
       }
     }
 
-    // iterate through all storage plugins and create types
+    // Now we iterate through all storage plugins and add the compiled types
+    // for each one. This will also save this information at the TripalStorage
+    // layer.
     foreach ($storageOps as $tsid => $types) {
       $tripalStorage = \Drupal::service("plugin.manager.tripal.storage")->getInstance($tsid);
       $tripalStorage->addTypes($types);
@@ -59,10 +76,13 @@ class TripalEntityStorageSchema extends SqlContentEntityStorageSchema {
     // build storage add and update operations
     $storageAdd = array()
     $storageUpdate = array()
+    // For each of the new field types...
     foreach ($newTypes as $name => $field) {
       if ($field instanceof TripalFieldItemInterface) {
         $types = $field->tripalTypes();
         $tsid = $field->tripalStorageId();
+        // Case 1: the new field already existed and has the same storage
+        //   - we will want to update the key-value information.
         if ( array_key_exists($name,$oldTypes) && $oldTypes[$name]->tripalStorageId() == $tsid ) {
           $otypes = $oldTypes[$name]->tripalTypes();
           if (array_key_exists($tsid,$storageUpdate)) {
@@ -72,6 +92,10 @@ class TripalEntityStorageSchema extends SqlContentEntityStorageSchema {
             $storageOps[$tsid] = array(array($types,$otypes));
           }
         }
+        // Case 2: the new field did not exist before with this storage.
+        //   - Both brand new fields (2a) and ones which already existed but with a
+        //     different storage (2b) will fall in this case.
+        //   - we will need to add the new field to it's new storage.
         else {
           if (array_key_exists($tsid,$storageAdd)) {
             $storageAdd[$tsid] = array_merge($storageAdd[$tsid],$types);
@@ -83,10 +107,13 @@ class TripalEntityStorageSchema extends SqlContentEntityStorageSchema {
       }
     }
 
-    // build storage remove operations
+    // Build storage remove operations.
     $storageRemove = array()
     foreach ($oldTypes as $name => $field) {
       if ($field instanceof TripalFieldItemInterface) {
+        // Case 3: The old field no longer exists for this entity.
+        //   - (i.e. the old field is not in the new field list)
+        //   - we will want to remove it from the storage.
         if (!array_key_exists($name,$newTypes)) {
           $types = $field->tripalTypes();
           $tsid = $field->tripalStorageId();
