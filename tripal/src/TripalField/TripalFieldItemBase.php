3cc9922899fe19d01a7323c43737837ac4adb948
Josh B <4ctrl.alt.del@gmail.com>
2022-02-24 13:19:36 -0800

    Finished tripal field item base.
    Finished adding all necessary methods to the tripal field item base class. This
    includes the schema and property definitions that are auto generated using the
    tripal field item base interface. This class was also moved to a base class
    namespace for tripal fields because it is the base class and not an actual
    implementation of a tripal field.

---
+++ tripal/src/TripalField/TripalFieldItemBase.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Drupal\tripal\TripalField;
+
+use Drupal\Core\Field\FieldItemBase;
+use Drupal\tripal\TripalStorage\TripalFieldItemInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Cache\FieldStorageDefinitionInterface;
+use Drupal\tripal\TripalStorage\IntStoragePropertyType;
+use Drupal\Core\TypedData\DataDefinition;
+
+class TripalFieldItemBase extends FieldItemBase extends TripalFieldItemInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fieldSettingsForm(array $form, FormStateInterface $form_state) {
+    $elements = [];
+
+    $elements["vocabulary_term"] = [
+      "#type" => "string",
+      "#title" => $this->t("Vocabulary Term"),
+      "#required" => TRUE,
+      "#description" => $this->t("The vocabulary term."),
+      "#disabled" => $has_data
+    ];
+
+    return $elements + parent::fieldSettingsForm($form,$form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition) {
+    $properties = [];
+
+    foreach ($this->tripalTypes() as $type) {
+      if ($type instanceof IntStoragePropertyType) {
+        $properties[$type->getFieldKey()] = DataDefinition::create("integer");
+      }
+    }
+
+    return $properties;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function schema(FieldStorageDefinitionInterface $field_definition) {
+    $schema = [];
+
+    foreach ($this->tripalTypes() as $type) {
+      if ($type instanceof IntStoragePropertyType) {
+        $column = [
+          "type" => "int"
+        ];
+        $schema["columns"][$type->getFieldKey()] = $column;
+      }
+    }
+
+    return $schema;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function storageSettingsForm(array &$form, FormStateInterface $form_state, $has_data) {
+    $elements = [];
+
+    # turn into selection
+    $elements["storage_plugin_id"] = [
+      "#type" => "string",
+      "#title" => $this->t("Tripal Storage Plugin ID."),
+      "#required" => TRUE,
+      "#description" => $this->t(""),
+      "#disabled" => $has_data
+    ];
+
+    return $elements + parent::storageSettingsForm($form,$form_state,$has_data);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function tripalStorageId() {
+    return $this->getSetting("storage_plugin_id");
+  }
+}
+
+#First field to implement:
+#rdfs_type
+#https://github.com/tripal/tripal/tree/7.x-3.x/tripal/includes/TripalFields/rdfs__type
