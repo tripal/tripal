853c169b1830494fc814aae76e610ea7528154f5
Josh B <4ctrl.alt.del@gmail.com>
2022-03-17 10:50:19 -0700

    Added default tripal rdfs type widget.
    Finished adding initial code for default tripal rdfs type widget.

---
+++ tripal/src/Plugin/Field/FieldWidget/DefaultTripalRDFSTypeWidget.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\tripal\Plugin\Field\FieldType;
+
+use Drupal\tripal\TripalField\TripalWidgetBase;
+
+/**
+ * Plugin implementation of default Tripal RDFS content type widget.
+ *
+ * @FieldWidget(
+ *   id = "default_tripal_rdfs_type_widget",
+ *   label = @Translation("Default Content Type Widget"),
+ *   description = @Translation("The default resource content type widget."),
+ *   field_types = [
+ *     "tripal_rdfs_type"
+ *   ]
+ * )
+ */
+class DefaultTripalRDFSTypeWidget extends TripalWidgetBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+    $element["details"]["type"] = [
+      "#type" => "string",
+      "#title" => $this->t("Content Type"),
+      "#required" => TRUE,
+      "#description" => $this->t("The resource content type.")
+    ];
+
+    return $elements;
+  }
+}
