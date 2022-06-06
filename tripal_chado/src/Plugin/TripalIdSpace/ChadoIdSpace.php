01c6d981861b2516718e5a6b42cf5d0714fe2eb4
Stephen Ficklin <spficklin@gmail.com>
2022-06-06 14:36:24 -0700

    period check-in. Test is working. waiting on bug fixes

---
+++ tripal_chado/src/Plugin/TripalIdSpace/ChadoIdSpace.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace Drupal\tripal_chado\Plugin\TripalIdSpace;
+
+use Drupal\tripal\TripalVocabTerms\TripalIdSpaceBase;
+
+/**
+ * Chado Implementation of TripalIdSpaceBase
+ * 
+ *  @TripalIdSpace(
+ *    id = "chado_id_space",
+ *    label = @Translation("Vocabulary IDSpace in Chado"),
+ *  )
+ */
+class ChadoIdSpace extends TripalIdSpaceBase {
+
+  /**
+   * Gets the parent of the given term. The given term must be a valid term for
+   * this id space. If the given term is a root of this id space then NULL
+   * is returned.
+   *
+   * @param Drupal\tripal\TripalVocabTerms\TripalTerm $child
+   *   The given term.
+   *
+   * @return Drupal\tripal\TripalVocabTerms\TripalTerm|NULL
+   *   The parent term or NULL.
+   */
+  public function getParent($child){
+    
+  }
+  
+  /**
+   * Gets the children terms of the given term. The given term must be a valid
+   * term for this id space or NULL. If the given term is NULL then the root
+   * children of this id space is returned.
+   *
+   * @param Drupal\tripal\TripalVocabTerms\TripalTerm|NULL $parent
+   *   The given term or NULL.
+   *
+   * @return array
+   *   An array of Drupal\tripal\TripalVocabTerms\TripalTerm children objects.
+   */
+  public function getChildren($parent = NULL){
+    
+  }
+  
+  /**
+   * Returns the term in this id space with the given accession. If no such term
+   * exists then NULL is returned.
+   *
+   * @param string $accession
+   *   The accession.
+   *
+   * @return Drupal\tripal\TripalVocabTerms\TripalTerm|NULL
+   *   The term or NULL.
+   */
+  public function getTerm($accession){
+    
+  }
+  
+  /**
+   * Returns the terms in this id space whose names match the given name with
+   * the given options array.
+   *
+   * The given options array has the following recognized keys:
+   *
+   * exact(boolean): True to only include exact matches else false to include
+   * all substring matches. The default is false.
+   *
+   * @param string $name
+   *   The name.
+   *
+   * @param array $options
+   *   The options array.
+   *
+   * @return array
+   *   Array of matching Drupal\tripal\TripalVocabTerms\TripalTerm instances.
+   */
+  public function getTerms($name,$options){
+    
+  }
+  
+  /**
+   * Sets the default vocabulary of this id space to the given vocabulary name.
+   *
+   * @param string name
+   *   The vocabulary name.
+   */
+  public function setDefaultVocabulary($name){
+    
+  }
+  
+  /**
+   * Returns this id space's default vocabulary name or NULL if no default has
+   * been set.
+   *
+   * @return string
+   *   The vocabulary name.
+   */
+  public function getDefaultVocabulary(){
+    
+  }
+  
+  /**
+   * Saves the given term to this id space with the given options array and
+   * optional parent. If the given parent is NULL and the given term is new then
+   * it is added as a root term of this id space. If the given parent is NULL,
+   * the given term already exists, and the appropriate option was given to
+   * update the existing term's parent then it is moved to a root term of this
+   * id space.
+   *
+   * The given options array has the following recognized keys:
+   *
+   * failIfExists(boolean): True to force this method to fail if the given term
+   * already exists else false to update the term if it already exists. The
+   * default is false.
+   *
+   * updateParent(boolean): True to update The given term's parent to the one
+   * given or false to not update the existing term's parent. If the given term
+   * is new this has no effect. The default is false.
+   *
+   * @param Drupal\tripal\TripalVocabTerms\TripalTerm $term
+   *   The term.
+   *
+   * @param array $options
+   *   The options array.
+   *
+   * @param Drupal\tripal\TripalVocabTerms\TripalTerm|NULL $parent
+   *   The parent term or NULL.
+   *
+   * @return bool
+   *   True on success or false otherwise.
+   */
+  public function saveTerm($term,$options,$parent = NULL){
+    
+  }
+  
+  /**
+   * Removes the term with the given accession from this id space. All children
+   * terms are also removed.
+   * !!!WARNING!!!
+   * If the removed term in this id space is referenced by entities this could
+   * break data integrity. This method must be used with extreme caution!
+   *
+   * @param string $accession
+   *   The accession.
+   *
+   * @return bool
+   *   True if the term was removed or false otherwise.
+   */
+  public function removeTerm($accession){
+    
+  }
+  
+  /**
+   * Returns the URL prefix of this id space.
+   *
+   * @return string
+   *   The URL prefix.
+   */
+  public function getURLPrefix(){
+    
+  }
+  
+  /**
+   * Sets the URL prefix of this id space to the given URL prefix.
+   *
+   * @param string $prefix
+   *   The URL prefix.
+   */
+  public function setURLPrefix($prefix){
+    
+  }
+}
--- tripal_chado/tests/src/Functional/ChadoVocabTermsTest.php
+++ tripal_chado/tests/src/Functional/ChadoVocabTermsTest.php
@@ -2,10 +2,6 @@
 
 namespace Drupal\Tests\tripal_chado\Functional;
 
-use Drupal\Tests\tripal_chado\Functional\ChadoTestBase;
-use Drupal\tripal_chado\ChadoVocabTerms\ChadoVocabulary;
-use Drupal\tripal_chado\ChadoVocabTerms\ChadoIdSpace;
-use Drupal\tripal_chado\ChadoVocabTerms\ChadoTerm;
 
 /**
  * Tests for the ChadoCVTerm classes
@@ -22,9 +18,32 @@ class ChadoVocabTermsTest extends ChadoTestBase {
    * @Depends Drupal\tripal_chado\Task\ChadoInstallerTest::testPerformTaskInstaller
    *
    */
-  public function testVocabulary() {
+  public function testIdSpace() {
     // Create a temporary schema.
     $biodb = $this->getTestSchema(ChadoTestBase::INIT_DUMMY);
+        
+    // Create instances of the plugin managers for ID Space and Vocabulary.
+    $idsmanager = \Drupal::service('tripal.collection_plugin_manager.idspace');
+    $vmanager = \Drupal::service('tripal.collection_plugin_manager.vocabulary');
+    
+    
+    // We need to create an instance of the ID space collection using an identifier for
+    // our collection.  We'll use the same name as the CV ID space.
+    // We'll model this test after the Gene Ontology which has one ID Space (i.e., GO) but
+    // three vocabularies.
+    $goIdSpace = $idsmanager->createCollection("GO","chado_id_space");    
+    $cc = $vmanager->createCollection("cellular_component","chado_vocabulary");
+    $mf = $vmanager->createCollection("molecular_function","chado_vocabulary");
+    $bp = $vmanager->createCollection("biological_process","chado_vocabulary");    
+    $mf->addIdSpace("GO");
+    $cc->addIdSpace("GO");
+    $bp->addIdSpace("GO");
+    
+    // ID spaces need a default vocabulary.  It doesn't make sense to have one for
+    // the gene ontology but we'll set one anyway.
+    $goIdSpace->setDefaultVocabulary("cellular_component");
+    
+    
     
   }
 }
