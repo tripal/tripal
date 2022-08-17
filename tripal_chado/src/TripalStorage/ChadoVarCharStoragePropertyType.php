88c35bd4917ac2298a08f0a891abce828a2429ee
Lacey-Anne Sanderson <laceyannesanderson@gmail.com>
2022-08-17 16:12:51 -0600

    Remove ChadoStoragePropertyTypeTrait as it's not needed.

--- tripal_chado/src/TripalStorage/ChadoVarCharStoragePropertyType.php
+++ tripal_chado/src/TripalStorage/ChadoVarCharStoragePropertyType.php
@@ -9,25 +9,4 @@ use Drupal\tripal\TripalStorage\VarCharStoragePropertyType;
  */
 class ChadoVarCharStoragePropertyType extends VarCharStoragePropertyType {
 
-  use ChadoStoragePropertyTypeTrait;
-  
-  /**
-   * Constructs a new variable character tripal storage property type.
-   *
-   * @param string entityType
-   *   The entity type associated with this property type.
-   * @param string fieldType
-   *   The field type associated with this property type.
-   * @param string key
-   *   The key associated with this property type.
-   * @param int size
-   *   The maximum size of characters for this type.
-   */
-  public function __construct($entityType, $fieldType, $key, $size = 255) {
-    parent::__construct($entityType, $fieldType, $key, $size);
-    $this->setMapping();    
-    $this->verifyTableColumn();
-  }
-
-
 }
