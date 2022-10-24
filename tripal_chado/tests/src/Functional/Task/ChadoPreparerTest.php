1c69ce9ddd6ac0af0ed0659b04552d7c4c8e1a95
Lacey Sanderson <laceyannesanderson@gmail.com>
2022-10-24 14:51:55 -0600

    Skeleton preparer test, currently fails.

---
+++ tripal_chado/tests/src/Functional/Task/ChadoPreparerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\Tests\tripal_chado\Functional\Task;
+
+use Drupal\Tests\tripal_chado\Functional\ChadoTestBrowserBase;
+use Drupal\tripal_chado\Task\ChadoPreparer;
+
+
+/**
+ * Tests for Chado preparer task.
+ *
+ * @coversDefaultClass \Drupal\tripal_chado\Task\ChadoPreparer
+ *
+ * @group Tripal
+ * @group Tripal Chado
+ * @group Tripal Chado Task
+ * @group Tripal Chado Preparer
+ */
+class ChadoPreparerFunctionalTest extends ChadoTestBrowserBase {
+
+  /**
+   * Tests task.
+   *
+   * @cover ::setParameters
+   * @cover ::performTask
+   */
+  public function testPerformTaskRenamer() {
+
+    // Test preparer.
+    $preparer = \Drupal::service('tripal_chado.preparer');
+    $preparer->setParameters([
+      'output_schemas' => [$this->chado->getSchemaName()],
+    ]);
+    $success = $preparer->performTask();
+    $this->assertTrue($success, 'Task performed.');
+
+  }
+}
