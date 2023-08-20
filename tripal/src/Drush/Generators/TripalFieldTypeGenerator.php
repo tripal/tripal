af1c30986647d906f7602b7e40fa41eef36ebf63
Lacey-Anne Sanderson <laceyannesanderson@gmail.com>
2023-08-20 11:37:22 -0600

    Update field type to include additional required values.

--- tripal/src/Drush/Generators/TripalFieldTypeGenerator.php
+++ tripal/src/Drush/Generators/TripalFieldTypeGenerator.php
@@ -30,12 +30,14 @@ final class TripalFieldTypeGenerator extends BaseGenerator {
     // Validators
     $id_validator = new RegExp('/^[a-z][a-z0-9_]*[a-z0-9]$/', 'The value must consist of only lower case alphanumeric characters and underscores. It should start with a letter and not end with an underscore.');
     $label_validator = new RegExp('/^[a-zA-Z][a-zA-Z0-9- ]*[a-zA-Z0-9]$/', 'The value must be alphanumeric. We suggest focusing on a title-case human-readable name for your field.');
+    $term_validator = new RegExp('/:/', 'The value must be an ID Space and Accession defining the term with a : separating them (e.g. rdfs:type).');
 
     // Field Type
     $vars['field_id'] = $prompt->ask('Field Type ID', '{machine_name}_example', $id_validator);
 
     $vars['field_label'] = $prompt->ask('Field Type Label', Utils::machine2human($vars['field_id'], TRUE) . ' Field Type', $label_validator);
     $vars['description'] = $prompt->ask('Field Type Description');
+    $vars['field_term'] = $prompt->ask('Field Term (IdSpace:Accession)', 'rdfs:type', $term_validator);
     $vars['widget_id'] = $prompt->ask('Default Field Widget ID', '{field_id}_widget', $id_validator);
     $vars['formatter_id'] = $prompt->ask('Default Field Formatter ID', '{field_id}_formatter', $id_validator);
     $vars['class'] = $prompt->askClass(default: '{field_id|camelize}TypeItem');
