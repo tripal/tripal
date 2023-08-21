5a4b43d39da1065d62739f35809bc4916e65c41e
Douglas Senalik <dsenalik@wisc.edu>
2023-08-21 15:37:17 -0500

    Lower level @group options

--- tripal_chado/tests/src/Kernel/Plugin/ChadoStorage/ChadoAnalysisDefaultTest.php
+++ tripal_chado/tests/src/Kernel/Plugin/ChadoStorage/ChadoAnalysisDefaultTest.php
@@ -38,6 +38,7 @@ use Drupal\Tests\tripal_chado\Functional\MockClass\FieldConfigMock;
  * @group Tripal
  * @group Tripal Chado
  * @group Tripal Chado Fields
+ * @group Tripal Chado Fields Analysis
  * @group ChadoStorage
  */
 class ChadoAnalysisDefaultTest extends ChadoTestKernelBase {
--- tripal_chado/tests/src/Kernel/Plugin/ChadoStorage/ChadoContactDefaultTest.php
+++ tripal_chado/tests/src/Kernel/Plugin/ChadoStorage/ChadoContactDefaultTest.php
@@ -31,8 +31,8 @@ use Drupal\Tests\tripal_chado\Functional\MockClass\FieldConfigMock;
  * @group Tripal
  * @group Tripal Chado
  * @group Tripal Chado Fields
+ * @group Tripal Chado Fields Contact
  * @group ChadoStorage
- * @group ContactTest
  */
 class ChadoContactDefaultTest extends ChadoTestKernelBase {
 
