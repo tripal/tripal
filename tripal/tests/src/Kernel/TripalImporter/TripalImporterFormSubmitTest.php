f65b9d390328250b045b2d8c7f56080cb47a9b06
Lacey-Anne Sanderson <lacey.sanderson@usask.ca>
2023-09-12 14:05:14 -0600

    Added testing for local file.

--- tripal/tests/src/Kernel/TripalImporter/TripalImporterFormSubmitTest.php
+++ tripal/tests/src/Kernel/TripalImporter/TripalImporterFormSubmitTest.php
@@ -48,7 +48,7 @@ class TripalImporterFormSubmitTest extends KernelTestBase {
       'require_analysis' => FALSE,
       'button_text' => 'Import file',
       'file_upload' => TRUE,
-      'file_load' => TRUE,
+      'file_local' => TRUE,
       'file_remote' => TRUE,
       'file_required' => TRUE,
       'cardinality' => 1,
@@ -136,12 +136,109 @@ class TripalImporterFormSubmitTest extends KernelTestBase {
   }
 
   /**
-   * Tests the validation of the file elements added by the Base Tripal Importer.
+   * Tests the validation of the local file element added by the Base Tripal Importer.
    *
    * Specifically,
-   *  -
+   *  - VALID: provide an existing local file, correct file format
+   *  - ERROR: indicate a file which does not exist locally.
    */
-  public function testTripalImporterFormValidateFile() {
+  public function testTripalImporterFormValidateLocalFile() {
+    global $DRUPAL_ROOT;
+
+    $manager = $this->setMockManager($this->definitions);
+    $container = \Drupal::getContainer();
+    $container->set('tripal.importer', $manager);
+
+    $plugin_id = 'fakeImporterName';
+    $expected = $this->definitions;
+
+    // --- CASE VALID
+    // --- Supply a valid remote path and ensure the form works!
+    $test_file_path = 'modules/contrib/tripal/LICENSE.txt';
+
+    // Now setup the form_state.
+    $form_state = new \Drupal\Core\Form\FormState();
+    $form_state->addBuildInfo('args', [$plugin_id]);
+    $form_state->setValue('file_local', $test_file_path);
+
+    // Now try validation!
+    \Drupal::formBuilder()->submitForm(
+      'Drupal\tripal\Form\TripalImporterForm',
+      $form_state
+    );
+    // And do some basic checks to ensure there were no errors.
+    $this->assertTrue($form_state->isValidationComplete(),
+      "We expect the form state to have been updated to indicate that validation is complete.");
+    //   Looking for form validation errors
+    $form_validation_messages = $form_state->getErrors();
+    $helpful_output = [];
+    foreach ($form_validation_messages as $element => $markup) {
+      $helpful_output[] = $element . " => " . (string) $markup;
+    }
+    $this->assertCount(0, $form_validation_messages,
+      "We should not have any validation errors for '$test_file_path' but instead we have: " . implode(" AND ", $helpful_output));
+    //   Looking for drupal message errors.
+    $messages = \Drupal::messenger()->all();
+    $this->assertIsArray($messages,
+      "We expect to have status messages to the user on submission of the form.");
+    $this->assertArrayNotHasKey('error', $messages,
+      "There should not be any error messages from this form. Instead we recieved: " . print_r($messages, TRUE));
+    //   Now delete drupal messages so we start the next test clean.
+    \Drupal::messenger()->deleteAll();
+
+    // --- CASE ERROR
+    // --- Supply a file which does not exist locally
+    $bad_remote_uri = [
+      '/var/di/dump/doo',
+      'relative/but/not/here',
+      'singleWord.no',
+    ];
+    foreach ($bad_remote_uri as $test_file_path) {
+      // Now setup the form_state.
+      $form_state = new \Drupal\Core\Form\FormState();
+      $form_state->addBuildInfo('args', [$plugin_id]);
+      $form_state->setValue('file_local', $test_file_path);
+
+      // Now try validation!
+      \Drupal::formBuilder()->submitForm(
+        'Drupal\tripal\Form\TripalImporterForm',
+        $form_state
+      );
+      // And do some basic checks to ensure there were no errors.
+      $this->assertTrue($form_state->isValidationComplete(),
+        "We expect the form state to have been updated to indicate that validation is complete.");
+      //   Looking for form validation errors
+      $form_validation_messages = $form_state->getErrors();
+      $this->assertCount(1, $form_validation_messages,
+        "We expect validation errors for '$test_file_path' but did not recieve them.");
+      $this->assertArrayHasKey('file_local', $form_validation_messages,
+        "There should be an entry for file_local in the validation errors for '$test_file_path'.");
+      //   Looking for drupal message errors.
+      $messages = \Drupal::messenger()->all();
+      $this->assertIsArray($messages,
+        "We expect to have status messages to the user on submission of the form.");
+      $this->assertArrayHasKey('error', $messages,
+        "There should be any error messages from this form but we didn't recieve any.");
+      $this->assertCount(1, $messages['error'],
+        "There should be only one error message.");
+      $this->assertStringContainsString('Cannot find the file', (string) $messages['error'][0],
+        "The error did not match the one we expected for an file which doesn't exist for file_local.");
+      $this->assertArrayNotHasKey('status', $messages,
+        "There should not be any success/status messages from this form. Instead we recieved: " . print_r($messages, TRUE));
+      //   Now delete drupal messages so we start the next test clean.
+      \Drupal::messenger()->deleteAll();
+    }
+  }
+
+  /**
+   * Tests the validation of the remote file element added by the Base Tripal Importer.
+   *
+   * Specifically,
+   *  - VALID: provide a valid remote URL
+   *  - ERROR: provide a badly formatted URI
+   *  - ERROR: provide a correctly formatted but non-existent URI
+   */
+  public function testTripalImporterFormValidateRemoteFile() {
 
     $manager = $this->setMockManager($this->definitions);
     $container = \Drupal::getContainer();
