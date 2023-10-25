1fc29382d1ce90d71335d00246d0ffade2b8cfd8
Lacey-Anne Sanderson <lacey.sanderson@usask.ca>
2023-10-25 16:56:52 -0600

    Stub out tests for store and store_link.

---
+++ tripal_chado/tests/src/Kernel/Plugin/ChadoStorage/ChadoStorageActions_StoreTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Drupal\Tests\tripal_chado\Kernel\Plugin\ChadoStorage;
+
+use Drupal\Tests\tripal_chado\Kernel\ChadoTestKernelBase;
+use Drupal\Tests\tripal_chado\Traits\ChadoStorageTestTrait;
+
+use Drupal\tripal\TripalStorage\StoragePropertyValue;
+use Drupal\tripal\TripalStorage\StoragePropertyTypeBase;
+use Drupal\tripal_chado\TripalStorage\ChadoIntStoragePropertyType;
+use Drupal\tripal_chado\TripalStorage\ChadoVarCharStoragePropertyType;
+
+/**
+ * Tests that specific ChadoStorage actions perform as expected.
+ *
+ * @group Tripal
+ * @group Tripal Chado
+ * @group ChadoStorage
+ * @group ChadoStorage Actions
+ */
+class ChadoStorageActions_StoreTest extends ChadoTestKernelBase {
+
+  use ChadoStorageTestTrait;
+
+  // We will populate this variable at the start of each test
+  // with fields specific to that test.
+  protected $fields = [];
+
+  protected $yaml_file = __DIR__ . "/ChadoStorageActions-FieldDefinitions.yml";
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() :void {
+    parent::setUp();
+
+    // Ensure we see all logging in tests.
+    \Drupal::state()->set('is_a_test_environment', TRUE);
+
+    // We need to mock the logger to test the progress reporting.
+    $container = \Drupal::getContainer();
+    $mock_logger = $this->getMockBuilder(\Drupal\tripal\Services\TripalLogger::class)
+      ->onlyMethods(['warning', 'error'])
+      ->getMock();
+    $mock_logger->method('warning')
+      ->willReturnCallback(function($message, $context, $options) {
+        print str_replace(array_keys($context), $context, $message);
+        return NULL;
+      });
+    $mock_logger->method('error')
+      ->willReturnCallback(function($message, $context, $options) {
+        print str_replace(array_keys($context), $context, $message);
+        return NULL;
+      });
+    $container->set('tripal.logger', $mock_logger);
+
+    $this->setUpChadoStorageTestEnviro();
+  }
+
+/**
+   * Test the store action.
+   *
+   * Chado Table:
+   *     Columns:
+   *
+   * Specifically, ensure that a property with the store action
+   *  -
+   */
+  public function testStoreAction() {
+
+    // Set the fields for this test and then re-populate the storage arrays.
+    // $this->setFieldsFromYaml($this->yaml_file, 'testStoreAction');
+    // $this->cleanChadoStorageValues();
+
+    // Stop here and mark this test as incomplete.
+    $this->markTestIncomplete(
+      'This test has not been implemented yet.'
+    );
+  }
+}
