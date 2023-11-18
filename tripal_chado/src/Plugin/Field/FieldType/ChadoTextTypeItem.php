ddf2eeb74581b403ed1837d455bc5625afabaf62
dsenalik <dsenalik@wisc.edu>
2023-11-18 14:51:42 -0600

    Chado Text Type can now be added through GUI

--- tripal_chado/src/Plugin/Field/FieldType/ChadoTextTypeItem.php
+++ tripal_chado/src/Plugin/Field/FieldType/ChadoTextTypeItem.php
@@ -10,6 +10,8 @@ use Drupal\core\Field\FieldDefinitionInterface;
 use Drupal\tripal_chado\TripalField\ChadoFieldItemBase;
 use Drupal\tripal_chado\TripalStorage\ChadoTextStoragePropertyType;
 use Drupal\tripal_chado\TripalStorage\ChadoIntStoragePropertyType;
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\ReplaceCommand;
 
 
 /**
@@ -44,6 +46,9 @@ class ChadoTextTypeItem extends ChadoFieldItemBase {
     $entity_type_id = $field_definition->getTargetEntityTypeId();
     $settings = $field_definition->getSetting('storage_plugin_settings');
     $base_table = $settings['base_table'];
+    if (!$base_table) {
+      return;
+    }
     $base_column = $settings['base_column'];
 
     // Get the base table columns needed for this field.
@@ -72,4 +77,56 @@ class ChadoTextTypeItem extends ChadoFieldItemBase {
       ]),
     ];
   }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function storageSettingsForm(array &$form, FormStateInterface $form_state, $has_data) {
+    $elements = parent::storageSettingsForm($form, $form_state, $has_data);
+    $storage_settings = $this->getSetting('storage_plugin_settings');
+    $base_table = $form_state->getValue(['settings', 'storage_plugin_settings', 'base_table']);
+
+    // Add an ajax callback so that when the base table is selected, the
+    // base column select can be populated.
+    $elements['storage_plugin_settings']['base_table']['#ajax'] = [
+      'callback' =>  [$this, 'storageSettingsFormAjaxCallback'],
+      'event' => 'change',
+      'progress' => [
+        'type' => 'throbber',
+        'message' => $this->t('Retrieving table columns...'),
+      ],
+      'wrapper' => 'edit-base_column',
+    ];
+
+    $base_columns = $this->getTableColumns($base_table, ['text', 'character varying']);
+    $elements['storage_plugin_settings']['base_column'] = [
+      '#type' => 'select',
+      '#title' => t('Table Column'),
+      '#description' => t('Select the column in the base table that contains the field data'),
+      '#options' => $base_columns,
+      '#default_value' => $this->getSetting('base_column') ?? '',
+      '#required' => TRUE,
+      '#disabled' => $has_data or !$base_table,
+      '#prefix' => '<div id="edit-base_column">',
+      '#suffix' => '</div>',
+    ];
+
+    return $elements;
+  }
+
+  /**
+   * Ajax callback to update the base column select. The select
+   * can't be populated until we know the base table.
+   *
+   * @param array $form
+   *   The form array.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The form state object.
+   */
+  public function storageSettingsFormAjaxCallback($form, &$form_state) {
+    $response = new AjaxResponse();
+    $response->addCommand(new ReplaceCommand('#edit-base_column', $form['settings']['storage_plugin_settings']['base_column']));
+    return $response;
+  }
+
 }
