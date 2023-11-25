39a8e23edc17094fca0d70f0992e9fcb1c6681a3
dsenalik <dsenalik@wisc.edu>
2023-11-25 10:32:12 -0600

    updated organism field

--- tripal_chado/src/Plugin/Field/FieldWidget/ChadoOrganismWidgetDefault.php
+++ tripal_chado/src/Plugin/Field/FieldWidget/ChadoOrganismWidgetDefault.php
@@ -2,18 +2,17 @@
 
 namespace Drupal\tripal_chado\Plugin\Field\FieldWidget;
 
-use Drupal\tripal\TripalField\TripalWidgetBase;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\tripal_chado\TripalField\ChadoWidgetBase;
 
 /**
- * Plugin implementation of default Tripal organism widget.
+ * Plugin implementation of default Chado organism widget.
  *
  * @FieldWidget(
  *   id = "chado_organism_widget_default",
- *   label = @Translation("Chado Organism Reference Widget"),
- *   description = @Translation("A chado organism reference widget"),
+ *   label = @Translation("Chado Organism Widget"),
+ *   description = @Translation("The default organism widget."),
  *   field_types = {
  *     "chado_organism_default"
  *   }
@@ -21,52 +20,93 @@ use Drupal\tripal_chado\TripalField\ChadoWidgetBase;
  */
 class ChadoOrganismWidgetDefault extends ChadoWidgetBase {
 
-
   /**
    * {@inheritdoc}
    */
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
 
-    // Get the list of organisms.
-    $organisms = [];
-    $chado = \Drupal::service('tripal_chado.database');
-    $query = $chado->select('organism', 'o');
-    $query->leftJoin('cvterm', 'cvt', 'o.type_id = cvt.cvterm_id');
-    $query->fields('o', ['organism_id', 'genus', 'species', 'infraspecific_name']);
-    $query->fields('cvt', ['name']);
-    $query->orderBy('genus');
-    $query->orderBy('species');
-    $results = $query->execute();
-
-    while ($organism = $results->fetchObject()) {
-      $org_name = $organism->genus . ' ' . $organism->species;
-
-      if ($organism->name) {
-        $org_name .= ' ' . $organism->name;
-      }
-      if ($organism->infraspecific_name) {
-        $org_name .= ' ' . $organism->infraspecific_name;
-      }
-      $organisms[$organism->organism_id] = $org_name;
-    }
+    // Get the list of organisms. Second parameter includes common names.
+    $organisms = chado_get_organism_select_options(FALSE, TRUE);
 
     $item_vals = $items[$delta]->getValue();
     $record_id = $item_vals['record_id'] ?? 0;
+    $linker_id = $item_vals['linker_id'] ?? 0;
+    $link = $item_vals['link'] ?? 0;
     $organism_id = $item_vals['organism_id'] ?? 0;
+    // If a linker table is used, values for additional columns that
+    // may or may not be present in that table.
+    $linker_type_id = $item_vals['linker_type_id'] ?? 1;
+    $linker_rank = $item_vals['linker_rank'] ?? $delta;
+    $linker_pub_id = $item_vals['linker_pub_id'] ?? 1;
 
     $elements = [];
     $elements['record_id'] = [
       '#type' => 'value',
       '#default_value' => $record_id,
     ];
+    $elements['linker_id'] = [
+      '#type' => 'value',
+      '#default_value' => $linker_id,
+    ];
+    $elements['link'] = [
+      '#type' => 'value',
+      '#default_value' => $link,
+    ];
     $elements['organism_id'] = $element + [
       '#type' => 'select',
       '#options' => $organisms,
       '#default_value' => $organism_id,
-      '#placeholder' => $this->getSetting('placeholder'),
       '#empty_option' => '-- Select --',
     ];
 
+    // For linker table columns that may or may not be present,
+    // it doesn't hurt to always include them, they will be ignored
+    // when not needed.
+    $elements['linker_type_id'] = [
+      '#type' => 'value',
+      '#default_value' => $linker_type_id,
+    ];
+    $elements['linker_rank'] = [
+      '#type' => 'value',
+      '#default_value' => $linker_rank,
+    ];
+    // e.g. cell_line_feature has pub_id with not null constraint
+    $elements['linker_pub_id'] = [
+      '#type' => 'value',
+      '#default_value' => $linker_pub_id,
+    ];
+
     return $elements;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function massageFormValues(array $values, array $form, FormStateInterface $form_state) {
+    // Handle any empty values.
+    foreach ($values as $val_key => $value) {
+      if ($value['organism_id'] == '') {
+        if ($value['record_id']) {
+          // If there is a record_id, but no organism_id, this means
+          // we need to pass in this record to chado storage to
+          // have the linker record be deleted there. To do this,
+          // we need to have the correct primitive type for this
+          // field, so change from empty string to zero.
+          $values[$val_key]['organism_id'] = 0;
+        }
+        else {
+          unset($values[$val_key]);
+        }
+      }
+    }
+
+    // Reset the weights
+    $i = 0;
+    foreach ($values as $val_key => $value) {
+      $values[$val_key]['_weight'] = $i;
+      $i++;
+    }
+
+    return $values;
   }
 }
