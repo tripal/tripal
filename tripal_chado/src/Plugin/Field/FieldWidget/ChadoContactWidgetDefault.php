3696e45a756762ae058aaca974a58f2baf1c59ae
dsenalik <dsenalik@wisc.edu>
2024-01-10 15:24:30 -0600

    Updates to Contact field

--- tripal_chado/src/Plugin/Field/FieldWidget/ChadoContactWidgetDefault.php
+++ tripal_chado/src/Plugin/Field/FieldWidget/ChadoContactWidgetDefault.php
@@ -25,6 +25,13 @@ class ChadoContactWidgetDefault extends ChadoWidgetBase {
    */
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
 
+    // Get the field settings.
+    $field_definition = $items[$delta]->getFieldDefinition();
+    $storage_settings = $field_definition->getSetting('storage_plugin_settings');
+    $linker_fkey_column = $storage_settings['linker_fkey_column']
+      ?? $storage_settings['base_column'] ?? 'contact_id';
+    $property_definitions = $items[$delta]->getFieldDefinition()->getFieldStorageDefinition()->getPropertyDefinitions();
+
     // Get the list of contacts.
     $contacts = [];
     $chado = \Drupal::service('tripal_chado.database');
@@ -51,12 +58,7 @@ class ChadoContactWidgetDefault extends ChadoWidgetBase {
     $record_id = $item_vals['record_id'] ?? 0;
     $linker_id = $item_vals['linker_id'] ?? 0;
     $link = $item_vals['link'] ?? 0;
-    $contact_id = $item_vals['contact_id'] ?? 0;
-    // If a linker table is used, values for additional columns that
-    // may or may not be present in that table.
-    $linker_type_id = $item_vals['linker_type_id'] ?? 1;
-    $linker_rank = $item_vals['linker_rank'] ?? $delta;
-    $linker_pub_id = $item_vals['linker_pub_id'] ?? 1;
+    $contact_id = $item_vals[$linker_fkey_column] ?? 0;
 
     $elements = [];
     $elements['record_id'] = [
@@ -71,29 +73,30 @@ class ChadoContactWidgetDefault extends ChadoWidgetBase {
       '#type' => 'value',
       '#default_value' => $link,
     ];
-    $elements['contact_id'] = $element + [
+    // pass the foreign key name through the form for massageFormValues()
+    $elements['linker_fkey_column'] = [
+      '#type' => 'value',
+      '#default_value' => $linker_fkey_column,
+    ];
+    $elements[$linker_fkey_column] = $element + [
       '#type' => 'select',
       '#options' => $contacts,
       '#default_value' => $contact_id,
       '#empty_option' => '-- Select --',
     ];
 
-    // For linker table columns that may or may not be present,
-    // it doesn't hurt to always include them, they will be ignored
-    // when not needed.
-    $elements['linker_type_id'] = [
-      '#type' => 'value',
-      '#default_value' => $linker_type_id,
-    ];
-    $elements['linker_rank'] = [
-      '#type' => 'value',
-      '#default_value' => $linker_rank,
-    ];
-    // e.g. cell_line_feature has pub_id with not null constraint
-    $elements['linker_pub_id'] = [
-      '#type' => 'value',
-      '#default_value' => $linker_pub_id,
-    ];
+    // If there are any additional columns present in the linker table,
+    // use a default of 1 which will work for type_id or rank.
+    // or pub_id. Any existing value will pass through as the default.
+    foreach ($property_definitions as $property => $definition) {
+      if (($property != 'linker_id') and preg_match('/^linker_/', $property)) {
+        $default_value = $item_vals[$property] ?? 1;
+        $elements[$property] = [
+          '#type' => 'value',
+          '#default_value' => $default_value,
+        ];
+      }
+    }
 
     return $elements;
   }
@@ -102,16 +105,19 @@ class ChadoContactWidgetDefault extends ChadoWidgetBase {
    * {@inheritDoc}
    */
   public function massageFormValues(array $values, array $form, FormStateInterface $form_state) {
+
     // Handle any empty values.
     foreach ($values as $val_key => $value) {
-      if ($value['contact_id'] == '') {
+      // Foreign key is usually contact_id, but not always.
+      $linker_fkey_column = $value['linker_fkey_column'];
+      if ($value[$linker_fkey_column] == '') {
         if ($value['record_id']) {
           // If there is a record_id, but no contact_id, this means
           // we need to pass in this record to chado storage to
           // have the linker record be deleted there. To do this,
           // we need to have the correct primitive type for this
           // field, so change from empty string to zero.
-          $values[$val_key]['contact_id'] = 0;
+          $values[$val_key][$linker_fkey_column] = 0;
         }
         else {
           unset($values[$val_key]);
