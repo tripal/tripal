faccacf6d57c12a7536c4523cdceed9564919da6
dsenalik <dsenalik@wisc.edu>
2024-01-10 14:33:24 -0600

    Add new Study field

---
+++ tripal_chado/src/Plugin/Field/FieldWidget/ChadoStudyWidgetDefault.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Drupal\tripal_chado\Plugin\Field\FieldWidget;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\tripal_chado\TripalField\ChadoWidgetBase;
+
+/**
+ * Plugin implementation of default Chado study widget.
+ *
+ * @FieldWidget(
+ *   id = "chado_study_widget_default",
+ *   label = @Translation("Chado Study Widget"),
+ *   description = @Translation("The default study widget."),
+ *   field_types = {
+ *     "chado_study_type_default"
+ *   }
+ * )
+ */
+class ChadoStudyWidgetDefault extends ChadoWidgetBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+
+    // Get the field settings.
+    $field_definition = $items[$delta]->getFieldDefinition();
+    $storage_settings = $field_definition->getSetting('storage_plugin_settings');
+    $linker_fkey_column = $storage_settings['linker_fkey_column']
+      ?? $storage_settings['base_column'] ?? 'study_id';
+    $property_definitions = $items[$delta]->getFieldDefinition()->getFieldStorageDefinition()->getPropertyDefinitions();
+
+    // Get the list of studies. Include contacts because that has a not null constraint.
+    $studys = [];
+    $chado = \Drupal::service('tripal_chado.database');
+    $query = $chado->select('study', 's');
+    $query->leftJoin('contact', 'c', 's.contact_id = c.contact_id');
+    $query->fields('s', ['study_id', 'name']);
+    $query->addField('c', 'name', 'contact_name');
+    $query->orderBy('name', 'contact_name');
+    $results = $query->execute();
+    while ($study = $results->fetchObject()) {
+      $study_name = $study->name;
+      if ($study->contact_name) {
+        $study_name .= ' (' . $study->contact_name . ')';
+      }
+      $studys[$study->study_id] = $study_name;
+    }
+    natcasesort($studys);
+
+    $item_vals = $items[$delta]->getValue();
+    $record_id = $item_vals['record_id'] ?? 0;
+    $linker_id = $item_vals['linker_id'] ?? 0;
+    $link = $item_vals['link'] ?? 0;
+    $study_id = $item_vals['study_id'] ?? 0;
+
+    $elements = [];
+    $elements['record_id'] = [
+      '#type' => 'value',
+      '#default_value' => $record_id,
+    ];
+    $elements['linker_id'] = [
+      '#type' => 'value',
+      '#default_value' => $linker_id,
+    ];
+    $elements['link'] = [
+      '#type' => 'value',
+      '#default_value' => $link,
+    ];
+    // pass the foreign key name through the form for massageFormValues()
+    $elements['linker_fkey_column'] = [
+      '#type' => 'value',
+      '#default_value' => $linker_fkey_column,
+    ];
+    $elements[$linker_fkey_column] = $element + [
+      '#type' => 'select',
+      '#options' => $studys,
+      '#default_value' => $study_id,
+      '#empty_option' => '-- Select --',
+    ];
+
+    // If there are any additional columns present in the linker table,
+    // use a default of 1 which will work for type_id or rank.
+    // or pub_id. Any existing value will pass through as the default.
+    foreach ($property_definitions as $property => $definition) {
+      if (($property != 'linker_id') and preg_match('/^linker_/', $property)) {
+        $default_value = $item_vals[$property] ?? 1;
+        $elements[$property] = [
+          '#type' => 'value',
+          '#default_value' => $default_value,
+        ];
+      }
+    }
+
+    return $elements;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function massageFormValues(array $values, array $form, FormStateInterface $form_state) {
+
+    // Handle any empty values.
+    foreach ($values as $val_key => $value) {
+      // Foreign key is usually study_id
+      $linker_fkey_column = $value['linker_fkey_column'];
+      if ($value[$linker_fkey_column] == '') {
+        if ($value['record_id']) {
+          // If there is a record_id, but no study_id, this means
+          // we need to pass in this record to chado storage to
+          // have the linker record be deleted there. To do this,
+          // we need to have the correct primitive type for this
+          // field, so change from empty string to zero.
+          $values[$val_key][$linker_fkey_column] = 0;
+        }
+        else {
+          unset($values[$val_key]);
+        }
+      }
+    }
+
+    // Reset the weights
+    $i = 0;
+    foreach ($values as $val_key => $value) {
+      $values[$val_key]['_weight'] = $i;
+      $i++;
+    }
+
+    return $values;
+  }
+}
