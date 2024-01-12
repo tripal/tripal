b580985d9b4105cb1d0acc1194aeed24b7098e84
dsenalik <dsenalik@wisc.edu>
2024-01-12 09:31:05 -0600

    alpha 2 typo fixes

--- tripal/src/Services/TripalEntityTypeCollection.php
+++ tripal/src/Services/TripalEntityTypeCollection.php
@@ -116,7 +116,7 @@ class TripalEntityTypeCollection implements ContainerInjectionInterface  {
         }
       }
       else {
-        throw new \Exception("Unable to retrieve the configuration with an id of $config_id using the assumption that its in the file $config_item.");
+        throw new \Exception("Unable to retrieve the configuration with an id of $config_id using the assumption that it's in the file $config_item.");
       }
     }
   }
