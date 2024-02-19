dd06abb38577b61fe83dff8d928ad539becfc495
Stephen Ficklin <spficklin@gmail.com>
2024-02-18 18:55:46 -0800

    Testing with organism content type

--- tripal_layout/src/Controller/TripalEntityUILayoutController.php
+++ tripal_layout/src/Controller/TripalEntityUILayoutController.php
@@ -2,56 +2,196 @@
 namespace Drupal\tripal_layout\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\Entity\EntityViewDisplay;
 
 class TripalEntityUILayoutController extends ControllerBase {
 
+
   /**
+   * A generic funtion for getting a settings for addDetailsFieldGroup().
    *
-   * @param unknown $tripal_entity_type
+   * @param string $name
+   *   The setting name
+   * @param unknown $default
+   *   The default value if no setting is set.
+   * @param array $settings
+   *   The list of settings.
+   *
    * @return unknown
    */
-  public function applyLayout($tripal_entity_type) {
-    $bundle_name = $tripal_entity_type->id();
-    $term = $tripal_entity_type->getTerm();
+  protected function getSetting($name, $default, $settings = []) {
+    $value = array_key_exists($name, $settings) ? $settings[$name] : $default;
 
-    // As a sanity check make sure we have a default view.
-    /** @var Drupal\Core\Entity\EntityDisplayRepository $displays **/
-    $displays = \Drupal::service('entity_display.repository');
-    $view_modes = $displays->getViewModeOptionsByBundle('tripal_entity', $bundle_name);
-    if (!in_array('default', array_keys($view_modes))) {
-      \Drupal::messenger()->addWarning(t('Cannot apply the layout. The default view mode is not present.'));
-      return $this->redirect('entity.entity_view_display.tripal_entity.default',
-          ['tripal_entity_type' => $bundle_name]);
-    }
+    return $value;
+  }
 
-    // Get the field manager, field definitions for the bundle type, and
-    // the field type manager.
-    /** @var \Drupal\Core\Entity\EntityFieldManager $field_manager **/
-    /** @var \Drupal\Core\Field\FieldTypePluginManager $field_type_manager **/
-    $field_manager = \Drupal::service('entity_field.manager');
-    $field_defs = $field_manager->getFieldDefinitions('tripal_entity', $bundle_name);
-    $field_type_manager = \Drupal::service('plugin.manager.field.field_type');
+  /**
+   *
+   * @param unknown $name
+   * @param unknown $parent
+   * @param EntityViewDisplay $display
+   */
+  protected function setChild($name, $parent, EntityViewDisplay $display) {
 
-    /** @var \Drupal\Core\Entity\EntityTypeManager $entity_type_manager **/
-    /** @var \Drupal\Core\Entity\Entity\EntityViewDisplay $entity_display **/
-    /** @var \Drupal\Core\Config\Entity\ConfigEntityStorage $config_entity_storage **/
-    $entity_type_manager = \Drupal::service('entity_type.manager');
-    $config_entity_storage = $entity_type_manager->getStorage('entity_view_display');
-    $entity_display = $config_entity_storage->load('tripal_entity.' . $bundle_name . '.default');
-    dpm($entity_display->getThirdPartySettings('field_group'));
-
-    // Iterate over the field definitions for the bundle and collect the
-    // information so we can use it later.
-    /** @var \Drupal\Core\Field\BaseFieldDefinition $field_definition **/
-    $field_definition = NULL;
-    foreach ($field_defs as $field_name => $field_definition) {
-      if (is_a($field_definition, 'Drupal\field\Entity\FieldConfig')) {
-
+    // First remove the component from where it may already be a child.
+    $field_groups = $display->getThirdPartySettings('field_group');
+    foreach ($field_groups as $group_name => $group_details) {
+      if (is_array($group_details['children'])) {
+        if (in_array($name, $group_details['children'])) {
+          unset($group_details['children'][$name]);
+          $display->setThirdPartySetting('field_group', $group_name, $group_details);
+        }
       }
     }
 
+    // Now add it where it needs to go as a child.
+    $parent_group = $display->getThirdPartySetting('field_group', $parent);
+    if (!in_array($child, $parent_group['children'])) {
+      $parent_group['children'][] = $name;
+    }
+    $display->setThirdPartySetting('field_group', $parent, $parent_group);
+  }
+
+
+  /**
+   *
+   * @param unknown $name
+   * @param EntityViewDisplay $dispaly
+   * @param array $settings
+   */
+  protected function addTableFieldGroup($name, EntityViewDisplay $display, $settings = []) {
+    $field_groups = $display->getThirdPartySettings('field_group');
+
+    // If the field group doesn't exist then add it.
+    if (!array_key_exists('name', array_keys($field_groups))) {
+
+      $classes = $this->getSetting('classes', '', $settings);
+      $classes .= 'tripal-layout-table';
+
+      $parent_name = $this->getSetting('parent_name', '', $settings);
+      $table_group = [
+        "children" => [],
+        "label" => $this->getSetting('label', 'Missing Label', $settings),
+        "parent_name" => $parent_name,
+        "region" => "content",
+        "weight" => $this->getSetting('weight', 0, $settings),
+        "format_type" => "field_group_table",
+        "format_settings" => [
+          "show_empty_fields" => $this->getSetting('show_empty', FALSE, $settings),
+          "label_visibility" => "1",
+          "desc" => "",
+          "desc_visibility" => "1",
+          "first_column" => "",
+          "second_column" => "",
+          "empty_label_behavior" => "1",
+          "table_row_striping" => "1",
+          "always_show_field_label" => "1",
+          "empty_field_placeholder" => "",
+          "id" => "",
+          "classes" => $classes,
+          "always_show_field_value" => 0,
+          "hide_table_if_empty" => 0
+        ]
+      ];
+      $display->setThirdPartySetting('field_group', $name, $table_group);
+      if ($parent_name !== "") {
+        $this->setChild($name, $parent_name, $display);
+      }
+    }
+  }
+
+  /**
+   * Adds a field group.
+   *
+   * @param string $name
+   *   The name of the field component
+   * @param EntityViewDisplay $display
+   *   The display configuration.
+   */
+  protected function addDetailsFieldGroup($name, EntityViewDisplay $display, $settings = []) {
+    $field_groups = $display->getThirdPartySettings('field_group');
+
+    // If the field group doesn't exist then add it.
+    if (!array_key_exists('name', array_keys($field_groups))) {
+
+      $classes = $this->getSetting('classes', '', $settings);
+      $classes .= 'tripal-layout-details';
+
+      $parent_name = $this->getSetting('parent_name', '', $settings);
+      $details_group = [
+        "children" => [],
+        "label" => $this->getSetting('label', 'Missing Label', $settings),
+        "parent_name" => $parent_name,
+        "region" => "content",
+        "weight" => $this->getSetting('weight', 0, $settings),
+        "format_type" => "details",
+        "format_settings" => [
+          "classes" => $classes,
+          "show_empty_fields" => $this->getSetting('show_empty', FALSE, $settings),
+          "id" => "",
+          "open" => $this->getSetting('open', FALSE, $settings),
+          "description" => "",
+        ],
+      ];
+      $display->setThirdPartySetting('field_group', $name, $details_group);
+      if ($parent_name !== "") {
+        $this->setChild($name, $parent_name, $display);
+      }
+    }
+  }
+
+  protected function addFieldGroupChild($name, EntityViewDisplay $display) {
+
+  }
+  /**
+   * Hides a field from display
+   *
+   * @param string $name
+   *   The name of the field component
+   * @param EntityViewDisplay $display
+   *   The display configuration.
+   */
+  protected function hideComponent($name, EntityViewDisplay $display) {
+    $components = $display->getComponents();
+    $hidden = $display->get('hidden');
+    if (in_array($name, array_keys($components))) {
+      $hidden[$name] = TRUE;
+      $display->set('hidden', $hidden);
+      $display->removeComponent($name);
+    }
+  }
+
+  /**
+   * Applies the default Tripal layout to a tripal entity.
+   *
+   * @param \Drupal\tripal\Entity\TripalEntityType $tripal_entity_type
+   */
+  public function applyLayout($tripal_entity_type) {
+
+    /** @var \Drupal\Core\Entity\EntityTypeManager $entity_type_manager **/
+    /** @var \Drupal\Core\Config\Entity\ConfigEntityStorage $config_entity_storage **/
+    /** @var \Drupal\Core\Entity\Entity\EntityViewDisplay $display **/
+    $bundle = $tripal_entity_type->id();
+    $bundle_label = $tripal_entity_type->getLabel();
+    $entity_type_manager = \Drupal::service('entity_type.manager');
+    $config_entity_storage = $entity_type_manager->getStorage('entity_view_display');
+    $display = $config_entity_storage->load('tripal_entity.' . $bundle . '.default');
+    dpm($display);
+
+    $this->hideComponent('title', $display);
+    $this->hideComponent('uid', $display);
+
+    $settings = ['label' => $bundle_label . ' Details', 'open' => TRUE];
+    $this->addDetailsFieldGroup('summary', $display, $settings);
+
+    $settings = ['label' => $bundle_label . ' Table', 'parent_name' => 'summary', 'open' => TRUE];
+    $this->addTableFieldGroup('summary_table', $display, $settings);
+    $this->setChild('organism_comment', 'summary_table', $display);
+
+    $display->save();
+
     return $this->redirect('entity.entity_view_display.tripal_entity.default',
-        ['tripal_entity_type' => $bundle_name]);
+        ['tripal_entity_type' => $bundle]);
   }
 
 }
