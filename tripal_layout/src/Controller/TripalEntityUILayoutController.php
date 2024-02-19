beb83a2c0e93853aa4ccdd6717a327f9de576073
Stephen Ficklin <spficklin@gmail.com>
2024-02-18 23:41:39 -0800

    Config file working and default organism layout working

--- tripal_layout/src/Controller/TripalEntityUILayoutController.php
+++ tripal_layout/src/Controller/TripalEntityUILayoutController.php
@@ -63,7 +63,7 @@ class TripalEntityUILayoutController extends ControllerBase {
     $field_groups = $display->getThirdPartySettings('field_group');
 
     // If the field group doesn't exist then add it.
-    if (!array_key_exists('name', array_keys($field_groups))) {
+    if (!array_key_exists($name, array_keys($field_groups))) {
 
       $classes = $this->getSetting('classes', '', $settings);
       $classes .= 'tripal-layout-table';
@@ -101,6 +101,27 @@ class TripalEntityUILayoutController extends ControllerBase {
   }
 
   /**
+   * Sets all fields for display.
+   *
+   * @param EntityViewDisplay $display
+   */
+  protected function unhideAll(EntityViewDisplay $display) {
+     $display->set('hidden', []);
+  }
+
+  /**
+   * Removes all field groups from the display
+   *
+   * @param EntityViewDisplay $display
+   */
+  protected function clearFieldGroups(EntityViewDisplay $display) {
+    $field_groups = $display->getThirdPartySettings('field_group');
+    foreach ($field_groups as $group_name => $group_details) {
+      $display->unsetThirdPartySetting('field_group', $group_name);
+    }
+  }
+
+  /**
    * Adds a field group.
    *
    * @param string $name
@@ -112,7 +133,7 @@ class TripalEntityUILayoutController extends ControllerBase {
     $field_groups = $display->getThirdPartySettings('field_group');
 
     // If the field group doesn't exist then add it.
-    if (!array_key_exists('name', array_keys($field_groups))) {
+    if (!array_key_exists($name, array_keys($field_groups))) {
 
       $classes = $this->getSetting('classes', '', $settings);
       $classes .= 'tripal-layout-details';
@@ -155,11 +176,80 @@ class TripalEntityUILayoutController extends ControllerBase {
     $components = $display->getComponents();
     $hidden = $display->get('hidden');
     if (in_array($name, array_keys($components))) {
-      $hidden[$name] = TRUE;
-      $display->set('hidden', $hidden);
       $display->removeComponent($name);
     }
+    $hidden[$name] = TRUE;
+    $display->set('hidden', $hidden);
   }
+
+  /**
+   * Retreives the layout configuration for the given bundle.
+   *
+   * @param string $bundle
+   * @return array
+   *   The layout configuration.
+   */
+  protected function getLayout($bundle) {
+    $bundle_layouts = [];
+    $config_factory = \Drupal::service('config.factory');
+    $config_list = $config_factory->listAll('tripal_layout.tripal_layout_default_view');
+
+    // Iterate throught the configuration files and find those that have
+    // a layout for this bundle.
+    foreach($config_list as $config_item) {
+      $config = $config_factory->get($config_item);
+      $layouts = $config->get('layouts');
+      foreach ($layouts as $layout) {
+        if ($layout['tripal_entity_type'] == $bundle) {
+          $bundle_layouts[$config_item] = [$layout];
+        }
+      }
+    }
+
+    if (count($layouts) == 0) {
+      \Drupal::messenger()->addWarning(t('No default layouts could be found for this content type.'));
+      return $this->redirect('entity.tripal_entity.field_ui_fields',
+          ['tripal_entity_type' => $bundle_name]);
+
+    }
+    if (count($layouts) > 1) {
+      \Drupal::messenger()->addWarning(t('There are multiple layouts for the same content type. '
+          . 'Selecting the first. @layouts'), ['@layouts' => print_r($layouts, TRUE)]);
+    }
+
+    return $layouts[0];
+  }
+
+
+  /**
+   * Removes all layout applied by this module.
+   *
+   * @param \Drupal\tripal\Entity\TripalEntityType $tripal_entity_type
+   */
+
+  public function resetLayout($tripal_entity_type) {
+    /** @var \Drupal\Core\Entity\EntityTypeManager $entity_type_manager **/
+    /** @var \Drupal\Core\Config\Entity\ConfigEntityStorage $config_entity_storage **/
+    /** @var \Drupal\Core\Entity\Entity\EntityViewDisplay $display **/
+    $bundle = $tripal_entity_type->id();
+    $bundle_label = $tripal_entity_type->getLabel();
+    $entity_type_manager = \Drupal::service('entity_type.manager');
+    $config_entity_storage = $entity_type_manager->getStorage('entity_view_display');
+    $display = $config_entity_storage->load('tripal_entity.' . $bundle . '.default');
+
+    // First reset the display.
+    $this->clearFieldGroups($display);
+    $this->unhideAll($display);
+
+    // Save all of the changes to the display.
+    $display->save();
+
+    \Drupal::messenger()->addMessage(t('Layout has been reset and saved.'));
+
+    return $this->redirect('entity.entity_view_display.tripal_entity.default',
+        ['tripal_entity_type' => $bundle]);
+  }
+
 
   /**
    * Applies the default Tripal layout to a tripal entity.
@@ -176,19 +266,51 @@ class TripalEntityUILayoutController extends ControllerBase {
     $entity_type_manager = \Drupal::service('entity_type.manager');
     $config_entity_storage = $entity_type_manager->getStorage('entity_view_display');
     $display = $config_entity_storage->load('tripal_entity.' . $bundle . '.default');
-    dpm($display);
 
-    $this->hideComponent('title', $display);
-    $this->hideComponent('uid', $display);
+    // First reset the display.
+    $this->clearFieldGroups($display);
+    $this->unhideAll($display);
 
-    $settings = ['label' => $bundle_label . ' Details', 'open' => TRUE];
-    $this->addDetailsFieldGroup('summary', $display, $settings);
+    // Get the layout for this bundle.
+    $layout = $this->getLayout($bundle);
 
-    $settings = ['label' => $bundle_label . ' Table', 'parent_name' => 'summary', 'open' => TRUE];
-    $this->addTableFieldGroup('summary_table', $display, $settings);
-    $this->setChild('organism_comment', 'summary_table', $display);
+    // If there are field group definitinos then create those.
+    if (array_key_exists('field_groups', $layout)) {
 
+      // First, create the field groups
+      foreach ($layout['field_groups'] as $group_type => $field_groups) {
+        if ($group_type == 'details') {
+          foreach ($field_groups as $group_name => $settings) {
+            $this->addDetailsFieldGroup($group_name, $display, $settings);
+          }
+        }
+        if ($group_type == 'field_group_table') {
+          foreach ($field_groups as $group_name => $settings) {
+            $this->addTableFieldGroup($group_name, $display, $settings);
+          }
+        }
+      }
+
+      // Now set the children.
+      foreach ($layout['field_groups'] as $group_type => $field_groups) {
+        foreach ($field_groups as $group_name => $settings) {
+          $children = $settings['children'];
+          foreach ($children as $child) {
+            $this->setChild($child, $group_name, $display);
+          }
+        }
+      }
+    }
+
+    // Now hide any fields that should be hidden.
+    foreach ($layout['hidden'] as $field_name) {
+      $this->hideComponent($field_name, $display);
+    }
+
+    // Save all of the changes to the display.
     $display->save();
+
+    \Drupal::messenger()->addMessage(t('The default Tripal layout has been set for this content type and saved.'));
 
     return $this->redirect('entity.entity_view_display.tripal_entity.default',
         ['tripal_entity_type' => $bundle]);
