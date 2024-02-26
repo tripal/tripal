ccd1e5e28db4fbf17b971b8f405ed7102039b511
Malladi <sm2699@xi19051258.sgc.loc>
2024-02-26 16:22:52 -0600

    Tripal tripal.tripalfield_collection.genomic_chado.yml for sequence_coordinates-field: tv4g1-issue1414-data__sequence_coordinates

--- tripal_chado/src/Plugin/Field/FieldType/ChadoSequenceCoordinatesDefault.php
+++ tripal_chado/src/Plugin/Field/FieldType/ChadoSequenceCoordinatesDefault.php
@@ -103,11 +103,7 @@ class ChadoSequenceCoordinatesDefault extends ChadoFieldItemBase {
 
     $properties[] = new ChadoIntStoragePropertyType($entity_type_id, self::$id, 'uniquename', $ft_uniqname_term, [
       'action' => 'store',
-<<<<<<< Updated upstream
       'path' => 'feature.feature_id>featureloc.feature_id;featureloc.srcfeature_id>feature.feature_id;uniquename',
-=======
-      'path' => 'feature.name',
->>>>>>> Stashed changes
     ]);
 
     $properties[] = new ChadoIntStoragePropertyType($entity_type_id, self::$id, 'fmin', $fmin_term, [
