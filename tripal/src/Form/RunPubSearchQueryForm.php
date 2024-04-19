cb223a5c20975ffe146ade897cd26e3b986ab7e4
Risharde Ramnath <risharde@gmail.com>
2024-04-18 20:32:02 -0400

    Remove pub search query execute form + Run Pub Search Query run button should now point directly to the loader url

--- tripal/src/Form/RunPubSearchQueryForm.php
+++ tripal/src/Form/RunPubSearchQueryForm.php
@@ -105,7 +105,7 @@ class RunPubSearchQueryForm extends FormBase {
           '#markup' => 
             Link::fromTextAndUrl(
               'Run', 
-              Url::fromUri('internal:/admin/tripal/loaders/publications/run_publication_search_query_execute/' . $pub_query->pub_library_query_id)
+              Url::fromUri('internal:/admin/tripal/loaders/chado_pub_search_query_loader/' . $pub_query->pub_library_query_id)
             )
             ->toString()
         ];
