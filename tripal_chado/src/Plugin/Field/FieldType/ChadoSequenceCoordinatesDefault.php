0225ae4cbfe458dba9034c7840a8add765ec398a
Douglas Senalik <dsenalik@wisc.edu>
2024-04-26 13:43:49 -0500

    can now return if no base table

--- tripal_chado/src/Plugin/Field/FieldType/ChadoSequenceCoordinatesDefault.php
+++ tripal_chado/src/Plugin/Field/FieldType/ChadoSequenceCoordinatesDefault.php
@@ -60,14 +60,10 @@ class ChadoSequenceCoordinatesDefault extends ChadoFieldItemBase {
     $field_settings = $field_definition->getSetting('storage_plugin_settings');
     $base_table = $field_settings['base_table'];
 
+    // If we don't have a base table then we're not ready to specify the
+    // properties for this field.
     if (!$base_table) {
-      $record_id_term = 'data:3002';
-      return [
-        new ChadoIntStoragePropertyType($entity_type_id, self::$id, 'record_id', $record_id_term, [
-          'action' => 'store_id',
-          'drupal_store' => TRUE,
-        ])
-      ];
+      return;
     }
 
     // Get the property terms by using the Chado table columns they map to.
