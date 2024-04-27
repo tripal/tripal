75636836d7e465433460e5013d05393b6254fd32
Lacey Sanderson <laceyannesanderson@gmail.com>
2024-04-27 15:18:10 -0600

    Default in formatter if not configured in widget yet.

--- tripal/src/Plugin/Field/FieldFormatter/DefaultTripalTextTypeFormatter.php
+++ tripal/src/Plugin/Field/FieldFormatter/DefaultTripalTextTypeFormatter.php
@@ -26,6 +26,9 @@ class DefaultTripalTextTypeFormatter extends TripalFormatterBase {
   public function viewElements(FieldItemListInterface $items, $langcode) {
     $elements = [];
 
+    // Default filter format.
+    $filter_format = 'basic_html';
+
     // We need to get the format set in the widget settings
     // because they need to match.
     $entity_type = $this->fieldDefinition->get('entity_type');
@@ -33,7 +36,9 @@ class DefaultTripalTextTypeFormatter extends TripalFormatterBase {
     $field_name = $this->fieldDefinition->get('field_name');
     $form_display = \Drupal::service('entity_display.repository')->getFormDisplay($entity_type, $bundle);
     $widget = $form_display->getComponent($field_name);
-    $filter_format = $widget['settings']['filter_format'];
+    if (array_key_exists('filter_format', $widget['settings'])) {
+      $filter_format = $widget['settings']['filter_format'];
+    }
 
     foreach($items as $delta => $item) {
       $value_string = $item->get('value')->getValue();
