fac979b6013c8875849977aaad93b4ca059b8ed2
Stephen Ficklin <spficklin@gmail.com>
2024-06-17 10:06:41 -0700

    Update tripal_chado/tests/src/Functional/Services/ChadoTripalPublishTest.php
    Co-authored-by: Lacey-Anne Sanderson <lacey.sanderson@usask.ca>

--- tripal_chado/tests/src/Functional/Services/ChadoTripalPublishTest.php
+++ tripal_chado/tests/src/Functional/Services/ChadoTripalPublishTest.php
@@ -483,7 +483,7 @@ class ChadoTripalPublishTest extends ChadoTestBrowserBase {
     // entity in those returned, but not the gorilla organism.
     $this->assertEquals('<em>Oryza species</em> subspecies <em>Japonica</em>', array_values($entities)[0],
       'The Oryza species subspecies Japonica organism should appear in the published list because it has new properties.');
-    $this->assertTrue(count(array_values($entities)) == 1,
+    $this->assertCount(1, array_values($entities),
       'There should only be one published entity for a single organism with new properties.');
 
     // Check that the property values got published.  The type_id should be
