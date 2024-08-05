cc6ebaca489a135928a2909c691e476b9b5e23ea
Risharde Ramnath <risharde@gmail.com>
2024-08-04 21:27:23 -0400

    PubImporter View custom field files

--- tripal_chado/tests/src/Functional/Plugin/PubSearchQueryImporterTest.php
+++ tripal_chado/tests/src/Functional/Plugin/PubSearchQueryImporterTest.php
@@ -26,26 +26,30 @@ class PubSearchQueryImporterTest extends ChadoTestBrowserBase
     // Installs up the chado with the test chado data
     $chado = $this->getTestSchema(ChadoTestBrowserBase::PREPARE_TEST_CHADO);
 
+    // $this->installSchema('tripal', ['tripal_pub_library_query']);
+
     // Keep track of the schema name in case we need it
     $schema_name = $chado->getSchemaName();
 
     // We need to add a publication query to the database
-    $sql = "INSERT INTO tripal_pub_library_query (name,criteria) VALUES (:name,:criteria);";
+    $sql = "INSERT INTO {1:tripal_pub_library_query} (name,criteria) VALUES (:name,:criteria);";
     $args = [
       ':name' => 'Populus',
       ':criteria' => 'a:9:{s:9:"remote_db";s:6:"pubmed";s:12:"num_criteria";s:1:"1";s:11:"loader_name";s:7:"Populus";s:8:"disabled";i:0;s:10:"do_contact";i:0;s:13:"pub_import_id";N;s:8:"criteria";a:1:{i:1;a:4:{s:12:"search_terms";s:7:"Populus";s:5:"scope";s:5:"title";s:9:"is_phrase";i:0;s:9:"operation";s:0:"";}}s:21:"form_state_user_input";a:12:{s:9:"plugin_id";s:25:"tripal_pub_library_pubmed";s:11:"button_next";s:4:"Next";s:11:"loader_name";s:7:"Populus";s:12:"ncbi_api_key";s:0:"";s:4:"days";s:0:"";s:12:"num_criteria";s:1:"1";s:5:"table";a:1:{i:1;a:4:{s:11:"operation-1";s:0:"";s:7:"scope-1";s:5:"title";s:14:"search_terms-1";s:7:"Populus";s:11:"is_phrase-1";N;}}s:13:"form_build_id";s:48:"form-UpjBwJmfHyqAeLFwZqHbVhpvtgcBvgEez31-4KJ9jUA";s:10:"form_token";s:43:"FIxhzP6k7V1ruQoEoDzVCKVOt97wfbvGypPBPGFx13M";s:7:"form_id";s:31:"chado_new_pub_search_query_form";s:8:"disabled";N;s:10:"do_contact";N;}s:4:"days";s:0:"";}'
     ];
     $public->query($sql,$args);
 
-    $results = $public->query("SELECT * FROM tripal_pub_library_query WHERE name = 'Populus';");
+    $results = $public->query("SELECT * FROM {1:tripal_pub_library_query} WHERE name = 'Populus';");
     $query_id = NULL;
     foreach ($results as $row) {
-      $query_id = $row['pub_library_query_id'];
+      $query_id = $row->pub_library_query_id;
     }
     print_r('Query ID: ' . $query_id . "\n");
 
     $pub_library_manager = \Drupal::service('tripal.pub_library');
-    $pub_record = $pub_library_manager->getSearchQuery($query_id);
+    $pub_record = $pub_library_manager->getSearchQuery(intval($query_id));
+    print_r("Pub Record:\n");
+    print_r($pub_record);
 
     $criteria = unserialize($pub_record->criteria);
     $plugin_id = $criteria['form_state_user_input']['plugin_id'];
