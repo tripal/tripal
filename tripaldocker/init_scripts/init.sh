4d2b7b734526512a1d2881731de53a7eef92a404
Lacey-Anne Sanderson <las166@mail.usask.ca>
2024-08-10 17:16:16 -0600

    Get supervisord working again!

--- tripaldocker/init_scripts/init.sh
+++ tripaldocker/init_scripts/init.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
+
 supervisord -c /etc/supervisord.conf
+
+## Now handled by Supervisord:
+## Start PostgreSQL
+# service postgresql start
+## Start Apache
+# apache2ctl -D FOREGROUND
