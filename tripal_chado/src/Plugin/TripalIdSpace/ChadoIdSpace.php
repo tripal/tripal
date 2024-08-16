5efc08e52d74d71ec8d276e7dc37c76c1c16d102
Douglas Senalik <dsenalik@wisc.edu>
2024-08-16 10:40:14 -0500

    revert case change

--- tripal_chado/src/Plugin/TripalIdSpace/ChadoIdSpace.php
+++ tripal_chado/src/Plugin/TripalIdSpace/ChadoIdSpace.php
@@ -421,7 +421,7 @@ class ChadoIdSpace extends TripalIdSpaceBase implements ContainerFactoryPluginIn
       $query1->condition('CVT.name', $name, '=');
     }
     else {
-      $query1->condition('CVT.name', $this->connection->escapeLike($name) . '%', 'like');
+      $query1->condition('CVT.name', $this->connection->escapeLike($name) . '%', 'LIKE');
     }
     $results = $query1->execute();
     while ($cvterm = $results->fetchObject()) {
