0a3144d0060af51a2984725e0af8f1a7d68bbf78
Lacey-Anne Sanderson <las166@mail.usask.ca>
2024-09-07 20:28:28 +0000

    Start testing validation of the settings form.

--- tripal/tests/src/Kernel/TripalField/FieldType/TripalFieldTypeSettingsTest.php
+++ tripal/tests/src/Kernel/TripalField/FieldType/TripalFieldTypeSettingsTest.php
@@ -36,7 +36,7 @@ class TripalFieldTypeSettingsTest extends TripalTestKernelBase {
     $senarios =  [];
 
     // BOOLEAN
-    $senarios[] = [
+    $senarios['boolean'] = [
       'field_info' => [
         'field_type_id' => 'tripal_boolean_type',
         'field_type_class' => 'Drupal\tripal\Plugin\Field\FieldType\TripalBooleanTypeItem',
@@ -49,8 +49,8 @@ class TripalFieldTypeSettingsTest extends TripalTestKernelBase {
     ];
 
     // INTEGER
-    $senarios[] = [
-      'type' => [
+    $senarios['integer'] = [
+      'field_info' => [
         'field_type_id' => 'tripal_integer_type',
         'field_type_class' => 'Drupal\tripal\Plugin\Field\FieldType\TripalIntegerTypeItem',
         'widget_id' => 'default_tripal_integer_type_widget',
@@ -62,8 +62,8 @@ class TripalFieldTypeSettingsTest extends TripalTestKernelBase {
     ];
 
     // STRING
-    $senarios[] = [
-      'type' => [
+    $senarios['string'] = [
+      'field_info' => [
         'field_type_id' => 'tripal_string_type',
         'field_type_class' => 'Drupal\tripal\Plugin\Field\FieldType\TripalStringTypeItem',
         'widget_id' => 'default_tripal_string_type_widget',
@@ -82,8 +82,8 @@ class TripalFieldTypeSettingsTest extends TripalTestKernelBase {
     ];
 
     // TEXT
-    $senarios[] = [
-      'type' =>[
+    $senarios['text'] = [
+      'field_info' =>[
         'field_type_id' => 'tripal_text_type',
         'field_type_class' => 'Drupal\tripal\Plugin\Field\FieldType\TripalTextTypeItem',
         'widget_id' => 'default_tripal_text_type_widget',
@@ -98,11 +98,11 @@ class TripalFieldTypeSettingsTest extends TripalTestKernelBase {
   }
 
   /**
-   * This method tests that we can create an entity with this field.
+   * This method tests that we can build the storage settings subform.
    *
    * @dataProvider provideFieldsToTest
    */
-  public function testStorageSettingsForm($field_info, $expectations) {
+  public function testStorageSettingsFormBuild($field_info, $expectations) {
 
     // Setup the field to be tested based on the data provider values.
     $field_name = $this->randomMachineName();
@@ -171,5 +171,101 @@ class TripalFieldTypeSettingsTest extends TripalTestKernelBase {
           "The " . $field_info['field_type_class'] . "::storageSettingsForm() form element $element_identifier does not have the expected value.");
       }
     }
+  }
+
+  /**
+   * Provides a collection of settings to test across all our fields.
+   *
+   * NOTE: Uses the provideFieldsToTest data provider as the source of the
+   * fields to be tested and then combines them with settings sets.
+   *
+   * @return array
+   *   An array of field by settings combinations to ensure we fully test the
+   *   settings forms.
+   */
+  public function provideFieldsWithSettings() {
+
+    $fields = $this->provideFieldsToTest();
+    $senarios = [];
+
+    // @todo loop through all fields with general settings.
+
+    // FIELD SPECIFIC SETTINGS
+    // -- String Field
+    $field = $fields['string']['field_info'];
+
+    $senarios['max_length_empty'] = [
+      'field_info' => $field,
+      'senario' => [
+        'storage_settings' => [
+          'submitted_values' => [
+            'max_length' => 0,
+          ],
+          'expectations' => [
+
+          ],
+        ],
+      ],
+    ];
+
+    return $senarios;
+  }
+
+  /**
+   * This method tests that we can build the storage settings subform.
+   *
+   * @dataProvider provideFieldsWithSettings
+   */
+  public function testStorageSettingsFormValidate($field_info, $senario) {
+
+    // We only care about the storage settings form
+    // so let's limit the senario to that.
+    $senario = $senario['storage_settings'];
+
+    // Setup the field to be tested based on the data provider values.
+    $field_name = $this->randomMachineName();
+    $fieldConfig = $this->createFieldInstance(
+      'tripal_entity',
+      [
+        'field_name' => $field_name,
+        'field_type' => $field_info['field_type_id'],
+        'formatter_id' => $field_info['formatter_id'],
+      ]
+    );
+
+    // Build the form using the Drupal form builder.
+    $formBuilder = \Drupal\field_ui\Form\FieldStorageConfigEditForm::create($this->container);
+    $formBuilder->setEntity($this->fieldStorage);
+    $form_state = new \Drupal\Core\Form\FormState();
+    $form_state->set('field_config', $fieldConfig);
+    $form_state->set('entity_type_id', 'tripal_entity');
+    $form_state->set('bundle', $this->TripalEntityType->getID());
+    $form = $formBuilder->form([], $form_state);
+    $this->assertIsArray(
+      $form,
+      'We were not able to build the field storage settings form.'
+    );
+
+    // Now set our settings in the form state as though the admin had.
+    foreach($senario['submitted_values'] as $element_key => $value) {
+      $form_state->set($element_key, $value);
+    }
+    // And then submit for validation.
+    $formBuilder->submitForm($form, $form_state);
+    /*
+    $this->assertTrue(
+      $form_state->isValidationComplete(),
+      "We expect the form state to have been updated to indicate that validation is complete."
+    );
+    */
+
+    // Looking for form validation errors
+    $form_validation_messages = $form_state->getErrors();
+    print_r($form_validation_messages);
+
+    //   Looking for drupal message errors.
+    $messages = \Drupal::messenger()->all();
+    print_r($messages);
+
   }
 }
