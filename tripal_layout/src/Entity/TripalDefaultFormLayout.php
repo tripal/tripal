f76375f5db90634a8158997c4e3cb3c519312b71
Lacey Sanderson <laceyannesanderson@gmail.com>
2024-09-23 14:45:14 -0600

    Initialize the form layout entity.

---
+++ tripal_layout/src/Entity/TripalDefaultFormLayout.php
@@ -0,0 +1,75 @@
+<?php
+namespace Drupal\tripal_layout\Entity;
+
+use Drupal\Core\Config\Entity\ConfigEntityBase;
+
+/**
+ * Defines the Default Form entity controlling the form layout.
+ *
+ * @ConfigEntityType(
+ *   id = "tripal_layout_default_form",
+ *   label = @Translation("Tripal Default Form Layout"),
+ *   handlers = {
+ *     "list_builder" = "Drupal\tripal_layout\ListBuilders\TripalDefaultFormLayoutListBuilder",
+ *     "form" = {
+ *       "delete" = "Drupal\tripal_layout\Form\TripalDefaultFormLayoutDeleteForm",
+ *     }
+ *   },
+ *   config_prefix = "tripal_layout_default_form",
+ *   admin_permission = "administer tripal",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "label",
+ *   },
+ *   config_export = {
+ *     "id",
+ *     "label",
+ *     "description",
+ *     "layouts"
+ *   },
+ *   links = {
+ *     "delete-form" = "/admin/tripal/config/tripal-layout-default-form/{tripal_layout_default_form}/delete",
+ *     "layouts" = "/admin/tripal/config/tripal-layout-default-form"
+ *   }
+ * )
+ */
+class TripalDefaultFormLayout extends ConfigEntityBase {
+
+  /**
+   * A unique ID for this form layout entity.
+   *
+   * @var string
+   */
+  protected $id;
+
+  /**
+   * A label to provide to the admin identifying this collection of form layouts.
+   *
+   * @var string
+   */
+  protected $label;
+
+  /**
+   * A description to provide to the admin describing this collection.
+   *
+   * @var string
+   */
+  protected $description;
+
+
+  /**
+   * The collection of form layouts itself directly from the YAML.
+   *
+   * @var array
+   */
+  protected $layouts;
+
+  /**
+   * Retrieves the description of this form layout collection.
+   *
+   * @return string
+   */
+  public function description() {
+    return $this->description;
+  }
+}
---
+++ tripal_layout/src/Form/TripalDefaultFormLayoutDeleteForm.php
@@ -0,0 +1,53 @@
+<?php
+namespace Drupal\tripal_layout\Form;
+
+use Drupal\Core\Entity\EntityConfirmFormBase;
+use Drupal\Core\Url;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Builds the form to delete an TripalDefaultFormLayout Config Entity.
+ */
+class TripalDefaultFormLayoutDeleteForm extends EntityConfirmFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getQuestion() {
+    return $this->t('Are you sure you want to delete %name?', ['%name' => $this->entity->label()]);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDescription() {
+    return $this->t('You are deleting an entire configuration that specifies how ' .
+        'content types forms should be organized by default. Rebuild the cache to re-import ' .
+        'the default settings of this configuration.');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCancelUrl() {
+    return new Url('entity.tripal_layout_default_form.layouts');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConfirmText() {
+    return $this->t('Delete');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->entity->delete();
+    $this->messenger()->addMessage($this->t('The %label Tripal Default Form Layout has been deleted.', ['%label' => $this->entity->label()]));
+
+    $form_state->setRedirectUrl($this->getCancelUrl());
+  }
+
+}
