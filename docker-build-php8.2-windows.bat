4443fbdf0327ef2fbc1b5297082737d7a0eae9dd
Lacey Sanderson <laceyannesanderson@gmail.com>
2024-10-05 23:09:54 +0000

    Remove windows dockerfiles as they shouldn't be needed anymore.

--- docker-build-php8.2-windows.bat
+++
@@ -1,2 +0,0 @@
-docker builder prune -f
-docker build --progress=plain --no-cache -f Dockerfile-php8.2-windows -t tripalproject/tripaldocker:latest .
--- docker-build-php8.3-windows.bat
+++
@@ -1,2 +0,0 @@
-docker builder prune -f
-docker build --progress=plain --no-cache -f Dockerfile-php8.3-windows -t tripalproject/tripaldocker:latest .
