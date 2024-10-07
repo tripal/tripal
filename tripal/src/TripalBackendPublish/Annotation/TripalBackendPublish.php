e66c926edc2dd3ec1859258609d21064cab21e15
Lacey-Anne Sanderson <las166@mail.usask.ca>
2024-10-07 19:02:31 +0000

    Add some basic annotation.

--- tripal/src/TripalBackendPublish/Annotation/TripalBackendPublish.php
+++ tripal/src/TripalBackendPublish/Annotation/TripalBackendPublish.php
@@ -11,4 +11,32 @@ use Drupal\Component\Annotation\Plugin;
  */
 class TripalBackendPublish extends Plugin {
 
+
+  /**
+   * The plugin ID.
+   *
+   * @var string
+   */
+  public $id;
+
+  /**
+   * The label of the plugin.
+   *
+   * @var \Drupal\Core\Annotation\Translation
+   *
+   * @ingroup plugin_translatable
+   */
+  public $label;
+
+  /**
+   * A brief description for this plugin.
+   *
+   * This description will be presented to the site user.
+   *
+   * @var \Drupal\Core\Annotation\Translation
+   *
+   * @ingroup plugin_translatable
+   */
+  public $description;
+
 }
