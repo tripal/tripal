3f43484f775457c0abbf3a197439f00425482521
Douglas Senalik <dsenalik@wisc.edu>
2024-10-19 14:51:33 -0500

    support alternate tokens

--- tripal/src/Services/TripalCitationManager.php
+++ tripal/src/Services/TripalCitationManager.php
@@ -13,25 +13,28 @@ class TripalCitationManager {
    * @param array $publication
    *   An associative array defining the publication properties
    * @param string $format
-   *   String defining fields and order to generate the citation.
-   *   Tokens may be nested to indicate prefix or suffix that are only
-   *   added if the token has a value. For example, a journal may not have issue numbers.
-   *   Example format: '{{Authors}.}{ {Title}.}{ {Year}.}{ {Journal}}{ {Volume}}{({Issue})}{: {Pages}.}'
+   *   A token string defining fields used to generate the citation.
+   *   Tokens are text enclosed in curly braces, e.g. "{Title}"
+   *   Tokens may be "doubled" inside another set of curly braces to
+   *   indicate a prefix or suffix that is only added if the token has
+   *   a value. For example, a journal may not have issue numbers.
+   *   Thus, for "{ {Volume}}{({Issue})}{:{Pages}.}", if there is
+   *   no issue number, then the parentheses will not be included.
    */
-  public static function generateCitation(array $publication, string $format) {
+  public function generateCitation(array $publication, string $format) {
     $citation = $format;
 
-    // First match double tokens e.g. {({issue})} or {{title}.}
+    // Match double tokens e.g. {({issue})} or { {title}.}
     if (preg_match_all('/\{[^\{\}]*\{[^\}]+\}[^\}]*\}/', $format, $matches)) {
-      foreach ($matches[0] as $token) {
+      foreach ($matches[0] as $token_string) {
         // separate into prefix, key, suffix
-        preg_match('/\{([^\{\}]*)\{([^\}]+)\}([^\}]*)\}/', $token, $submatches);
+        preg_match('/\{([^\{\}]*)\{([^\}]+)\}([^\}]*)\}/', $token_string, $submatches);
         $prefix = $submatches[1];
-        $key = $submatches[2];
+        $key = $this->firstMatchedToken($submatches[2], $publication);
         $suffix = $submatches[3];
         $value = $publication[$key] ?? '';
-        // If prefix or suffix are already part of the string, then omit them,
-        // e.g. title already ends in period and token is {{title}.}
+        // If prefix or suffix are already part of the value string, then
+        // omit them, e.g. title already ends in period and token is "{ {Title}.}"
         if (strlen($value)) {
           if (strlen($prefix) and substr($value, 0, strlen($prefix)) == $prefix) {
             $prefix = '';
@@ -41,18 +44,75 @@ class TripalCitationManager {
           }
           $value = $prefix . $value . $suffix;
         }
-        $citation = str_replace($token, $value, $citation);
+        $citation = str_replace($token_string, $value, $citation);
       }
     }
-    // Now match single tokens, e.g. {Authors}
+
+    // Match any remaining single tokens, e.g. {Authors}
     if (preg_match_all('/\{[^\{\}]+\}/', $format, $matches)) {
-      foreach ($matches[0] as $token) {
-        $key = substr($token, 1, strlen($token)-2);
+      foreach ($matches[0] as $token_string) {
+        $key = substr($token_string, 1, strlen($token_string)-2);
+        $key = $this->firstMatchedToken($key, $publication);
         $value = $publication[$key] ?? '';
-        $citation = str_replace($token, $value, $citation);
+        $citation = str_replace($token_string, $value, $citation);
       }
     }
 
     return $citation;
+  }
+
+  /**
+   * Determine the first defined token in a '|' delimited string of tokens
+   *
+   * @param string $token_string
+   *   One or more tokens delimited by "|"
+   * @param array $values
+   *   Associative array of key value pairs where keys correspond to the tokens.
+   *
+   * @return string
+   *   The first matching token that is defined as a key in $values.
+   *   Returns an empty string if none of the tokens are defined.
+   */
+  protected function firstMatchedToken(string $token_string, array $values): string {
+    $tokens = explode('|', $token_string);
+    foreach ($tokens as $token) {
+      if (array_key_exists($token, $values)) {
+        return $token;
+      }
+    }
+    return '';
+  }
+
+  /**
+   * Returns a token string suitable for citation generation
+   * of the specified publication type
+   *
+   * @param string $pub_type
+   *   The publication type.
+   *
+   */
+  public function getCitationTemplate(string $pub_type) {
+    $templates = [
+      'default' =>
+        '{{Authors}.}{ {Title}.}{ {Publication Date|Year}.}{ {Journal Name|Journal Abbreviation|Series Name|Series Abbreviation}}{ {Volume}}{({Issue})}{:{Pages}.}',
+      // These five templates implement equivalent citations as done by Tripal 3
+      // as found in tripal_chado/api/modules/tripal_chado.pub.api.inc
+      'Journal Article' =>
+        '{{Authors}.}{ {Title}.}{ {Publication Date|Year}.}{ {Journal Name|Journal Abbreviation|Series Name|Series Abbreviation}}{ {Volume}}{({Issue})}{:{Pages}.}',
+      'Review' =>
+        '{{Authors}.}{ {Title}.}{ {Journal Name|Journal Abbreviation|Series Name|Series Abbreviation}}{ {Publisher}.}{ {Publication Date|Year}.}{ {Volume}}{({Issue})}{:{Pages}.}',
+      "Research Support, Non-U.S. Gov't" =>
+        '{{Authors}.}{ {Title}.}{ {Journal Name}}{ {Publication Date|Year}.}',
+      'Letter' =>
+        '{{Authors}.}{ {Title}.}{ {Journal Name|Journal Abbreviation|Series Name|Series Abbreviation}}{ {Publication Date|Year}.}{ {Volume}}{({Issue})}{:{Pages}.}',
+      'Conference Proceedings' =>
+        '{{Authors}.}{ {Title}.}{ {Conference Name|Series Name|Series Abbreviation}}{ {Publication Date|Year}.}{ {Volume}}{({Issue})}{:{Pages}.}',
+    ];
+    if (array_key_exists($pub_type, $templates)) {
+      return $templates[$pub_type];
+    }
+    else {
+      return $templates['default'];
+    }
   }
 }
--- tripal/tests/src/Kernel/Services/TripalCitation/TripalCitationTest.php
+++ tripal/tests/src/Kernel/Services/TripalCitation/TripalCitationTest.php
@@ -35,20 +35,32 @@ class TripalCitationTest extends TripalTestKernelBase {
     $pub1 = [
       'Title' => 'Some impressive publication',
       'Journal Name' => 'Journal of Science',
+      'Series Name' => 'The Journal of Science',
       'Year' => '2024',
       'Volume' => '123',
       'Issue' => '4',
       'Pages' => '10-20',
       'Authors' => 'Chado, A.B.; Drupal, B.C.; Tripal, C.D.',
     ];
-    $format1 = '{{Authors}.}{ {Title}.}{ {Year}.}{ {Journal}}{ {Volume}}{({Issue})}{: {Pages}.}{^{bogus}$}';
-    $c1 = $citation_service->generateCitation($pub1, $format1);
-    $this->assertEquals('Chado, A.B.; Drupal, B.C.; Tripal, C.D. Some impressive publication. 2024. 123(4): 10-20.', $c1, 'Citation 1 is not as expected'); //@@@
+    // An unknown publication type should return the default template
+    $format1 = $citation_service->getCitationTemplate('undefined_type');
+    $this->assertEquals('{{Authors}.}{ {Title}.}{ {Publication Date|Year}.}{ {Journal Name|Journal Abbreviation|Series Name|Series Abbreviation}}{ {Volume}}{({Issue})}{:{Pages}.}',
+      $format1, 'Format 1 is not the default citation template');
 
-    $pub2 = $pub1;
-    unset($pub2['Issue']);
-    $c2 = $citation_service->generateCitation($pub2, $format1);
-    $this->assertEquals('Chado, A.B.; Drupal, B.C.; Tripal, C.D. Some impressive publication. 2024. 123: 10-20.', $c2, 'Citation 2 is not as expected'); //@@@
+    // Test a journal article
+    $format2 = $citation_service->getCitationTemplate('Journal Article');
+    $c2 = $citation_service->generateCitation($pub1, $format2);
+    $this->assertEquals('Chado, A.B.; Drupal, B.C.; Tripal, C.D. Some impressive publication. 2024. Journal of Science 123(4):10-20.',
+      $c2, 'Citation 2 is not the expected value');
+
+    // Test a review, with missing issue value, and missing Journal Name, use fallback Series Name
+    $pub3 = $pub1;
+    unset($pub3['Issue']);
+    unset($pub3['Journal Name']);
+    $format3 = $citation_service->getCitationTemplate('Review');
+    $c3 = $citation_service->generateCitation($pub3, $format3);
+    $this->assertEquals('Chado, A.B.; Drupal, B.C.; Tripal, C.D. Some impressive publication. The Journal of Science 2024. 123:10-20.',
+      $c3, 'Citation 3 is not the expected value');
   }
 
 }
