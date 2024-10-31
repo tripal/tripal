87dad25f9afe147733e8fda464409970f017de1b
Lacey Sanderson <laceyannesanderson@gmail.com>
2024-10-31 21:20:31 +0000

    Update Tripal Entity form to match node form better.

--- tripal/src/Form/TripalEntityForm.php
+++ tripal/src/Form/TripalEntityForm.php
@@ -23,10 +23,82 @@ class TripalEntityForm extends ContentEntityForm {
     $form = parent::buildForm($form, $form_state);
     $entity = $this->entity;
 
+    $form['#theme'] = 'tripal_entity_edit_form';
+
+    // Drupal only adds this to content forms if they support revisions but we want it in general.
+    if (!isset($form['advanced'])) {
+      $form['advanced'] = [
+        '#type' => 'vertical_tabs',
+        '#weight' => 99,
+      ];
+    }
+    $form['#attached']['library'] = ['node/drupal.node'];
+    // -- Additional collapsed regions can be added to this group by creating
+    //    a field group of type "Details Siderbar" and adding fields to it.
+    // For now we will add a metadata section to provide basic context info.
+    $form['advanced']['#attributes']['class'][] = 'entity-meta';
+    $form['meta'] = $this->buildMetadataFormElement($entity);
+    // -- If the theme being used is claro we want to add it's specific brand
+    // of styling. This is what it does in claro_form_node_form_alter
+    $form['#attached']['library'][] = 'claro/form-two-columns';
+    $form['advanced']['#type'] = 'container';
+    $form['advanced']['#accordion'] = TRUE;
+    $form['meta']['#type'] = 'container';
+
+    // We also want to disable the title to ensure it uses the token system.
     $form['title']['#disabled'] = TRUE;
     $form['title']['widget'][0]['value']['#description'] .= ' The title will be automatically updated based on the title format defined by administrators.';
 
     return $form;
+  }
+
+  protected function buildMetadataFormElement($entity) {
+    $element = [
+      '#type' => 'details',
+      '#group' => 'advanced',
+      '#weight' => -10,
+      '#title' => $this->t('Status'),
+      '#attributes' => [
+        'class' => [
+          'entity-meta__header',
+        ],
+      ],
+      '#tree' => TRUE,
+    ];
+    $element['published'] = [
+      '#type' => 'item',
+      '#markup' => $entity->isPublished() ? $this->t('Published') : $this->t('Not published'),
+      '#access' => !$entity->isNew(),
+      '#wrapper_attributes' => [
+        'class' => [
+          'entity-meta__title',
+        ],
+      ],
+    ];
+    $element['changed'] = [
+      '#type' => 'item',
+      '#title' => $this->t('Last saved'),
+      '#markup' => !$entity->isNew() ? $this->dateFormatter
+        ->format($entity->getChangedTime(), 'short') : $this->t('Not saved yet'),
+      '#wrapper_attributes' => [
+        'class' => [
+          'entity-meta__last-saved',
+        ],
+      ],
+    ];
+    $element['author'] = [
+      '#type' => 'item',
+      '#title' => $this->t('Author'),
+      '#markup' => $entity->getOwner()
+        ->getAccountName(),
+      '#wrapper_attributes' => [
+        'class' => [
+          'entity-meta__author',
+        ],
+      ],
+    ];
+
+    return $element;
   }
 
   /**
