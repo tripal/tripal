2d0e0724d365188deb3f257450a131b01a86a0ff
Douglas Senalik <dsenalik@wisc.edu>
2024-11-14 08:03:26 -0600

    helper script to find correct phpunit.xml

---
+++ tripaldocker/select_phpunit.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Select the appropriate version of the phpunit.xml
+# file to use. Drupal 10.x uses an earlier version.
+
+drupalversion=$(drush core-status --field='Drupal version')
+phpunit_config="/var/www/drupal/web/modules/contrib/tripal/phpunit.xml"
+if $(dpkg --compare-versions "${drupalversion}" "lt" "10.6"); then
+  phpunit_config="/var/www/drupal/web/modules/contrib/tripal/tripaldocker/phpunit.9.6.xml"
+fi
+
+echo -n "$phpunit_config"
