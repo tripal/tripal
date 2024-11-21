fbacb157654047bb270e5c1de76c41f3cf9c8fb1
Sean Buehler <ferrisx4@gmail.com>
2024-11-21 18:23:02 +0000

    initial attempt at deprecating tripaldbx.settings

--- tripal_chado/src/Database/ChadoConnection.php
+++ tripal_chado/src/Database/ChadoConnection.php
@@ -248,9 +248,9 @@ class ChadoConnection extends TripalDbxConnection {
     // Get test schema prefix. If none set, we use '0' so we can test the prefix
     // and avoid false-positive since no schema name is allowed to start by a
     // number.
-    $test_prefixes = \Drupal::config('tripaldbx.settings')
+    $test_prefixes = \Drupal::config('tripal_chado.settings')
       ->get('test_schema_base_names', '0')
-    ;
+    ; // PUMPKIN
     // Get default schema name.
     $default_schema_name = \Drupal::config('tripal_chado.settings')
       ->get('default_schema')
@@ -328,7 +328,7 @@ class ChadoConnection extends TripalDbxConnection {
    */
   public static function removeAllTestSchemas() :void {
     // Get Chado test schema prefix.
-    $test_schema = \Drupal::config('tripaldbx.settings')
+    $test_schema = \Drupal::config('tripal.settings')
       ->get('test_schema_base_names', [])['chado']
     ;
     // Remove all matching schemas.
