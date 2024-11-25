fc93ab6b147ff1281e03300c6d3ae72555183057
Douglas Senalik <dsenalik@wisc.edu>
2024-11-25 16:01:32 -0600

    replace parsing with token parser service

--- tripal/src/Services/TripalCitationManager.php
+++ tripal/src/Services/TripalCitationManager.php
@@ -17,7 +17,7 @@ class TripalCitationManager {
   /**
    * TripalCitationManager constructor.
    *
-   * @param  \Drupal\tripal\Services\TripalTokenParser
+   * @param \Drupal\tripal\Services\TripalTokenParser
    *   The token parser service.
    */
   public function __construct(TripalTokenParser $token_parser) {
@@ -27,8 +27,6 @@ class TripalCitationManager {
   /**
    * Generate citation
    *
-   * @param array $publication
-   *   An associative array defining the publication properties
    * @param string $format
    *   A token string defining fields used to generate the citation.
    *   Tokens are text enclosed in square brackets, e.g. "[Title]"
@@ -37,67 +35,12 @@ class TripalCitationManager {
    *   a value. For example, a journal may not have issue numbers.
    *   Thus, for "[ [Volume]][([Issue])][:[Pages].]", if there is
    *   no issue number, then the parentheses will not be included.
-   */
-  public function generateCitation(array $publication, string $format) {
-    $citation = $format;
-
-    // Match double tokens e.g. {({issue})} or { {title}.}
-    if (preg_match_all('/\[[^\[\]]*\[[^\]]+\][^\]]*\]/', $format, $matches)) {
-      foreach ($matches[0] as $token_string) {
-        // separate into prefix, key, suffix
-        preg_match('/\[([^\[\]]*)\[([^\]]+)\]([^\]]*)\]/', $token_string, $submatches);
-        $prefix = $submatches[1];
-        $key = $this->firstMatchedToken($submatches[2], $publication);
-        $suffix = $submatches[3];
-        $value = $publication[$key] ?? '';
-        // If prefix or suffix are already part of the value string, then
-        // omit them, e.g. title already ends in period and token is "{ {Title}.}"
-        if (strlen($value)) {
-          if (strlen($prefix) and substr($value, 0, strlen($prefix)) == $prefix) {
-            $prefix = '';
-          }
-          if (strlen($suffix) and substr($value, -strlen($suffix)) == $suffix) {
-            $suffix = '';
-          }
-          $value = $prefix . $value . $suffix;
-        }
-        $citation = str_replace($token_string, $value, $citation);
-      }
-    }
-
-    // Match any remaining single tokens, e.g. [Authors]
-    if (preg_match_all('/\[[^\[\]]+\]/', $format, $matches)) {
-      foreach ($matches[0] as $token_string) {
-        $key = substr($token_string, 1, strlen($token_string)-2);
-        $key = $this->firstMatchedToken($key, $publication);
-        $value = $publication[$key] ?? '';
-        $citation = str_replace($token_string, $value, $citation);
-      }
-    }
-
-    return $citation;
-  }
-
-  /**
-   * Determine the first defined token in a '|' delimited string of tokens
-   *
-   * @param string $token_string
-   *   One or more tokens delimited by "|"
    * @param array $values
-   *   Associative array of key value pairs where keys correspond to the tokens.
-   *
-   * @return string
-   *   The first matching token that is defined as a key in $values.
-   *   Returns an empty string if none of the tokens are defined.
+   *   An associative array defining the publication properties
    */
-  protected function firstMatchedToken(string $token_string, array $values): string {
-    $tokens = explode('|', $token_string);
-    foreach ($tokens as $token) {
-      if (array_key_exists($token, $values)) {
-        return $token;
-      }
-    }
-    return '';
+  public function generateCitation(string $format, array $values) {
+    $citation = $this->token_parser($format, $values);
+    return $citation;
   }
 
   /**
