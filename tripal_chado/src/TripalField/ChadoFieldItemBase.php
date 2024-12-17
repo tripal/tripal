fb0715a3c8a248ea8b206acc67ccd1fe281a795f
Sean Buehler <ferrisx4@gmail.com>
2024-12-17 15:15:06 +0000

    make this function a class function,  a class var

--- tripal_chado/src/TripalField/ChadoFieldItemBase.php
+++ tripal_chado/src/TripalField/ChadoFieldItemBase.php
@@ -40,6 +40,9 @@ abstract class ChadoFieldItemBase extends TripalFieldItemBase {
   protected static $chadostorage_namespace = 'Drupal\tripal_chado\Plugin\TripalStorage\ChadoStorage';
   protected static $drupal_entity_callback = 'drupalEntityIdLookupCallback';
 
+  // The term mapping as defined by Chado
+  protected static $mapping = NULL;
+
   /**
    * {@inheritdoc}
    */
@@ -750,5 +753,32 @@ abstract class ChadoFieldItemBase extends TripalFieldItemBase {
     if (isset($fields[$machine_name])) {
       $fields[$machine_name]->delete();
     }
+  }
+
+  /**
+   *  Get the column's term ID.
+   * 
+   * @param string $table
+   *   The table name.
+   * 
+   * @param string column
+   *   The column name.
+   * 
+   * @param string $default_term
+   *   The default term to use.
+   */
+  public static function getColumnTermId(string $table, string $column, string $default_term): string {
+    $id = $default_term;
+    if (is_null(self::$mapping)) {
+      $storage = \Drupal::entityTypeManager()->getStorage('chado_term_mapping');
+      self::$mapping = $storage->load('core_mapping');
+    }
+    if (self::$mapping) {
+      $id = $mapping->getColumnTermId($table, $column);
+      if (!$id) {
+        $id = $default_term;
+      }
+    }
+    return $id;
   }
 }
