2dab970c515fc93734cf0fca66eb8d950930d55a
dsenalik <dsenalik@wisc.edu>
2024-12-31 17:18:23 -0600

    move functional project autocomplete tests to kernel generic autocomplete

--- tripal_chado/tests/src/Kernel/Controller/ChadoGenericAutocompleteControllerTest.php
+++ tripal_chado/tests/src/Kernel/Controller/ChadoGenericAutocompleteControllerTest.php
@@ -21,6 +21,10 @@ class ChadoGenericAutocompleteControllerTest extends ChadoTestKernelBase {
 
   protected $connection;
 
+  protected array $types;
+
+  protected array $project_ids;
+
   /**
    * {@inheritdoc}
    */
@@ -33,22 +37,61 @@ class ChadoGenericAutocompleteControllerTest extends ChadoTestKernelBase {
     // Open connection to a test Chado
     $this->connection = $this->getTestSchema(ChadoTestKernelBase::PREPARE_TEST_CHADO);
 
-    // Create a small number of projects.
-    for ($i=1; $i <= 3; $i++) {
-      $this->connection->insert('1:project')
-        ->fields([
-          'name' => 'Project No. ' . $i,
-          'description' => 'Description No. ' . $i,
-        ])->execute();
+    // Look up a few cvterms
+    $terms = ['null', 'Genome Project', 'Book', 'Proceedings Article'];
+    foreach ($terms as $term) {
+      $this->types[$term] = $this->connection
+        ->select('1:cvterm', 'T')
+        ->fields('T', ['cvterm_id'])
+        ->condition('T.name', $term, '=')
+        ->execute()
+        ->fetchField();
     }
 
-    // Create a large number of pubs.
-    for ($i=1; $i <= 51; $i++) {
+    // Create a small number of projects with some various type_id values
+    // Set type_id of null on projects: Good, Winner and Great.
+    $projects = [
+      'Project Okay' => ['type_id' => $this->types['Genome Project']],
+      'Project Good' => ['type_id' => $this->types['null']],
+      'Project Best' => ['type_id' => $this->types['Genome Project']],
+      'Project Winner' => ['type_id' => $this->types['null']],
+      'Project Super' => ['type_id' => $this->types['Genome Project']],
+      'Project Great' => ['type_id' => $this->types['null']],
+      'Project Green' => ['type_id' => $this->types['Genome Project']],
+      'Awesome Project' => ['type_id' => $this->types['Genome Project']],
+      'Wow Project' => ['type_id' => $this->types['Genome Project']],
+      'Yes Project' => ['type_id' => NULL],
+    ];
+
+    // Create test project records with type_id set for all but the last one
+    foreach ($projects as $name => $options) {
+      $description = $name . ' description';
+      $project_id = $this->connection
+        ->insert('1:project')
+        ->fields(['name' => $name, 'description' => $description])
+        ->execute();
+      $this->project_ids[$name] = $project_id;
+
+      if (!is_null($options['type_id'])) {
+        $select = $this->connection
+          ->insert('1:projectprop')
+          ->fields(['project_id' => $project_id, 'type_id' => $options['type_id'], 'value' => 'test type_id'])
+          ->execute();
+        // Add another random property to make sure having multiple properties is okay
+        $select = $this->connection
+          ->insert('1:projectprop')
+          ->fields(['project_id' => $project_id, 'type_id' => 2, 'value' => 'random property'])
+          ->execute();
+      }
+    }
+
+    // Create a large number of pubs of two types
+    for ($i=1; $i <= 101; $i++) {
       $this->connection->insert('1:pub')
         ->fields([
           'title' => 'Publication No. ' . $i,
           'uniquename' => 'ID' . $i,
-          'type_id' => (($i % 20) + 1),
+          'type_id' => (($i % 2) == 0) ? $this->types['Book'] : $this->types['Proceedings Article'],
         ])->execute();
     }
   }
@@ -79,32 +122,61 @@ class ChadoGenericAutocompleteControllerTest extends ChadoTestKernelBase {
     $results = $autocomplete->handleAutocomplete($request, 'project', 'name', 'project', 10, 0)->getContent();
     $this->assertCount(0, json_decode($results), 'Expected no results from "xxx" project generic autocomplete');
 
-    // Test for an internal string
+    // Test for an internal string, matches "Great" and "Green"
     $request = Request::create(
       'chado/generic/autocomplete/project/name/project/10/0',
       'GET',
-      ['q' => 'rojec']
+      ['q' => 'gre']
     );
     $results = $autocomplete->handleAutocomplete($request, 'project', 'name', 'project', 10, 0)->getContent();
-    $this->assertCount(3, json_decode($results), 'Expected exactly three results from "rojec" project generic autocomplete');
+    $this->assertCount(2, json_decode($results), 'Expected exactly two results from "gre" project generic autocomplete');
+
+    // Make sure both of these have the expected pkey id embedded
+    foreach (json_decode($results) as $result) {
+      $value = $result->value;
+      $title = preg_replace('/ \(\d+\)$/', '', $value);
+      $expected_id = $this->project_ids[$title] ?? -1;
+      $id = $autocomplete->getPkeyId($value);
+      $this->assertGreaterThan(0, $expected_id, 'Failed determining expected project_id from ' . $value);
+      $this->assertIsInt($id, 'Expected integer from getPkeyId()');
+      $this->assertGreaterThan(0, $id, 'Expected a positive integer from getPkeyId() operating on ' . $value);
+      $this->assertEquals($expected_id, $id, 'Retrieved id did not match expected value for ' . $value);
+    }
+
+    // Test getPkeyId on a string without embedded pkey id
+    $id = $autocomplete->getPkeyId('This is missing an embedded value');
+    $this->assertEquals(0, $id, 'getPkeyId() should return zero when there is no embedded id in parentheses');
+    $id = $autocomplete->getPkeyId('');
+    $this->assertEquals(0, $id, 'getPkeyId() should return zero for an empty string');
 
     // Test count limit
     $request = Request::create(
-      'chado/generic/autocomplete/analysis/name/pub/10/0',
+      'chado/generic/autocomplete/pub/title/pub/10/0',
       'GET',
       ['q' => 'p']
     );
     $results = $autocomplete->handleAutocomplete($request, 'pub', 'title', 'pub', 10, 0)->getContent();
     $this->assertCount(10, json_decode($results), 'Expected exactly 10 results from pub generic autocomplete');
 
-    // Test limiting by type_id=2
+    // Test limiting by type_id for a table with a type_id column
+    $type_id = $this->types['Proceedings Article'];
     $request = Request::create(
-      'chado/generic/autocomplete/analysis/name/pub/10/2',
+      'chado/generic/autocomplete/pub/title/pub/1000/' . $type_id,
       'GET',
       ['q' => 'p']
     );
-    $results = $autocomplete->handleAutocomplete($request, 'pub', 'title', 'pub', 10, 2)->getContent();
-    $this->assertCount(3, json_decode($results), 'Expected exactly 3 results from pub generic autocomplete type_id=2');
+    $results = $autocomplete->handleAutocomplete($request, 'pub', 'title', 'pub', 1000, $type_id)->getContent();
+    $this->assertCount(51, json_decode($results), 'Expected exactly 51 results from pub generic autocomplete for Proceedings Article containing "p"');
+
+    // Test limiting by type_id for a table with a type_id in a property table
+    $type_id = $this->types['Genome Project'];
+    $request = Request::create(
+      'chado/generic/autocomplete/project/name/projectprop/1000/' . $type_id,
+      'GET',
+      ['q' => 'w']
+    );
+    $results = $autocomplete->handleAutocomplete($request, 'project', 'name', 'projectprop', 1000, $type_id)->getContent();
+    $this->assertCount(2, json_decode($results), 'Expected exactly 2 results from project generic autocomplete for Genome Project containing "w"');
 
   }
 }
