e7d5c79bb581278810e0c51709b71d58ef071b76
Lacey Sanderson <laceyannesanderson@gmail.com>
2025-01-22 22:47:54 +0000

    Ensure field has settings for view mode before adding them.

--- tripal/src/Services/TripalFieldCollection.php
+++ tripal/src/Services/TripalFieldCollection.php
@@ -660,16 +660,18 @@ class TripalFieldCollection implements ContainerInjectionInterface  {
         $bundle_id = $entity_type->id();
         $view_modes = $entity_display->getViewModeOptionsByBundle('tripal_entity', $bundle_id);
         foreach (array_keys($view_modes) as $view_mode) {
+          $view_mode_options = $field_def['display']['view'][$view_mode] ?? [];
           \Drupal::service('entity_display.repository')
             ->getViewDisplay('tripal_entity', $bundle_id, $view_mode)
-            ->setComponent($field_def['name'], $field_def['display']['view'][$view_mode])
+            ->setComponent($field_def['name'], $view_mode_options)
             ->save();
         }
         $form_modes = $entity_display->getFormModeOptionsByBundle('tripal_entity', $bundle_id);
         foreach (array_keys($form_modes) as $form_mode) {
+          $form_mode_options = $field_def['display']['form'][$form_mode] ?? [];
           \Drupal::service('entity_display.repository')
             ->getFormDisplay('tripal_entity', $bundle_id, $form_mode)
-            ->setComponent($field_def['name'], $field_def['display']['form'][$form_mode])
+            ->setComponent($field_def['name'], $form_mode_options)
             ->save();
         }
 
