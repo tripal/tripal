c2c379c5968333efc8eca37189c77611efe80490
Douglas Senalik <dsenalik@wisc.edu>
2025-01-24 11:10:56 -0600

    Term for generic "stock" field is "Germplasm"
    Co-authored-by: Lacey-Anne Sanderson <lacey.sanderson@usask.ca>

--- tripal_chado/src/Plugin/Field/FieldType/ChadoStockTypeDefault.php
+++ tripal_chado/src/Plugin/Field/FieldType/ChadoStockTypeDefault.php
@@ -342,7 +342,7 @@ class ChadoStockTypeDefault extends ChadoFieldItemBase {
     $options += [
       'id' => self::$id,
       'table' => self::$object_table,
-      'label' => 'Stock',
+      'label' => 'Germplasm',
       'termIdSpace' => 'NCIT',
       'termAccession' => 'C70699',
       'description' => 'Any material sample taken from a biological entity for testing, diagnostic, propagation, treatment or research purposes, including a sample obtained from a living organism or taken from the biological object after halting of all its life functions.',
