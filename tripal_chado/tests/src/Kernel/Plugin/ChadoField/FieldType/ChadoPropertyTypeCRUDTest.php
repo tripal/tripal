5c72c868f70c88fe1f197eb023ad1b7bca1b4bc0
Lacey-Anne Sanderson <las166@mail.usask.ca>
2025-02-21 20:09:28 +0000

    Setup the test.

--- tripal_chado/tests/src/Kernel/Plugin/ChadoField/FieldType/ChadoPropertyTypeCRUDTest.php
+++ tripal_chado/tests/src/Kernel/Plugin/ChadoField/FieldType/ChadoPropertyTypeCRUDTest.php
@@ -20,11 +20,25 @@ use Drupal\Core\Form\FormState;
  * @group ChadoField
  */
 class ChadoPropertyTypeCRUDTest extends ChadoTestKernelBase {
-  protected $defaultTheme = 'stark';
-
-  protected static $modules = ['system', 'user', 'path', 'path_alias', 'field', 'tripal', 'tripal_chado'];
 
   use ChadoFieldTestTrait;
+
+  /**
+   * The theme to use when rendering content in the test environment.
+   *
+   * @var string
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * The modules that this test depends on.
+   *
+   * NOTE: since this is a kernel test, these modules are not being installed
+   * but are available to be installed.
+   *
+   * @var array
+   */
+  protected static $modules = ['system', 'user', 'path', 'path_alias', 'field', 'tripal', 'tripal_chado'];
 
   /**
    * The test chado connection. It is also set in the container.
@@ -33,4 +47,75 @@ class ChadoPropertyTypeCRUDTest extends ChadoTestKernelBase {
    */
   protected object $chado_connection;
 
+  /**
+   * The YAML file indicating the scenarios to test and how to setup the enviro.
+   *
+   * @var string
+   */
+  protected string $yaml_info_file = __DIR__ . '/ChadoPropertyType-TestInfo.yml';
+
+  /**
+   * Describes the environment to setup for this test.
+   *
+   * @var array
+   *   An array with the following keys:
+   *   - chado_version: the version of chado to test under.
+   *   - bundle: an array defining the tripal entity type to create.
+   *   - fields: a list of fields to be attached the above bundle.
+   */
+  protected array $system_under_test;
+
+  /**
+   * Describes the scenarios to test.
+   *
+   * This will be used in combination with the data provider. It can't be
+   * accessed directly in the dataProvider due to the way that PHPUnit is
+   * setup.
+   *
+   * @var array
+   *  A list of scenarios where each one has the following keys:
+   *  - label: A human-readable label for the scenario to be used in assert
+   *    messages.
+   *  - descrition: A description of the scenario and what you are wanting to
+   *    test. This will not be used in the test but is rather there to help
+   *    people reading the YAML file and to make it easier to maintain.
+   *  - create: An array of the values to be provided when creating a
+   *    TripalEntity. There should be a key matching the name of each field in
+   *    the system-under-test and it's value should be an array containing all
+   *    the property types for that field mapped to a value.
+   *  - edit: An array of the values to be provided when updating an existing
+   *    TripalEntity. There should be a key matching the name of each field in
+   *    the system-under-test and it's value should be an array containing all
+   *    the property types for that field mapped to a value.
+   */
+  protected array $scenarios;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    // First retrieve info from the YAML file for this particular test.
+    [$this->system_under_test, $this->scenarios] = $this->getTestInfoFromYaml($this->yaml_info_file);
+
+    // Create the test Chado installation we will be using.
+    if (!array_key_exists('chado_version', $this->system_under_test)) {
+      $this->system_under_test['chado_version'] = '1.3';
+    }
+    $this->chado_connection = $this->getTestSchema(
+      ChadoTestKernelBase::PREPARE_TEST_CHADO,
+      $this->system_under_test['chado_version']
+    );
+
+    // Next setup the environment according to the system under test.
+    $this->setupFieldTestEnvironment($this->system_under_test);
+  }
+
+  /**
+   * Tests the ChadoPropertyType field through TripalEntity->save().
+   */
+  public function testCRUD() {
+
+  }
 }
