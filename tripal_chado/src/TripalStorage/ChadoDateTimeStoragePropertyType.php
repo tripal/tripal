3a513077b0e5a6b3567e31407b751c6c283730a3
Lacey-Anne Sanderson <las166@mail.usask.ca>
2025-02-21 19:33:44 +0000

    Get Chado-specific property types to set the chado_id_space plugin id.

--- tripal_chado/src/TripalStorage/ChadoDateTimeStoragePropertyType.php
+++ tripal_chado/src/TripalStorage/ChadoDateTimeStoragePropertyType.php
@@ -9,4 +9,22 @@ use Drupal\tripal\TripalStorage\DateTimeStoragePropertyType;
  */
 class ChadoDateTimeStoragePropertyType extends DateTimeStoragePropertyType {
 
+  /**
+   * Constructs a new date time tripal storage property type.
+   *
+   * @param string entityType
+   *   The entity type associated with this property type.
+   * @param string fieldType
+   *   The field type associated with this property type.
+   * @param string key
+   *   The key associated with this property type.
+   * @param string term_id
+   *   The controlled vocabulary term asssociated with this property. It must be
+   *   in the form of "IdSpace:Accession" (e.g. "rdfs:label" or "OBI:0100026")
+   * @param array storage_settings
+   *   An array of settings required for this property by the storage backend.
+   */
+  public function __construct($entityType, $fieldType, $key, $term_id, $storage_settings = []) {
+    parent::__construct($entityType, $fieldType, $key, $term_id, $storage_settings, 'chado_id_space');
+  }
 }
