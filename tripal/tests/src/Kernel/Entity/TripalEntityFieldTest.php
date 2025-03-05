d8b4bc68ffc307b20f5a9447364dbcd6203b387e
Lacey-Anne Sanderson <las166@mail.usask.ca>
2025-03-05 23:04:39 +0000

    Map out more tests specific to URL alias.

--- tripal/tests/src/Kernel/Entity/TripalEntityFieldTest.php
+++ tripal/tests/src/Kernel/Entity/TripalEntityFieldTest.php
@@ -130,7 +130,7 @@ class TripalEntityFieldTest extends TripalTestKernelBase {
    * @param string $current_scenario_label
    *   The label of the scenario in the YAML.
    */
-  public function testTripalEntitySaveUrlAlias(int $current_scenario_key, string $current_scenario_label) {
+  public function testTripalEntitySaveTitleUrlBasic(int $current_scenario_key, string $current_scenario_label) {
     $current_scenario = $this->scenarios[$current_scenario_key];
     $this->assertEquals($current_scenario_label, $current_scenario['label'], "We may not have retrieved the expected scenario as the labels did not match.");
 
---
+++ tripal/tests/src/Kernel/Entity/TripalEntityUrlAliasTest.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace Drupal\Tests\tripal\Kernel\Entity;
+
+use Drupal\field\Entity\FieldStorageConfig;
+use Drupal\field\Entity\FieldConfig;
+use Drupal\Tests\tripal\Kernel\TripalTestKernelBase;
+use Drupal\Tests\tripal\Traits\TripalEntityFieldTestTrait;
+use Drupal\tripal\Entity\TripalEntity;
+use Drupal\tripal\Entity\TripalEntityType;
+
+/**
+ * Tests the TripalEntity URL alias system specifically.
+ *
+ * @group TripalEntity
+ * @group TripalTokenParser
+ */
+class TripalEntityUrlAliasTest extends TripalTestKernelBase {
+
+  use TripalEntityFieldTestTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = ['system', 'user', 'path', 'path_alias', 'field', 'datetime', 'tripal'];
+
+  /**
+   * The test drupal connection. It is also set in the container.
+   *
+   * @var object
+   */
+  protected object $drupal_connection;
+
+  /**
+   * The YAML file indicating the scenarios to test and how to setup the enviro.
+   *
+   * @var string
+   */
+  protected string $yaml_info_file = __DIR__ . '/TripalEntityUrlAliasTest-TestInfo.yml';
+
+  /**
+   * Describes the environment to setup for this test.
+   *
+   * @var array
+   *   An array with the following keys:
+   *   - bundle: an array defining the tripal entity type to create.
+   *   - fields: a list of fields to be attached the above bundle.
+   */
+  protected array $system_under_test;
+
+  /**
+   * The TripalEntityType id of the bundle being used in this test.
+   *
+   * @var string
+   */
+  protected string $bundle_name;
+
+  /**
+   * Describes the scenarios to test.
+   *
+   * This will be used in combination with the data provider. It can't be
+   * accessed directly in the dataProvider due to the way that PHPUnit is
+   * setup.
+   *
+   * @var array
+   *  A list of scenarios where each one has the following keys:
+   *  - label: A human-readable label for the scenario to be used in assert
+   *    messages.
+   *  - descrition: A description of the scenario and what you are wanting to
+   *    test. This will not be used in the test but is rather there to help
+   *    people reading the YAML file and to make it easier to maintain.
+   *  - create: An array of the values to be provided when creating a
+   *    TripalEntity. There should be a key matching the name of each field in
+   *    the system-under-test and it's value should be an array containing all
+   *    the property types for that field mapped to a value.
+   *  - edit: An array of the values to be provided when updating an existing
+   *    TripalEntity. There should be a key matching the name of each field in
+   *    the system-under-test and it's value should be an array containing all
+   *    the property types for that field mapped to a value.
+   */
+  protected array $scenarios;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    // The Drupal connection will be created in the parent. This is used
+    // when checking the Drupal field tables.
+    $this->drupal_connection = $this->container->get('database');
+
+    // First retrieve info from the YAML file for this particular test.
+    [$this->system_under_test, $this->scenarios] = $this->getTestInfoFromYaml($this->yaml_info_file);
+    $this->bundle_name = $this->system_under_test['bundle']['id'];
+
+    $this->setupEntityFieldTestEnvironment($this->system_under_test);
+  }
+
+  /**
+   * Data Provider: works with the YAML to provide scenarios for testing.
+   *
+   * @return array
+   *   List of scenarios to test where each one matches a key and label in the
+   *   associated YAML scenarios.
+   */
+  public static function provideScenarios() {
+    $scenarios = [];
+
+    $labels = [
+      "Use format for URL alias (multi-word)",
+      "Use format for URL alias (optional value)",
+      "Use format for URL (duplicate already exists)",
+      "Use form element value on create for URL alias",
+      "Use form element value on update for URL alias",
+      "Use form element value for URL alias (duplicate already exists)",
+      "Provided URL alias is not valid",
+    ];
+    foreach ($labels as $key => $label) {
+      $scenarios[] = [$key, $label];
+    }
+
+    return $scenarios;
+  }
+
+  /**
+   * Tests that TripalEntity::save() handles URL alias' with substitutions.
+   *
+   * @dataProvider provideScenarios
+   *
+   * @param int $current_scenario_key
+   *   The key of the scenario in the YAML.
+   * @param string $current_scenario_label
+   *   The label of the scenario in the YAML.
+   */
+  public function testTripalEntitySaveUrlAlias(int $current_scenario_key, string $current_scenario_label) {
+    $current_scenario = $this->scenarios[$current_scenario_key];
+    $this->assertEquals($current_scenario_label, $current_scenario['label'], "We may not have retrieved the expected scenario as the labels did not match.");
+
+  }
+}
