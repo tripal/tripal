2735a0bf0665e2d7610c44d7d3d04edfcbafd2be
Lacey-Anne Sanderson <las166@mail.usask.ca>
2025-03-14 02:21:32 +0000

    Add logic to loadValues and buildChadoRecords to skip fields that are 100% cached by Dupal.

--- tripal_chado/src/Plugin/TripalStorage/ChadoStorage.php
+++ tripal_chado/src/Plugin/TripalStorage/ChadoStorage.php
@@ -276,9 +276,20 @@ class ChadoStorage extends TripalStorageBase implements TripalStorageInterface {
     $this->field_debugger->printHeader('Load');
     $this->field_debugger->summarizeChadoStorage($this, 'At the beginning of ChadoStorage::loadValues');
 
+    // We can actually skip this entire process if all the fields are 100% cached.
+    // The cached_fields indicates for each field (key) if it is fully cached (TRUE)
+    // or not (FALSE). Thus is not fields in this variable are false then
+    // all the fields are cached!
+    if (array_search(FALSE, $this->cached_fields, TRUE) === FALSE) {
+      return TRUE;
+    }
+
     // Build the ChadoRecords object.
     $this->records = new ChadoRecords($this->field_debugger, $this->logger, $this->connection);
-    $this->buildChadoRecords($values);
+
+    // Parameters indicate this is not a find action and that we should not add
+    // any properties for fields which are 100% cached in drupal to ChadoRecords.
+    $this->buildChadoRecords($values, FALSE, TRUE);
 
     $transaction_chado = $this->connection->startTransaction();
     try {
@@ -661,13 +672,25 @@ class ChadoStorage extends TripalStorageBase implements TripalStorageInterface {
    *   will be set in the StoragePropertyValue object.
    * @param bool $is_find
    *   Set to TRUE if we are building the record array for finding records.
+   * @param bool $ignore_cached
+   *   If this is set to TRUE then any values that are part of a field where
+   *   all properties are cached in Drupal are not added to ChadoRecords.
    */
-  protected function buildChadoRecords($values, bool $is_find = FALSE) {
+  protected function buildChadoRecords($values, bool $is_find = FALSE, bool $ignore_cached = FALSE) {
 
     $this->field_debugger->reportValues($values, 'The values submitted to ChadoStorage');
 
     // Iterate through the value objects.
     foreach ($values as $field_name => $deltas) {
+
+      // Check if this field is 100% cached in drupal.
+      $field_cached = (array_key_exists($field_name, $this->cached_fields)) ? $this->cached_fields[$field_name] : FALSE;
+
+      // If we are supposed to ignore cached fields and the current field is
+      // 100% cached, then we can skip this field.
+      if ($ignore_cached AND $field_cached) {
+        continue;
+      }
 
       // Retrieve the field configuration.
       $definition = $this->getFieldDefinition($field_name);
