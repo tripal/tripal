da413202efe6992f7df668379bd3ea954ce4a060
Lacey Sanderson <laceyannesanderson@gmail.com>
2025-05-05 23:18:41 +0000

    Working towards triggering the after build but no luck yet.

--- tripal/tests/src/Traits/TripalEntityFieldTestTrait.php
+++ tripal/tests/src/Traits/TripalEntityFieldTestTrait.php
@@ -495,6 +495,19 @@ trait TripalEntityFieldTestTrait {
    *
    * This is typically used when testing fields.
    *
+   * When a Drupal form is built it goes through two main stages:
+   * 1. The retrieve and preparation stage: the form is built using the form's
+   * build function and all the alter form hooks are called. The output of this
+   * stage is a Form API array, an array with all the elements the form should
+   * have, but not the final render array.
+   * 2. Recursive building stage: a recursive process traversing the $form array
+   * calling auxiliary functions like #process and #after_build callbacks. The
+   * goal is to obtain a Drupal render array with the final shape of the form,
+   * with all the required elements.
+   *
+   * This method ensures both are done in order to accurately reflect the
+   * final form.
+   *
    * @param string $bundle_name
    *   The id of the Tripal Entity Type this form is for.
    *
@@ -506,30 +519,54 @@ trait TripalEntityFieldTestTrait {
    *     form elements.
    *   - FormState $form_state: the state of this form including relationships
    *     to both the form object and form array.
+   *
+   * @see Drupal\Core\Form\FormBuilder
    */
   public function setupTripalEntityAddForm(string $bundle_name): array {
     $entity_type_manager = $this->container->get('entity_type.manager');
+    $form_builder = \Drupal::formBuilder();
 
-    // Get the form object in order to test submission.
+    // Step 0: Prepare Form object and form state.
+    // -- Get the form object from the entity type manager.
     $form_object = $entity_type_manager->getFormObject('tripal_entity', 'add');
-
-    // Create the Form State and set it up to be used by an entity form.
-    $form_state = new FormState();
-
-    // Form state likes to have the form object it will be working with.
-    $form_state->setFormObject($form_object);
-
-    // Create an empty entity object for the form.
+    // -- Create an empty entity object for the form.
     // This is not saved yet but is needed as an empty husk for the form
     // to determine the fields, widgets, etc to build the form array.
     $entity = TripalEntity::create([
       'type' => $this->bundle_name,
     ]);
     $form_object->setEntity($entity);
+    // -- Now get the form id (needs the entity first).
+    $form_id = $form_object->getFormId();
 
-    // Build the form now as the form state likes to have the full array.
-    $form = $form_object->buildForm([], $form_state);
+    // -- Create the Form State and set it up to be used by an entity form.
+    $form_state = new FormState();
+    $form_state->setFormObject($form_object);
+
+    // -- Set the base user input for the form.
+    $input = [
+      'uid' => 1
+    ];
+    $form_state->setUserInput($input);
+
+    // Step 1: Form Retrieve + Preparation Phase.
+    $form = $form_builder->retrieveForm($form_id, $form_state);
+    $form_builder->prepareForm($form_id, $form, $form_state);
+    // -- The form state likes to have the full array.
     $form_state->setCompleteForm($form);
+
+    // Step 2: Recursive Form Building stage.
+    // This triggers the #after_build and $process callbacks of our widgets.
+    // @todo figure out why this gives us the following error:
+    // Drupal\Core\Database\DatabaseExceptionWrapper: SQLSTATE[22P02]: Invalid text representation: 7
+    // ERROR:  invalid input syntax for type bigint: "ly7k3fxv (1)"
+    // LINE 5: WHERE "users_field_data"."uid" IN ('ly7k3fxv (1)')
+    //       ^: SELECT "base_table"."uid" AS "uid", "base_table"."uid" AS "base_table_uid"
+    // FROM "test78899064users" "base_table"
+    // INNER JOIN "test78899064users_field_data" "users_field_data" ON "users_field_data"."uid" = "base_table"."uid"
+    // WHERE "users_field_data"."uid" IN (:db_condition_placeholder_0);
+    // Array( [:db_condition_placeholder_0] => ly7k3fxv (1)  )
+    // $form_builder->processForm($form_id, $form, $form_state);
 
     return [$form_object, $form, $form_state];
   }
