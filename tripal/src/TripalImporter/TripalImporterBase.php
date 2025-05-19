91372e0b1387ed4b27b4b6846ed12e2b7168a637
Douglas Senalik <dsenalik@wisc.edu>
2025-05-19 14:08:04 -0500

    update importer base to use file retriever service

--- tripal/src/TripalImporter/TripalImporterBase.php
+++ tripal/src/TripalImporter/TripalImporterBase.php
@@ -104,11 +104,11 @@ abstract class TripalImporterBase extends PluginBase implements TripalImporterIn
   protected $messenger = NULL;
 
   /**
-   * An instance of the Drupal HTTP client, i.e. Guzzle
+   * An instance of the Tripal file retriever service
    *
-   * @var object \GuzzleHttp\ClientInterface
+   * @var object \Drupal\tripal\Services\TripalFileRetriever
    */
-  protected $httpClient = NULL;
+  protected $fileretriever = NULL;
 
   /**
    * Stores the last percentage that progress was reported.
@@ -157,8 +157,7 @@ abstract class TripalImporterBase extends PluginBase implements TripalImporterIn
     $this->messenger = \Drupal::messenger();
 
     // Initialize http client
-    $this->httpClient = \Drupal::httpClient();
-
+    $this->fileretriever = \Drupal::service('tripal.fileretriever');
   }
 
   /**
@@ -399,55 +398,6 @@ abstract class TripalImporterBase extends PluginBase implements TripalImporterIn
   }
 
   /**
-   * Download the contents of a remote or local file from a specified URL
-   *
-   * @param string $url
-   *   The address of the file to download
-   * @param int $retries
-   *   The number of times to retry the remote download if an error occurs
-   * @param array $options
-   *   Any options to pass to the HTTP request
-   * @return string
-   *   The data obtained from the specified url, or NULL if it could not be downloaded
-   */
-  public function retrieveFile(string $url, int $retries = 3, array $options = []): string|null {
-    // Distinguish between local and remote files
-    $parsed_url = parse_url($url);
-    if ($parsed_url['host'] ?? NULL) {
-      while ($retries > 0) {
-        try {
-          $response = $this->httpClient->get($url, $options);
-          $response_body = (string) $response->getBody();
-          return $response_body;
-        }
-        catch (RequestException $e) {
-          if ($retries > 1) {
-            $this->logger->error('Unable to get response from @url: @exception. Will retry',
-                ['@url' => $url, '@exception' => $e->getMessage()]);
-          }
-          else {
-            $this->logger->error('Unable to get response from @url: @exception',
-                ['@url' => $url, '@exception' => $e->getMessage()]);
-            return NULL;
-          }
-        }
-        $retries--;
-        sleep(1);
-      }
-    }
-    // If there was no host in the url, then it is considered a local file
-    else {
-      $contents = file_get_contents($url);
-      if ($contents === FALSE) {
-        return NULL;
-      }
-      else {
-        return $contents;
-      }
-    }
-  }
-
-  /**
    * Prepares the importer files for execution.
    *
    * This function must be run prior to the run() function to ensure that
@@ -473,7 +423,7 @@ abstract class TripalImporterBase extends PluginBase implements TripalImporterIn
           $this->logger->notice('Saving as: %file', ['%file' => $temp]);
 
           // Download the remote file contents.
-          $content = $this->retrieveFile($file_remote);
+          $content = $this->fileretriever($file_remote);
           if (is_null($content)) {
             $this->is_prepared = FALSE;
             return;
@@ -481,6 +431,7 @@ abstract class TripalImporterBase extends PluginBase implements TripalImporterIn
 
           // Write the contents from the remote file to the local temp file.
           $saved = file_put_contents($temp, $content);
+print "CP05 saved to temp file \"$temp\" ";var_dump($saved); //@@@
           if (!$saved) {
             $this->logger->error('Unable to save to local temporary file @file',
                 ['@file' => $temp]);
@@ -495,6 +446,7 @@ abstract class TripalImporterBase extends PluginBase implements TripalImporterIn
         // Is this file compressed? If so, then uncompress it.
         $matches = [];
         if (preg_match('/^(.*?)\.gz$/', $this->arguments['files'][$i]['file_path'], $matches)) {
+print "CP06 uncompressing\n";//@@@
           $this->logger->notice('Uncompressing: %file', ['%file' => $this->arguments['files'][$i]['file_path']]);
           $buffer_size = 4096;
           $new_file_path = $matches[1];
