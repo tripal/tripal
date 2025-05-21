8fb175e5555f6298ea3fdf84a6dc7cdb5a4a65a9
Douglas Senalik <dsenalik@wisc.edu>
2025-05-21 13:07:44 -0500

    temporary code to be able to trigger mocked download error

--- tripal/src/Services/TripalFileRetriever.php
+++ tripal/src/Services/TripalFileRetriever.php
@@ -72,6 +72,7 @@ class TripalFileRetriever {
         catch (\Exception $e) {
           $this->handleURLExceptions($retries, $e, $url);
         }
+$fail = \Drupal::state()->get('FAIL', '0');if ($fail) { $contents = NULL; $retries = 0; $this->logger->error('Unable to get response from @url: mocked_error', ['@url' => $url]); } //@@@ simulate error
         $retries--;
         if (is_null($contents) && ($retries > 0)) {
           sleep(1);
@@ -135,6 +136,7 @@ class TripalFileRetriever {
         catch (\Exception $e) {
           $this->handleURLExceptions($retries, $e, $url);
         }
+$fail = \Drupal::state()->get('FAIL', '0');if ($fail) { $status = FALSE; $retries = 0; $this->logger->error('Unable to get response from @url: mocked_error', ['@url' => $url]); } //@@@ simulate error 10% of the time
         $retries--;
         if (!$status && ($retries > 0)) {
           sleep(1);
