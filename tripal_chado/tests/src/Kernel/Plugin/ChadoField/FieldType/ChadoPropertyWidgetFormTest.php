885a69983d25b76975bf55e1a3fd05abfe896b51
Lacey Sanderson <laceyannesanderson@gmail.com>
2025-05-29 19:48:44 +0000

    Check the entity values and also the edit form.

--- tripal_chado/tests/src/Kernel/Plugin/ChadoField/FieldType/ChadoPropertyWidgetFormTest.php
+++ tripal_chado/tests/src/Kernel/Plugin/ChadoField/FieldType/ChadoPropertyWidgetFormTest.php
@@ -218,19 +218,21 @@ class ChadoPropertyWidgetFormTest extends ChadoTestKernelBase {
     // -- if there are no errors then submit the form.
     if (count($errors) === 0) {
       $form_object->submitForm($form, $form_state);
+      $form_object->save($form, $form_state);
+      // -- confirm the entity matches our expectations.
+      $entity = $form_object->getEntity();
+      $this->assertFieldValuesMatch($current_scenario['create']['expected_field_values'], $entity, "Field values didn't match our expectations after saving the create form.");
     }
 
-    /*
     // 3. Test the edit form is generated properly.
     $form = $form_object = $form_state = NULL;
-    $entity_id = 1;
     // Setup an edit Tripal entity form to interact with (test defaults).
-    [$form_object, $form, $form_state] = $this->setupTripalEntityEditForm($this->bundle_name, $entity_id);
+    [$form_object, $form, $form_state] = $this->setupTripalEntityEditForm($this->bundle_name, $entity);
     // Confirm all the keys we expect are set to the values we expect.
     $this->assertFieldWidgetsMatch($current_scenario['edit']['expected_form_keys'], $this->system_under_test['fields'], $form, $current_scenario['label'] . ' [EDIT]');
 
     // 4. Test editing the entity through the form.
-    $this->populateTripalEntityForm($form_object, $form_state, $current_scenario['edit']['user_input']);
+    $this->populateTripalEntityFormState($form_object, $form_state, $current_scenario['edit']['user_input']);
     // -- now validate it with the data provided.
     $form_object->validateForm($form, $form_state);
     $errors = $form_state->getErrors();
@@ -238,8 +240,11 @@ class ChadoPropertyWidgetFormTest extends ChadoTestKernelBase {
     // -- if there are no errors then submit the form.
     if (count($errors) === 0) {
       $form_object->submitForm($form, $form_state);
+      $form_object->save($form, $form_state);
+      // -- confirm the entity matches our expectations.
+      $entity = $form_object->getEntity();
+      $this->assertFieldValuesMatch($current_scenario['edit']['expected_field_values'], $entity, "Field values didn't match our expectations after saving the edit form.");
     }
-    */
   }
 
 }
