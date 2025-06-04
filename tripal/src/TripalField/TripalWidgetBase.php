2d00ad95f6b09899551380902f0a511371b42e0d
Douglas Senalik <dsenalik@wisc.edu>
2025-06-04 12:40:52 -0500

    handle finite cardinality widgets similarly to unlimited

--- tripal/src/TripalField/TripalWidgetBase.php
+++ tripal/src/TripalField/TripalWidgetBase.php
@@ -5,7 +5,18 @@ namespace Drupal\tripal\TripalField;
 use Drupal\Core\Field\WidgetBase;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Form\FormStateInterface;
+#use Drupal\Component\Utility\Html;
+#use Drupal\Core\Ajax\AjaxResponse;
+#use Drupal\Core\Ajax\FocusFirstCommand;
+#use Drupal\Core\Ajax\InsertCommand;
 
+#use Drupal\Component\Utility\NestedArray;
+#use Drupal\Component\Utility\SortArray;
+#use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+#use Drupal\Core\Render\Element;
+#use Symfony\Component\DependencyInjection\ContainerInterface;
+#use Symfony\Component\Validator\ConstraintViolationInterface;
+#use Symfony\Component\Validator\ConstraintViolationListInterface;
 
 
 /**
@@ -30,4 +41,56 @@ abstract class TripalWidgetBase extends WidgetBase {
   public function sanitizeKey($key) {
     return preg_replace('/[^\w]/', '_', $key);
   }
+
+  /**
+   * Override Drupal's function to change the behavior of finite cardinality.
+   *
+   * In the case of a fixed finite cardinality that is not small, e.g. 100,
+   * we don't want to display 100 fields for every delta if they are not populated.
+   * See Drupal issue https://www.drupal.org/project/drupal/issues/1156338
+   * We will tell Drupal that it is unlimited cardinality up until the point where
+   * we actually hit the cardinality limit. Before that point, we can use the
+   * "Add another item" button in the same way as unlimited cardinality fields.
+   *
+   * @inheritdoc
+   */
+  protected function formMultipleElements(FieldItemListInterface $items, array &$form, FormStateInterface $form_state) {
+    $cardinality = $this->fieldDefinition
+        ->getFieldStorageDefinition()
+        ->getCardinality();
+
+    // Only override the Drupal behavior when we have a finite cardinality
+    // that is greater than one.
+    if ($cardinality > 1) {
+
+      // If not every item in the finite cardinality is occupied,
+      // temporarily set to unlimited before calling the parent class.
+      $temp_cardinality = $cardinality;
+      if (count($items) < $cardinality) {
+        $temp_cardinality = -1;
+      }
+      $this->fieldDefinition
+        ->getFieldStorageDefinition()
+        ->setCardinality($temp_cardinality);
+    }
+
+    // Have the parent class generate the elements with the adjusted cardinality.
+    $elements = parent::formMultipleElements($items, $form, $form_state);
+
+    // In the case where we have exactly as many widgets as the cardinality
+    // specifies, we will remove the "Add another item" button so that the
+    // user can't add another row and exceed the specified cardinality.
+    // This will only happen after the ajax triggers from a previous row addition.
+    if ($cardinality > 1) {
+      $max_delta = $elements['#max_delta'];
+      if (($max_delta + 1) >= $cardinality) {
+        if (array_key_exists('add_more', $elements)) {
+          unset($elements['add_more']);
+        }
+      }
+    }
+
+    return $elements;
+  }
+
 }
