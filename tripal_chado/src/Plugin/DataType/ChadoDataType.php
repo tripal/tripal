3f3c2e6c77f2bc340bef96e9a25ec6c9ca61b938
Douglas Senalik <dsenalik@wisc.edu>
2025-07-23 12:06:51 -0500

    Add attributes for DataType classes

--- tripal_chado/src/Plugin/DataType/ChadoDataType.php
+++ tripal_chado/src/Plugin/DataType/ChadoDataType.php
@@ -4,7 +4,15 @@ namespace Drupal\tripal_chado\Plugin\DataType;
 
 use Drupal\Core\TypedData\TypedData;
 use Drupal\Core\TypedData\ComplexDataInterface;
+use Drupal\Core\TypedData\Attribute\DataType;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\tripal_chado\TypedData\ChadoDataDefinition;
 
+#[DataType(
+  id: 'chado_record',
+  label: new TranslatableMarkup('Chado'),
+  definition_class: ChadoDataDefinition::class,
+)]
 /**
  * Plugin implementation of the ChadoLinker data type.
  *
--- tripal_chado/src/Plugin/DataType/ChadoLinker.php
+++ tripal_chado/src/Plugin/DataType/ChadoLinker.php
@@ -3,7 +3,15 @@
 namespace Drupal\tripal_chado\Plugin\DataType;
 
 use Drupal\Core\TypedData\TypedData;
+use Drupal\Core\TypedData\Attribute\DataType;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\tripal_chado\TypedData\ChadoLinkerDataDefinition;
 
+#[DataType(
+  id: 'chado_linker',
+  label: new TranslatableMarkup('Chado Linker'),
+  definition_class: ChadoLinkerDataDefinition::class,
+)]
 /**
  * Plugin implementation of the ChadoLinker data type
  *
