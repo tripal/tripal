5c97e9cc538bd2da54af57e3c15dfdba90715b2c
Douglas Senalik <dsenalik@wisc.edu>
2025-08-13 07:24:50 -0500

    Remove unused code block in test

--- tripal_chado/tests/src/Kernel/Plugin/ChadoField/FieldType/ChadoPhylotreeTypeCRUDTest.php
+++ tripal_chado/tests/src/Kernel/Plugin/ChadoField/FieldType/ChadoPhylotreeTypeCRUDTest.php
@@ -167,33 +167,6 @@ class ChadoPhylotreeTypeCRUDTest extends ChadoTestKernelBase {
     $current_scenario = $this->scenarios[$current_scenario_key];
     $this->assertEquals($current_scenario_label, $current_scenario['label'], "We may not have retrieved the expected scenario as the labels did not match.");
 
-#    // Set the project type just in case.
-#    $type_id = $this->getCvtermID('NCIT', 'C47885');
-#    $current_scenario['create']['user_input']['project_type'][0]['type_id'] = $type_id;
-#    $current_scenario['create']['expected']['project_type'][0]['type_id'] = $type_id;
-#    $current_scenario['edit']['expected']['project_type'][0]['type_id'] = $type_id;
-#    // Set the property field types just in case.
-#    $comment_type_id = $this->getCvtermID('rdfs', 'comment');
-#    $location_type_id = $this->getCvtermID('NCIT', 'C25341');
-#    foreach (['create', 'edit'] as $process_key) {
-#      foreach (['user_input', 'expected'] as $input_type) {
-#        if (array_key_exists('project_prop1', $current_scenario[$process_key][$input_type])) {
-#          foreach ($current_scenario[$process_key][$input_type]['project_prop1'] as $delta => $values) {
-#            if ($values['type_id'] === 181) {
-#              $current_scenario[$process_key][$input_type]['project_prop1'][$delta]['type_id'] = $comment_type_id;
-#            }
-#          }
-#        }
-#        if (array_key_exists('project_prop2', $current_scenario[$process_key][$input_type])) {
-#          foreach ($current_scenario[$process_key][$input_type]['project_prop2'] as $delta => $values) {
-#            if ($values['type_id'] === 159) {
-#              $current_scenario[$process_key][$input_type]['project_prop2'][$delta]['type_id'] = $location_type_id;
-#            }
-#          }
-#        }
-#      }
-#    }
-
     return $current_scenario;
   }
 
