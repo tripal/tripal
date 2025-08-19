cafa04af61c0459b797892fefc74f6e7fd02b38e
Douglas Senalik <dsenalik@wisc.edu>
2025-08-19 15:16:47 -0500

    Add file location validation

--- tripal_file/src/Plugin/Field/FieldWidget/TripalFileLocationWidgetDefault.php
+++ tripal_file/src/Plugin/Field/FieldWidget/TripalFileLocationWidgetDefault.php
@@ -64,9 +64,10 @@ class TripalFileLocationWidgetDefault extends ChadoWidgetBase {
       '#title' => $this->t('URI'),
       '#type' => 'textfield',
       '#default_value' => $uri,
-      '#description' => $this->t('Enter a web URL or a local URI to a file on this site.'),
+      '#description' => $this->t('Enter a web URL or a local URI to a file on this site. This value is required.'),
       // It is required, but check this in validation.
       '#required' => FALSE,
+      '#element_validate' => [[static::class, 'validateFilelocUri']],
     ];
     $elements['fileloc_filename'] = [
       '#title' => $this->t('File Name'),
@@ -78,9 +79,10 @@ class TripalFileLocationWidgetDefault extends ChadoWidgetBase {
     $elements['fileloc_md5checksum'] = [
       '#title' => $this->t('MD5 Checksum'),
       '#type' => 'textfield',
-      '#default_value' => $md5checksum,
+      '#default_value' => trim($md5checksum),
       '#description' => $this->t('If the file is local, this will be determined automatically.'),
       '#required' => FALSE,
+      '#element_validate' => [[static::class, 'validateMd5checksum']],
     ];
     $elements['fileloc_size'] = [
       '#title' => $this->t('File Size'),
@@ -106,7 +108,65 @@ class TripalFileLocationWidgetDefault extends ChadoWidgetBase {
    * {@inheritDoc}
    */
   public function massageFormValues(array $values, array $form, FormStateInterface $form_state) {
+# @todo wondering about the need for this...
+#    foreach ($values as $delta => $properties) {
+#      $values[$delta]['fileloc_md5checksum'] = trim($values[$delta]['fileloc_md5checksum']);
+#    }
     return $values;
+  }
+
+  /**
+   * Form element validation handler for the uri field.
+   *
+   * This field is required in the database table, but we do not set
+   * it as required in the form because it affects empty records.
+   *
+   * @param array $element
+   *   The form element being validated
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The form state of the (entire) configuration form
+   */
+  public static function validateFilelocUri($element, FormStateInterface $form_state) {
+    // element_parents e.g. 0 => "project_relationship", 1 => 0, 2 => "related_record".
+    $element_parents = $element['#parents'];
+    $delta = $element_parents[1];
+    $element_value = $element['#value'];
+    $form_state_values = $form_state->getValues();
+    $other_values = $form_state_values['file_location'][$delta];
+    if ($element_value == '') {
+      // If all other fields of the same delta are empty, then we allow
+      // the empty value, as it is probably the last delta.
+      $other_values_empty = (!$other_values['fileloc_filename']
+                          && !$other_values['fileloc_md5checksum']
+                          && !$other_values['fileloc_size']);
+      if (!$other_values_empty) {
+        $form_state->setErrorByName(implode('][', $element_parents),
+          t('The URI field is a required value'));
+      }
+    }
+  }
+
+  /**
+   * Form element validation handler for the MD5 Checksum field.
+   *
+   * An MD5 checksum must have a length of exactly 32 and be valid
+   * hexadecimal characters. An empty value is also allowed.
+   *
+   * @param array $element
+   *   The form element being validated
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The form state of the (entire) configuration form
+   */
+  public static function validateMd5checksum($element, FormStateInterface $form_state) {
+    $element_parents = $element['#parents'];
+    // element_parents e.g. 0 => "project_relationship", 1 => 0, 2 => "related_record"
+    $element_value = $element['#value'];
+    if ($element_value != '' && $element_value != '                                ') {
+      if (!preg_match('/^[0-9A-Fa-f]{32}$/', $element_value)) {
+        $form_state->setErrorByName(implode('][', $element_parents),
+          t('An MD5 checksum must be exactly 32 characters long and only contain valid hexadecimal characters, or be left blank.'));
+      }
+    }
   }
 
   /**
