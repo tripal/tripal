eedfeff04111665b722e62030263e5506a2266c5
Douglas Senalik <dsenalik@wisc.edu>
2025-09-22 09:05:38 -0500

    update all remaining fields to support custom tables 2

--- tripal_chado/src/Plugin/Field/FieldType/ChadoRelationshipTypeDefault.php
+++ tripal_chado/src/Plugin/Field/FieldType/ChadoRelationshipTypeDefault.php
@@ -369,7 +369,7 @@ class ChadoRelationshipTypeDefault extends ChadoFieldItemBase {
     $subject_column = '';
     $object_column = '';
     // The subject and object columns will be among the foreign keys to the base table
-    $table_schema_def = $chado->schema()->getTableDef($relationship_table, ['format' => 'Drupal']);
+    $table_schema_def = self::getChadoTableDef($schema, $relationship_table);
     if ($table_schema_def['foreign keys'][$base_table]['columns'] ?? NULL) {
       foreach (array_keys($table_schema_def['foreign keys'][$base_table]['columns']) as $relationship_column) {
         if (preg_match('/subject/', $relationship_column)) {
