23d67e8a95b53c65a9bb51590bd3d67d7f5b212a
Lacey-Anne Sanderson <las166@mail.usask.ca>
2025-10-10 00:42:37 +0000

    Default to main property name where there is not a main display property.

--- tripal/src/Entity/TripalEntity.php
+++ tripal/src/Entity/TripalEntity.php
@@ -551,8 +551,11 @@ class TripalEntity extends ContentEntityBase implements TripalEntityInterface {
         /** @var \Drupal\Core\TypedData\TypedDataInterface $prop **/
         $props = $item->getProperties();
         $main_prop_key = NULL;
-        if (method_exists($item, 'mainPropertyName')) {
+        if (method_exists($item, 'mainDisplayPropertyName')) {
           $main_prop_key = $item->mainDisplayPropertyName();
+        }
+        elseif (method_exists($item, 'mainPropertyName')) {
+          $main_prop_key = $item->mainPropertyName();
         }
         if (is_array($props)) {
           foreach ($props as $prop) {
