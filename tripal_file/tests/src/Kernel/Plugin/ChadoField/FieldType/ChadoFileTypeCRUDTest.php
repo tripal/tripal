ec1003c6f05c3435556e1ce9ccedefcd96c96dd4
Douglas Senalik <dsenalik@wisc.edu>
2025-10-19 15:04:26 -0500

    add test for chado_file_type_default field

---
+++ tripal_file/tests/src/Kernel/Plugin/ChadoField/FieldType/ChadoFileTypeCRUDTest.php
@@ -0,0 +1,309 @@
+<?php
+
+namespace Drupal\Tests\tripal_chado\Kernel\ChadoField\FieldType;
+
+use Drupal\Tests\tripal_chado\Kernel\ChadoTestKernelBase;
+use Drupal\Tests\tripal_chado\Traits\ChadoFieldTestTrait;
+use Drupal\tripal\Entity\TripalEntity;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Group;
+
+/**
+ * Tests the File Field Type.
+ *
+ * Specifically focused on create + update actions performed on the entity
+ * directly. Both TripalEntity, ChadoStorage and the fields will be covered.
+ *
+ * @group tripal-file
+ * @group TripalField
+ * @group ChadoField
+ */
+#[Group('tripal-file')]
+#[Group('TripalField')]
+#[Group('ChadoField')]
+class ChadoFileTypeCRUDTest extends ChadoTestKernelBase {
+
+  use ChadoFieldTestTrait;
+
+  /**
+   * The theme to use when rendering content in the test environment.
+   *
+   * @var string
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * The modules that this test depends on.
+   *
+   * NOTE: since this is a kernel test, these modules are not being installed
+   * but are available to be installed.
+   *
+   * @var array
+   */
+  protected static $modules = [
+    'system',
+    'user',
+    'path',
+    'path_alias',
+    'field',
+    'datetime',
+    'views',
+    'tripal',
+    'tripal_chado',
+    'tripal_file',
+  ];
+
+  /**
+   * The test drupal connection. It is also set in the container.
+   *
+   * @var object
+   */
+  protected object $drupal_connection;
+
+  /**
+   * The test chado connection. It is also set in the container.
+   *
+   * @var ChadoConnection
+   */
+  protected object $chado_connection;
+
+  /**
+   * The YAML file indicating the scenarios to test and how to setup the enviro.
+   *
+   * @var string
+   */
+  protected string $yaml_info_file = __DIR__ . '/ChadoFileType-TestInfo.yml';
+
+  /**
+   * File record IDs created for testing.
+   *
+   * @var array
+   */
+  protected array $file_ids;
+
+  /**
+   * Describes the environment to setup for this test.
+   *
+   * @var array
+   *   An array with the following keys:
+   *   - chado_version: the version of chado to test under.
+   *   - bundle: an array defining the tripal entity type to create.
+   *   - fields: a list of fields to be attached the above bundle.
+   */
+  protected array $system_under_test;
+
+  /**
+   * The TripalEntityType id of the bundle being used in this test.
+   *
+   * @var string
+   */
+  protected string $bundle_name;
+
+  /**
+   * Describes the scenarios to test.
+   *
+   * This will be used in combination with the data provider. It can't be
+   * accessed directly in the dataProvider due to the way that PHPUnit is
+   * setup.
+   *
+   * @var array
+   *  A list of scenarios where each one has the following keys:
+   *  - label: A human-readable label for the scenario to be used in assert
+   *    messages.
+   *  - description: A description of the scenario and what you are wanting to
+   *    test. This will not be used in the test but is rather there to help
+   *    people reading the YAML file and to make it easier to maintain.
+   *  - create: An array of the values to be provided when creating a
+   *    TripalEntity. There should be a key matching the name of each field in
+   *    the system-under-test and it's value should be an array containing all
+   *    the property types for that field mapped to a value.
+   *  - edit: An array of the values to be provided when updating an existing
+   *    TripalEntity. There should be a key matching the name of each field in
+   *    the system-under-test and it's value should be an array containing all
+   *    the property types for that field mapped to a value.
+   */
+  protected array $scenarios;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    // The Drupal connection will be created in the parent. This is used
+    // when checking the Drupal field tables.
+    $this->drupal_connection = $this->container->get('database');
+
+    // First retrieve info from the YAML file for this particular test.
+    [$this->system_under_test, $this->scenarios] = $this->getTestInfoFromYaml($this->yaml_info_file);
+    $this->bundle_name = $this->system_under_test['bundle']['id'];
+
+    // Create the test Chado installation we will be using.
+    if (!array_key_exists('chado_version', $this->system_under_test)) {
+      $this->system_under_test['chado_version'] = '1.3';
+    }
+    $this->chado_connection = $this->getTestSchema(
+      ChadoTestKernelBase::PREPARE_TEST_CHADO,
+      $this->system_under_test['chado_version']
+    );
+
+    // Next setup the environment, but skip the system under test for now,
+    // we need to setup the tripal_file module before that can be done.
+    $this->setupChadoEntityFieldTestEnvironment([]);
+
+    // Install schema for custom chado tables, needed for tripal_file module.
+    $this->installSchema('tripal_chado', ['tripal_custom_tables']);
+
+    // Set up the tripal file module.
+    $this->installConfig(['tripal_file']);
+
+    // Install the module's custom chado tables in the test schema.
+    $test_schema_name = $this->chado_connection->getSchemaName();
+    tripal_file_create_custom_chado_tables($test_schema_name);
+
+    // Install terms for the tripal_file module.
+    $terms_init_service = \Drupal::service('tripal_chado.terms_init');
+    $terms_init_service->installTerms('tripal_file_content_terms');
+
+    // Create three file records in chado for testing (not published).
+    for ($i = 1; $i <= 3; $i++) {
+      $this->file_ids[$i] = $this->chado_connection
+        ->insert('1:file')
+        ->fields(['name' => 'File ' . $i, 'type_id' => 1, 'description' => 'File description ' . $i])
+        ->execute();
+      $this->assertNotEmpty($this->file_ids[$i], 'Did not create file');
+    }
+
+    // Now that tripal_file is set up, we can setup the environment
+    // according to the system under test.
+    $this->setupChadoEntityFieldSystemUnderTest($this->system_under_test);
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function tearDown(): void {
+
+    // This is a list of the tables that the module creates when installed.
+    $tripal_file_tables = [
+      'file',
+      'license',
+      'fileloc',
+      'fileprop',
+      'file_relationship',
+      'file_dbxref',
+      'file_license',
+      'analysis_file',
+      'assay_file',
+      'biomaterial_file',
+      'file_contact',
+      'cv_file',
+      'eimage_file',
+      'feature_file',
+      'featuremap_file',
+      'library_file',
+      'nd_protocol_file',
+      'organism_file',
+      'phylotree_file',
+      'project_file',
+      'file_pub',
+      'stock_file',
+      'stockcollection_file',
+      'study_file',
+    ];
+
+    // Tests the module uninstall.
+    $test_schema_name = $this->chado_connection->getSchemaName();
+    tripal_file_drop_custom_chado_tables($test_schema_name);
+    foreach ($tripal_file_tables as $table) {
+      $table_exists = $this->chado_connection->schema()->tableExists($table);
+      $this->assertFalse($table_exists, "The table \"$table\" was not removed when the tripal_file module was uninstalled");
+    }
+
+    parent::tearDown();
+  }
+
+  /**
+   * Data Provider: works with the YAML to provide scenarios for testing.
+   *
+   * @return array
+   *   List of scenarios to test where each one matches a key and label in the
+   *   associated YAML scenarios.
+   */
+  public static function provideScenarios() {
+    $scenarios = [];
+
+    $scenarios[] = [
+      0,
+      "Add files when edited",
+    ];
+
+    return $scenarios;
+  }
+
+  /**
+   * Retrieves the current scenario based on the data provider.
+   *
+   * NOTE: Also ensures the type_ids match what is currently in the database.
+   *
+   * @param int $current_scenario_key
+   *   The key of the scenario in the YAML.
+   * @param string $current_scenario_label
+   *   The label of the scenario in the YAML.
+   *
+   * @return array
+   *   The scenario to be tested as defined in the YAML.
+   */
+  public function retrieveCurrentScenario(int $current_scenario_key, string $current_scenario_label) {
+
+    // Retrieve the correct scenario.
+    $current_scenario = $this->scenarios[$current_scenario_key];
+    $this->assertEquals($current_scenario_label, $current_scenario['label'], "We may not have retrieved the expected scenario as the labels did not match.");
+
+    return $current_scenario;
+  }
+
+  /**
+   * Tests the ChadoFileType field through TripalEntity->save().
+   *
+   * @param int $current_scenario_key
+   *   The key of the scenario in the YAML.
+   * @param string $current_scenario_label
+   *   The label of the scenario in the YAML.
+   *
+   * @dataProvider provideScenarios
+   */
+  #[DataProvider('provideScenarios')]
+  public function testChadoFileTypeEntityCrud(int $current_scenario_key, string $current_scenario_label) {
+    $current_scenario = $this->retrieveCurrentScenario($current_scenario_key, $current_scenario_label);
+
+    // 1. Create the entity with that value set.
+    $entity = TripalEntity::create([
+      'type' => $this->bundle_name,
+      'title' => $this->randomString(),
+    ] + $current_scenario['create']['user_input']);
+    $this->assertInstanceOf(TripalEntity::class, $entity, "We were not able to create a piece of tripal content to test our " . $current_scenario['label'] . " scenario.");
+    $status = $entity->save();
+    $this->assertEquals(SAVED_NEW, $status, "We expected to have saved a new entity for our " . $current_scenario['label'] . " scenario.");
+    // @debug print_r($entity->toArray());
+
+    // 2. Load the entity we just created so we can check the values.
+    $created_entity = TripalEntity::load($entity->id());
+    $this->assertFieldValuesMatch($current_scenario['create']['expected'], $created_entity, $current_scenario['label'] . ' CREATE ');
+
+    // 3. Make changes and then save again.
+    foreach ($current_scenario['edit']['user_input'] as $field_name => $new_values) {
+      $created_entity->set($field_name, $new_values);
+    }
+    // @debug print_r($created_entity->toArray());
+    $status = $created_entity->save();
+    $this->assertEquals(SAVED_UPDATED, $status, "We expected to have updated the existing entity for our " . $current_scenario['label'] . " scenario.");
+
+    // 4. Load the entity we just updated so we can check the values.
+    $updated_entity = TripalEntity::load($created_entity->id());
+    // @debug print_r($updated_entity->toArray());
+    $this->assertFieldValuesMatch($current_scenario['edit']['expected'], $updated_entity, $current_scenario['label'] . ' EDIT ');
+  }
+
+}
