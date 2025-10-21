ae39ad8bd009b87f3f69e5246608d26fe1c02147
Lacey-Anne Sanderson <laceyannesanderson@gmail.com>
2025-10-21 18:20:43 +0000

    Simplify AccessController to only use check and respect hooks. Add methods to check TripalEntity routes.

--- tripal/src/Access/TripalEntityAccessControlHandler.php
+++ tripal/src/Access/TripalEntityAccessControlHandler.php
@@ -2,17 +2,13 @@
 
 namespace Drupal\tripal\Access;
 
-use Drupal\Core\Entity\EntityAccessControlHandler;
 use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityAccessControlHandler;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Access\AccessResult;
-use Drupal\Core\Access\AccessResultInterface;
-use Drupal\Core\Cache\CacheableMetadata;
-use Drupal\Core\Field\FieldDefinitionInterface;
-use Drupal\Core\Field\FieldItemListInterface;
 
 /**
- * Access controller for the Tripal Content entity.
+ * Access controller for the Tripal Content entity (i.e. TripalEntity).
  *
  * @see \Drupal\tripal\Entity\TripalEntity.
  */
@@ -20,61 +16,44 @@ class TripalEntityAccessControlHandler extends EntityAccessControlHandler {
 
   /**
    * {@inheritdoc}
-   */
-  public function access(EntityInterface $entity, $operation, ?AccountInterface $account = NULL, $return_as_object = FALSE) {
-    $account = $this->prepareUser($account);
-
-    if ($account->hasPermission('administer tripal content')) {
-      $result = AccessResult::allowed()->cachePerPermissions();
-      return $return_as_object ? $result : $result->isAllowed();
-    }
-    if (!$account->hasPermission('access content')) {
-      $result = AccessResult::forbidden("The 'access content' permission is required.")->cachePerPermissions();
-      return $return_as_object ? $result : $result->isAllowed();
-    }
-    $result = parent::access($entity, $operation, $account, TRUE)->cachePerPermissions();
-
-    return $return_as_object ? $result : $result->isAllowed();
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function createAccess($entity_bundle = NULL, ?AccountInterface $account = NULL, array $context = [], $return_as_object = FALSE) {
-    $account = $this->prepareUser($account);
-
-    if ($account->hasPermission('administer tripal content')) {
-      $result = AccessResult::allowed()->cachePerPermissions();
-      return $return_as_object ? $result : $result->isAllowed();
-    }
-    if (!$account->hasPermission('access content')) {
-      $result = AccessResult::forbidden("The 'access content' permission is required.")->cachePerPermissions();
-      return $return_as_object ? $result : $result->isAllowed();
-    }
-
-    $result = parent::createAccess($entity_bundle, $account, $context, TRUE)->cachePerPermissions();
-    return $return_as_object ? $result : $result->isAllowed();
-  }
-
-  /**
-   * {@inheritdoc}
+   *
+   * Note: we do not want to call the parent here as we are doing our own thing
+   * and do not want to introduce any unexpected behaviour.
+   *
+   * If an extension module wants to override what we are doing here then they
+   * should implement one of the following hooks which take priority:
+   * hook_entity_access() or hook_ENTITY_TYPE_access().
    */
   protected function checkAccess(EntityInterface $entity, $operation, AccountInterface $account) {
 
-    // Ensure that the Tripal Content Admin permission bypasses the following permissions.
+    // Ensure that the Tripal Content Admin permission bypasses
+    // the following permissions.
     if ($account->hasPermission('administer tripal content')) {
       return AccessResult::allowed();
     }
 
+    // For view, update, delete we want to use bundle-specific permissions
+    // so lets get the bundle now.
+    $entity_bundle = $entity->getType();
+
+    // Now, lets check the permission based on the bundle and operation.
+    // Always return allow if has permission in order to allow other
+    // implementations to override this functionality.
     switch ($operation) {
       case 'view':
-        return AccessResult::allowedIfHasPermission($account, 'view tripal content entities');
+        // @todo add logic here to check 1) "all" then 2) "own".
+        return AccessResult::allowedIfHasPermission($account, "view all $entity_bundle content");
 
       case 'update':
-        return AccessResult::allowedIfHasPermission($account, 'edit tripal content entities');
+        // @todo add logic here to check 1) "any" then 2) "own".
+        return AccessResult::allowedIfHasPermission($account, "edit any $entity_bundle content");
 
       case 'delete':
-        return AccessResult::allowedIfHasPermission($account, 'delete tripal content entities');
+        // @todo add logic here to check 1) "any" then 2) "own".
+        return AccessResult::allowedIfHasPermission($account, "delete any $entity_bundle content");
+
+      // @todo figure out how to catch unpublish :thinking: is it available as
+      // an operation?
     }
 
     // Unknown operation, no opinion.
@@ -82,78 +61,96 @@ class TripalEntityAccessControlHandler extends EntityAccessControlHandler {
   }
 
   /**
-   * Performs view access checks.
-   *
-   * @param \Drupal\tripal\Entity\TripalEntityInterface $tripal_entity
-   *   The node for which to check access.
-   * @param \Drupal\Core\Session\AccountInterface $account
-   *   The user for which to check access.
-   * @param \Drupal\Core\Cache\CacheableMetadata $cacheability
-   *   Allows cacheability information bubble up from this method.
-   *
-   * @return \Drupal\Core\Access\AccessResultInterface|null
-   *   The calculated access result or null when no opinion.
-   */
-  protected function checkViewAccess(TripalEntityInterface $tripal_entity, AccountInterface $account, CacheableMetadata $cacheability): ?AccessResultInterface {
-    // If the node status changes, so does the outcome of the check below, so
-    // we need to add the node as a cacheable dependency.
-    $cacheability->addCacheableDependency($tripal_entity);
-
-    if ($tripal_entity->isPublished()) {
-      return NULL;
-    }
-    $cacheability->addCacheContexts(['user.permissions']);
-
-    if (!$account->hasPermission('view own unpublished content')) {
-      return NULL;
-    }
-
-    $cacheability->addCacheContexts(['user.roles:authenticated']);
-    // The "view own unpublished content" permission must not be granted
-    // to anonymous users for security reasons.
-    if (!$account->isAuthenticated()) {
-      return NULL;
-    }
-
-    // When access is granted due to the 'view own unpublished content'
-    // permission and for no other reason, node grants are bypassed. However,
-    // to ensure the full set of cacheable metadata is available to variation
-    // cache, additionally add the node_grants cache context so that if the
-    // status or the owner of the node changes, cache redirects will continue to
-    // reflect the latest state without needing to be invalidated.
-    $cacheability->addCacheContexts(['user']);
-    if ($this->moduleHandler->hasImplementations('node_grants')) {
-      $cacheability->addCacheContexts(['user.node_grants:view']);
-    }
-    if ($account->id() != $tripal_entity->getOwnerId()) {
-      return NULL;
-    }
-
-    return AccessResult::allowed()->addCacheableDependency($cacheability);
-  }
-
-  /**
    * {@inheritdoc}
+   *
+   * Note: we do not want to call the parent here as we are doing our own thing
+   * and do not want to introduce any unexpected behaviour.
+   *
+   * If an extension module wants to override what we are doing here then they
+   * should implement one of the following hooks which take priority:
+   * hook_entity_create_access() or hook_ENTITY_TYPE_create_access().
    */
   protected function checkCreateAccess(AccountInterface $account, array $context, $entity_bundle = NULL) {
-    return AccessResult::allowedIf($account->hasPermission('create ' . $entity_bundle . ' content'))->cachePerPermissions();
+
+    // Allow administrator permission to bypass per content type permissions.
+    if ($account->hasPermission('administer tripal content')) {
+      return AccessResult::allowed()->cachePerPermissions();
+    }
+
+    // Check 'create TYPE content' permission.
+    // Always return allow if has permission in order to allow other
+    // implementations to override this functionality.
+    if (!$account->hasPermission("create $entity_bundle content")) {
+      return AccessResult::forbidden("You do not have permission to create this Tripal Content. Have your administrator give you the 'Create new content' permission for this type of Tripal Content specifically.")->cachePerPermissions();
+    }
   }
 
   /**
-   * {@inheritdoc}
+   * Grant access if the user has 'create TYPE content' for any TYPE.
+   *
+   * Note: This is meant to be referenced directly in routes.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   The account of the user to check access for.
+   *
+   * @return \Drupal\Core\Access\AccessResult
+   *   The access result.
+   *
+   * @see entity.tripal_entity.add_page
    */
-  protected function checkFieldAccess($operation, FieldDefinitionInterface $field_definition, AccountInterface $account, ?FieldItemListInterface $items = NULL) {
-    $fieldName = $field_definition->getName();
-    if ($operation == 'edit' && $fieldName === 'status') {
-      return AccessResult::allowedIfHasPermissions($account, ['administer node published status', 'administer nodes'], 'OR');
+  public static function checkHasAnyCreateAccess(AccountInterface $account): AccessResult {
+
+    // @todo this needs to be implemented.
+    return AccessResult::allowed();
+  }
+
+  /**
+   * Grant access if 'publish tripal content' or 'administer tripal content'.
+   *
+   * Note: This is meant to be referenced directly in routes.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   The account of the user to check access for.
+   *
+   * @return \Drupal\Core\Access\AccessResult
+   *   The access result.
+   *
+   * @see tripal.content_bio_data_publish_form
+   */
+  public static function checkHasPublishOrAdminAccess(AccountInterface $account): AccessResult {
+    return self::checkWithAdminOverrideAccess($account, 'publish tripal content');
+  }
+
+  /**
+   * Grant access if the user has $permission or 'administer tripal content'.
+   *
+   * Note: This is meant to be referenced directly in routes.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   The account of the user to check access for.
+   * @param string $permission
+   *   An existing permission string to check as defined in permissions.yml.
+   *
+   * @return \Drupal\Core\Access\AccessResult
+   *   The access result.
+   *
+   * @see entity.tripal_entity.add_page
+   */
+  public static function checkWithAdminOverrideAccess(AccountInterface $account, string $permission): AccessResult {
+
+    // Allow administrator permission to bypass indicated permission.
+    if ($account->hasPermission('administer tripal content')) {
+      return AccessResult::allowed()->cachePerPermissions();
     }
-    // Only users with the administer nodes permission can edit administrative
-    // fields.
-    $administrative_fields = ['uid', 'created', 'promote', 'sticky'];
-    if ($operation == 'edit' && in_array($fieldName, $administrative_fields, TRUE)) {
-      return AccessResult::allowedIfHasPermission($account, 'administer nodes');
+
+    // Check permission indicated.
+    if ($account->hasPermission($permission)) {
+      return AccessResult::allowed()->cachePerPermissions();
     }
-    return parent::checkFieldAccess($operation, $field_definition, $account, $items);
+
+    // If neither then we opt out of deciding.
+    // @todo we may need to return forbidden here.
+    return AccessResult::neutral();
   }
 
 }
