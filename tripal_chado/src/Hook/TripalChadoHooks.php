8d846c59c0274731347e72d605ef422413396c26
Douglas Senalik <dsenalik@wisc.edu>
2025-11-06 10:20:04 -0600

    Creates OOP hook for legacy config_schema_info_alter hook

--- tripal_chado/src/Hook/TripalChadoHooks.php
+++ tripal_chado/src/Hook/TripalChadoHooks.php
@@ -41,7 +41,7 @@ class TripalChadoHooks {
    * Implements hook_form_alter().
    */
   #[Hook('form_alter')]
-  public function alter(array &$form, FormStateInterface $form_state, $form_id) {
+  public function formAlter(array &$form, FormStateInterface $form_state, $form_id) {
 
     // If this is the field_config_edit_form and if we are adding a Chado
     // field, then we need to add a submit hook to set the base table in the
@@ -52,7 +52,13 @@ class TripalChadoHooks {
       // this is a Chado field and we should add the callback on submit so
       // that we can set the 3rd party settings on the content type.
       // Retrieve base_table from the form state, if present.
-      $base_table = $form_state->getValue(['field_storage', 'subform', 'settings', 'storage_plugin_settings', 'base_table']);
+      $base_table = $form_state->getValue([
+        'field_storage',
+        'subform',
+        'settings',
+        'storage_plugin_settings',
+        'base_table',
+      ]);
       if ($base_table) {
         // Add submit callback.
         $form['actions']['submit']['#submit'][] = [
@@ -61,6 +67,183 @@ class TripalChadoHooks {
         ];
       }
     }
+  }
+
+  /**
+   * Implements hook_config_schema_info_alter().
+   *
+   * Specifically, we are altering config schema set in the tripal module.
+   * We use this approach to ensure we are extending the existing schema
+   * which makes these changes available to extension modules defining their
+   * own yml files.
+   */
+  #[Hook('config_schema_info_alter')]
+  public function configSchemaInfoAlter(&$definitions) {
+
+    // Third party settings for entity types
+    // (always add to both collection + content type entity).
+    // -- Collection.
+    $ttetc_mapping = $definitions['tripal.tripalentitytype_collection.*']['mapping']['content_types']['sequence']['mapping']['settings']['mapping']
+      ?? [];
+    $ttetc_mapping['chado_base_table'] = [
+      'type' => 'string',
+      'label' => 'Entity Third Party Setting for Base Chado Table',
+      'nullable' => TRUE,
+    ];
+    $ttetc_mapping['bundle_type_table'] = [
+      'type' => 'string',
+      'label' => 'Entity Third Party Setting for Table Specifying Content CV Term',
+      'nullable' => TRUE,
+    ];
+    $ttetc_mapping['bundle_type_column'] = [
+      'type' => 'string',
+      'label' => 'Entity Third Party Setting for Column Specifying Content CV Term',
+      'nullable' => TRUE,
+    ];
+    $definitions['tripal.tripalentitytype_collection.*']['mapping']['content_types']['sequence']['mapping']['settings']['mapping']
+      = $ttetc_mapping;
+
+    // -- Content Type Entity.
+    $tct_mapping = $definitions['tripal.content_type.*']['mapping']['third_party_settings']['mapping']['tripal']['mapping']
+      ?? [];
+    $tct_mapping['chado_base_table'] = [
+      'type' => 'string',
+      'label' => 'Entity Third Party Setting for Base Chado Table',
+      'nullable' => TRUE,
+    ];
+    $tct_mapping['bundle_type_table'] = [
+      'type' => 'string',
+      'label' => 'Entity Third Party Setting for Table Specifying Content CV Term',
+      'nullable' => TRUE,
+    ];
+    $tct_mapping['bundle_type_column'] = [
+      'type' => 'string',
+      'label' => 'Entity Third Party Setting for Column Specifying Content CV Term',
+      'nullable' => TRUE,
+    ];
+    $definitions['tripal.content_type.*']['mapping']['third_party_settings']['mapping']['tripal']['mapping']
+      = $tct_mapping;
+
+    // ADDITIONS TO TRIPAL FIELDS.
+    $fste_mapping = $definitions['field.storage.tripal_entity.*']['mapping']['settings']['mapping']['storage_plugin_settings']['mapping']
+      ?? [];
+    $fste_mapping['base_table'] = [
+      'type' => 'string',
+      'label' => 'Field Base Chado Table',
+      'nullable' => FALSE,
+    ];
+    $fste_mapping['base_column'] = [
+      'type' => 'string',
+      'label' => 'Field Base Chado Column',
+      'nullable' => FALSE,
+    ];
+    // Specific to the schema__additional_type field.
+    // Indicates the table where the type_id column is used to differentiate
+    // the type.
+    $fste_mapping['type_table'] = [
+      'type' => 'string',
+      'label' => 'Table containing Type ID',
+      'nullable' => FALSE,
+    ];
+    // Indicates the column used as a foreign key to the cvterm table where
+    // the type is linked.
+    $fste_mapping['type_column'] = [
+      'type' => 'string',
+      'label' => 'Column Linking to CVTerm table',
+      'nullable' => FALSE,
+    ];
+    // Specific to the chado_linker__prop field.
+    // Indicates the table that contains the property columns.
+    $fste_mapping['prop_table'] = [
+      'type' => 'string',
+      'label' => 'Table containing property columns',
+      'nullable' => FALSE,
+    ];
+    // Specific to linker tables.
+    $fste_mapping['linker_table'] = [
+      'type' => 'string',
+      'label' => 'The linking table used by the current field.',
+      'nullable' => TRUE,
+    ];
+    $fste_mapping['linker_fkey_column'] = [
+      'type' => 'string',
+      'label' => 'The column in the linking table which is a foreign key pointing to the base table (e.g. for feature_synonym this would be feature_id).',
+      'nullable' => TRUE,
+    ];
+    // Specific to relationship tables.
+    $fste_mapping['subject_column'] = [
+      'type' => 'string',
+      'label' => 'The column in the relationship table which is a foreign key for the relationship subject pointing to the base table.',
+      'nullable' => TRUE,
+    ];
+    $fste_mapping['object_column'] = [
+      'type' => 'string',
+      'label' => 'The column in the relationship table which is a foreign key for the relationship object pointing to the base table.',
+      'nullable' => TRUE,
+    ];
+    $definitions['field.storage.tripal_entity.*']['mapping']['settings']['mapping']['storage_plugin_settings']['mapping']
+      = $fste_mapping;
+
+    // ADDITIONS TO TRIPAL FIELD COLLECTION -YML FIELD CREATION.
+    // Adds the Base table and column information needed for all Chado fields.
+    $ttfc_mapping = $definitions['tripal.tripalfield_collection.*']['mapping']['fields']['sequence']['mapping']['storage_settings']['mapping']['storage_plugin_settings']['mapping']
+      ?? [];
+    $ttfc_mapping['base_table'] = [
+      'type' => 'string',
+      'label' => 'Field Base Chado Table',
+      'nullable' => FALSE,
+    ];
+    $ttfc_mapping['base_column'] = [
+      'type' => 'string',
+      'label' => 'Field Base Chado Column',
+      'nullable' => FALSE,
+    ];
+    // Specific to the schema__additional_type field.
+    // Indicates the table where the type_id column is used to differentiate
+    // the type.
+    $ttfc_mapping['type_table'] = [
+      'type' => 'string',
+      'label' => 'Table containing Type ID',
+      'nullable' => FALSE,
+    ];
+    // Indicates the column used as a foreign key to the cvterm table where
+    // the type is linked.
+    $ttfc_mapping['type_column'] = [
+      'type' => 'string',
+      'label' => 'Column Linking to CVTerm table',
+      'nullable' => FALSE,
+    ];
+    // Specific to the chado_linker__prop field.
+    // Indicates the table that contains the property columns.
+    $ttfc_mapping['prop_table'] = [
+      'type' => 'string',
+      'label' => 'Table containing property columns',
+      'nullable' => FALSE,
+    ];
+    // Specific to linker tables.
+    $ttfc_mapping['linker_table'] = [
+      'type' => 'string',
+      'label' => 'The linking table used by the current field.',
+      'nullable' => TRUE,
+    ];
+    $ttfc_mapping['linker_fkey_column'] = [
+      'type' => 'string',
+      'label' => 'The column in the linking table which is a foreign key pointing to the base table (e.g. for feature_synonym this would be feature_id).',
+      'nullable' => TRUE,
+    ];
+    // Specific to relationship tables.
+    $ttfc_mapping['subject_column'] = [
+      'type' => 'string',
+      'label' => 'The column in the relationship table which is a foreign key for the relationship subject pointing to the base table.',
+      'nullable' => TRUE,
+    ];
+    $ttfc_mapping['object_column'] = [
+      'type' => 'string',
+      'label' => 'The column in the relationship table which is a foreign key for the relationship object pointing to the base table.',
+      'nullable' => TRUE,
+    ];
+    $definitions['tripal.tripalfield_collection.*']['mapping']['fields']['sequence']['mapping']['storage_settings']['mapping']['storage_plugin_settings']['mapping']
+      = $ttfc_mapping;
   }
 
 }
