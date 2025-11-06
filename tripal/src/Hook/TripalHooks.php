e3ae0d24c982d29dc2cded6ff997a0e3647cfc1b
Douglas Senalik <dsenalik@wisc.edu>
2025-11-06 13:59:32 -0600

    Creates OOP hook for legacy tripal_views_pre_view hook

--- tripal/src/Hook/TripalHooks.php
+++ tripal/src/Hook/TripalHooks.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\tripal\Hook;
 
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Drupal\Core\Hook\Attribute\Hook;
 use Drupal\Core\Render\Markup;
 use Drupal\Core\Routing\RouteMatchInterface;
@@ -348,6 +349,42 @@ class TripalHooks {
       'entity.tripal_entity.add_form',
       'entity.tripal_entity.edit_form',
     ];
+  }
+
+  /**
+   * Implements hook_views_pre_view().
+   */
+  #[Hook('views_pre_view')]
+  public function viewsPreView(ViewExecutable $view, $display_id, array &$args) {
+    // Override permission of Tripal Content Type listing
+    // (admin/content/bio_data) in order to support OR when evaluating
+    // permissions since views UI does not support this.
+    // WARNING: requires you to have NO Permissions on this view through the UI!
+    if ($view->id() == 'tripal_content_type_listing') {
+      // The user must have 1+ of the following permissions to access the
+      // Tripal Content Type listing (admin/content/bio_data).
+      $perm = [
+        'access tripal content overview',
+        'administer tripal content',
+      ];
+
+      // Check if current user has either permissions when attempting to
+      // view listing.
+      $user = \Drupal::currentUser();
+
+      $has_permission = FALSE;
+      foreach ($perm as $permission) {
+        if ($user->hasPermission($permission)) {
+          $has_permission = TRUE;
+          break;
+        }
+      }
+
+      // Deny access if user does not have necessary permission.
+      if (!$has_permission) {
+        throw new AccessDeniedHttpException();
+      }
+    }
   }
 
 }
