b867d5947e035b964c419f46e44ea515276858bf
Douglas Senalik <dsenalik@wisc.edu>
2025-11-06 14:04:23 -0600

    Creates OOP hook for view_presave hook

--- tripal/src/Hook/TripalHooks.php
+++ tripal/src/Hook/TripalHooks.php
@@ -387,4 +387,23 @@ class TripalHooks {
     }
   }
 
+  /**
+   * Implements hook_ENTITY_TYPE_presave().
+   *
+   * A new views 'class' setting was introduced in Drupal 11.2, but it has
+   * not been added to our yaml views configurations so that we maintain
+   * backward compatibility. We do the update here before Drupal does it
+   * in order to avoid a deprecation notice in unit tests.
+   * This hook will do nothing for Drupal <11.2.
+   */
+  #[Hook('view_presave')]
+  public function viewPresave(ViewEntityInterface $view): void {
+    /** @var \Drupal\views\ViewsConfigUpdater $config_updater */
+    $config_updater = \Drupal::classResolver(ViewsConfigUpdater::class);
+    if (method_exists($config_updater, 'processTableCssClassUpdate')) {
+      $config_updater->setDeprecationsEnabled(FALSE);
+      $config_updater->processTableCssClassUpdate($view);
+    }
+  }
+
 }
