f6357d10dea4567ba2562473238f655c34d894c9
Vidusha Wilpita <vwilpita@yahoo.com>
2025-12-02 15:27:17 +0000

    added the DB linker field to stockcollection

---
+++ tripal_chado/src/Plugin/Field/FieldFormatter/ChadoStockcollectionDbFormatterDefault.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Drupal\tripal_chado\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\tripal\TripalField\Attribute\TripalFieldFormatter;
+use Drupal\tripal_chado\TripalField\ChadoFormatterBase;
+
+/**
+ * Plugin implementation of the 'chado_stockcollection_db_formatter_default' field formatter.
+ */
+#[TripalFieldFormatter(
+  id: 'chado_stockcollection_db_formatter_default',
+  label: new TranslatableMarkup('Chado Stock Collection DB Formatter'),
+  description: new TranslatableMarkup('Displays the specific database.'),
+  field_types: [
+    'chado_stockcollection_db_type_default',
+  ],
+)]
+class ChadoStockcollectionDbFormatterDefault extends ChadoFormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    $settings = parent::defaultSettings();
+    $settings['token_string'] = '[name]';
+    return $settings;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+    $list = [];
+    $token_string = $this->getSetting('token_string');
+    $lookup_manager = \Drupal::service('tripal.tripal_entity.lookup');
+
+    foreach ($items as $delta => $item) {
+      $values = [
+        'entity_id' => $item->get('entity_id')->getString(),
+        'name' => $item->get('db_name')->getString(),
+      ];
+
+      // Substitute values in token string to generate displayed string.
+      $displayed_string = $token_string;
+      foreach ($values as $key => $value) {
+        $displayed_string = preg_replace("/\[$key\]/", $value, $displayed_string);
+      }
+
+      // Create a clickable link to the corresponding entity when one exists.
+      $renderable_item = $lookup_manager->getRenderableItem($displayed_string, $values['entity_id']);
+
+      $list[$delta] = $renderable_item;
+    }
+
+    // If only one element has been found, don't make into a list.
+    if (count($list) == 1) {
+      $elements = $list;
+    }
+
+    // If more than one value has been found, display all values in an
+    // unordered list.
+    // @todo add pager here.
+    elseif (count($list) > 1) {
+      $elements[0] = [
+        '#theme' => 'item_list',
+        '#list_type' => 'ul',
+        '#items' => $list,
+        '#wrapper_attributes' => ['class' => 'container'],
+      ];
+    }
+
+    return $elements;
+  }
+
+}
---
+++ tripal_chado/src/Plugin/Field/FieldWidget/ChadoStockcollectionDbWidgetDefault.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Drupal\tripal_chado\Plugin\Field\FieldWidget;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\tripal\TripalField\Attribute\TripalFieldWidget;
+use Drupal\tripal_chado\TripalField\ChadoWidgetBase;
+
+/**
+ * Plugin implementation of the 'chado_stockcollection_db_widget_default' field widget.
+ */
+#[TripalFieldWidget(
+  id: 'chado_stockcollection_db_widget_default',
+  label: new TranslatableMarkup('Chado Stock Collection DB Widget'),
+  description: new TranslatableMarkup('The stock collection database widget.'),
+  field_types: [
+    'chado_stockcollection_db_type_default',
+  ],
+)]
+class ChadoStockcollectionDbWidgetDefault extends ChadoWidgetBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+
+    // Get the field settings.
+    $field_definition = $items[$delta]->getFieldDefinition();
+    $storage_settings = $field_definition->getSetting('storage_plugin_settings');
+    $linker_fkey_column = $storage_settings['linker_fkey_column']
+      ?? $storage_settings['base_column'] ?? 'db_id';
+    $property_definitions = $items[$delta]->getFieldDefinition()->getFieldStorageDefinition()->getPropertyDefinitions();
+    $field_name = $items->getFieldDefinition()->get('field_name');
+
+    $item_vals = $items[$delta]->getValue();
+    $record_id = $item_vals['record_id'] ?? 0;
+    $linker_id = $item_vals['linker_id'] ?? 0;
+    $link = $item_vals['link'] ?? 0;
+    $db_id = $item_vals['db_id'] ?? 0;
+
+    $elements = [];
+    $elements['record_id'] = [
+      '#type' => 'value',
+      '#default_value' => $record_id,
+    ];
+    $elements['linker_id'] = [
+      '#type' => 'value',
+      '#default_value' => $linker_id,
+    ];
+    $elements['link'] = [
+      '#type' => 'value',
+      '#default_value' => $link,
+    ];
+    // Pass the foreign key name through the form for massageFormValues().
+    $elements['linker_fkey_column'] = [
+      '#type' => 'value',
+      '#default_value' => $linker_fkey_column,
+    ];
+    // Pass the field machine name through the form for massageFormValues().
+    $elements['field_name'] = [
+      '#type' => 'value',
+      '#default_value' => $field_name,
+    ];
+
+    // Create a select element specific to this content type.
+    // Create a select element specific to this content type.
+    $options = [
+      'base_table' => 'db',
+      'column_name' => 'name',
+      'type_column' => 'x',
+      'property_table' => 'db',
+    ];
+    $select_element = $this->genericSelectElement('db_id', $db_id, $options);
+    $elements[$linker_fkey_column] = $element + $select_element;
+
+    // If there are any additional columns present in the linker table,
+    // use a default of 1 which will work for type_id or rank.
+    // or pub_id. Any existing value will pass through as the default.
+    foreach ($property_definitions as $property => $definition) {
+      if (($property != 'linker_id') and preg_match('/^linker_/', $property)) {
+        $default_value = $item_vals[$property] ?? 1;
+        $elements[$property] = [
+          '#type' => 'value',
+          '#default_value' => $default_value,
+        ];
+      }
+    }
+
+    // Save some initial values to allow later handling of the "Remove" button.
+    $this->saveInitialValues($delta, $field_name, $linker_id, $form_state);
+
+    return $elements;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function massageFormValues(array $values, array $form, FormStateInterface $form_state) {
+
+    $values = $this->genericSelectMassageFormValues('db_id', $values);
+    return $this->massageLinkingFormValues('db_id', $values, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return self::defaultSelectSettings() + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    return $this->selectSettingsForm($form, $form_state) + parent::settingsForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    return $this->selectSettingsSummary() + parent::settingsSummary();
+  }
+
+}
