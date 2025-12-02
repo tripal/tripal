3bcb3ff75a6d2f62d1affe3473dc17d71cbc8a79
Lacey-Anne Sanderson <las166@mail.usask.ca>
2025-12-02 21:49:30 +0000

    Just some more easy coding standards updates while we are in the file.

--- tripal/tests/src/Functional/Entity/Subclass/TripalEntityAccessControlHandlerFake.php
+++ tripal/tests/src/Functional/Entity/Subclass/TripalEntityAccessControlHandlerFake.php
@@ -2,18 +2,25 @@
 
 namespace Drupal\Tests\tripal\Functional\Entity\Subclass;
 
-use \Drupal\tripal\Access\TripalEntityAccessControlHandler;
+use Drupal\tripal\Access\TripalEntityAccessControlHandler;
 
 /**
  * Mock class.
  */
 class TripalEntityAccessControlHandlerFake extends TripalEntityAccessControlHandler {
 
-	public function returnProtectedCheckAccess($entity, $operation, $account) {
-		return $this->checkAccess($entity, $operation, $account);
-	}
+  /**
+   * Public method to allow us to test protected checkAccess().
+   */
+  public function returnProtectedCheckAccess($entity, $operation, $account) {
+    return $this->checkAccess($entity, $operation, $account);
+  }
 
-	public function returnProtectedCheckCreateAccess($account, $entity_bundle) {
-		return $this->checkCreateAccess($account, [], $entity_bundle);
-	}
+  /**
+   * Public method to allow us to test protected checkCreateAccess().
+   */
+  public function returnProtectedCheckCreateAccess($account, $entity_bundle) {
+    return $this->checkCreateAccess($account, [], $entity_bundle);
+  }
+
 }
