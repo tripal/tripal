cc0fbf2f56d335e4c3b42a1c3edfa8856c10b578
Douglas Senalik <dsenalik@wisc.edu>
2025-12-02 13:27:27 -0600

    handle foreign keys from database properly

--- tripal_chado/src/Plugin/Field/FieldType/ChadoContactTypeDefault.php
+++ tripal_chado/src/Plugin/Field/FieldType/ChadoContactTypeDefault.php
@@ -111,9 +111,9 @@ class ChadoContactTypeDefault extends ChadoFieldItemBase {
     $extra_linker_columns = [];
     if ($linker_table != $base_table) {
       $linker_schema_def = self::getChadoTableDef($linker_table, $schema);
-      $linker_pkey_col = $linker_schema_def['primary key'];
+      $linker_pkey_col = self::getPrimaryKey($linker_table, $schema);
       // the following should be the same as $base_pkey_col @todo make sure it is
-      $linker_left_col = array_keys($linker_schema_def['foreign keys'][$base_table]['columns'])[0];
+      $linker_left_col = self::getChadoForeignKeyColumn($linker_table, $base_table, $schema);
       $linker_left_term = self::getColumnTermId($linker_table, $linker_left_col, self::$record_id_term);
       $linker_fkey_term = self::getColumnTermId($linker_table, $linker_fkey_column, self::$record_id_term);
 
