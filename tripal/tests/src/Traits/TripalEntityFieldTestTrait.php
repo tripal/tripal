57c859c7b12b86849c38198a66c3a1db4c46c620
Lacey-Anne Sanderson <las166@mail.usask.ca>
2026-03-09 20:10:22 +0000

    Don't assume settings key is present.

--- tripal/tests/src/Traits/TripalEntityFieldTestTrait.php
+++ tripal/tests/src/Traits/TripalEntityFieldTestTrait.php
@@ -376,6 +376,7 @@ trait TripalEntityFieldTestTrait {
     $values['formatter_id'] = $values['formatter_id'] ?? 'default_tripal_string_type_formatter';
     $values['widget_id'] = $values['widget_id'] ?? 'default_tripal_string_type_widget';
     $values['field_type'] = $values['field_type'] ?? 'tripal_string_type';
+    $values['settings'] = $values['settings'] ?? [];
     // -- Bundle
     if (array_key_exists('bundle', $values)) {
       $bundle = $values['bundle'];
