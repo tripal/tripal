e4cf1ff75f6da9e4b33b9bb967fcac610b939fb8
Douglas Senalik <dsenalik@wisc.edu>
2026-05-31 10:52:00 -0500

    chado views query instructs tripaldbx to prefix all tables

--- tripal_chado/src/Plugin/views/query/TripalChadoViewsQuery.php
+++ tripal_chado/src/Plugin/views/query/TripalChadoViewsQuery.php
@@ -6,20 +6,21 @@ use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\views\Attribute\ViewsQuery;
 use Drupal\views\Plugin\views\join\JoinPluginBase;
 use Drupal\views\Plugin\views\query\Sql;
+use Drupal\views\ViewExecutable;
 
 /**
  * Use a chado connection for drupal views to allow use of chado tables.
  *
  * @ViewsQuery(
- *   id = "tripal_chado_views_connection",
- *   title = @Translation("Tripal Chado SQL Connection for Views"),
- *   help = @Translation("Connection to chado for a Drupal view.")
+ *   id = "tripal_chado_views_query",
+ *   title = @Translation("Tripal Chado Views Query"),
+ *   help = @Translation("Provides a connection to chado for a Drupal view.")
  * )
  */
 #[ViewsQuery(
-  id: 'tripal_chado_views_connection',
-  title: new TranslatableMarkup('Tripal Chado SQL Connection for Views'),
-  help: new TranslatableMarkup('Connection to chado for a Drupal view.')
+  id: 'tripal_chado_views_query',
+  title: new TranslatableMarkup('Tripal Chado Views Query'),
+  help: new TranslatableMarkup('Provides a connection to chado for a Drupal view.')
 )]
 class TripalChadoViewsQuery extends Sql {
 
@@ -32,19 +33,24 @@ class TripalChadoViewsQuery extends Sql {
   }
 
   /**
+   * Executes the views query.
+   *
+   * Extended to allow referencing chado tables without 1: prefix.
+   *
    * {@inheritdoc}
    */
-  public function ensureTable($table, $relationship = NULL, ?JoinPluginBase $join = NULL) {
+  public function execute(ViewExecutable $view) {
+    // Instruct TripalDbx to also prefix tables without a 1: prefix,
+    // e.g. "{db}" will be prefixed as "chado.db", since this class is
+    // intended strictly for chado table views, and drupal does play
+    // well with our 1: prefixing notation.
+    $chado_connection = $this->getConnection();
+    $chado_connection->useTripalDbxSchemaFor('Drupal\Core\Database\Query\Query');
 
-    // Drupal gets confused by the '1:' prefixing style use for chado
-    // because a colon is not a valid alias character. Remove the
-    // colon from the returned alias.
-    $alias = parent::ensureTable($table, $relationship, $join);
-    if (isset($alias)) {
-      $alias = preg_replace('/:/', '', $alias);
-    }
+    parent::execute($view);
 
-    return $alias;
+    // Undo the override.
+    $chado_connection->useDrupalSchemaFor('Drupal\Core\Database\Query\Query');
   }
 
 }
