59f8ea8d77bf805b4f02a51104a7bfe3e58a6317
Douglas Senalik <dsenalik@wisc.edu>
2026-08-01 13:59:41 -0500

    formatter support decimal separator setting

--- tripal/src/Plugin/Field/FieldFormatter/DefaultTripalRealTypeFormatter.php
+++ tripal/src/Plugin/Field/FieldFormatter/DefaultTripalRealTypeFormatter.php
@@ -29,7 +29,9 @@ class DefaultTripalRealTypeFormatter extends TripalFormatterBase {
     $settings['field_prefix'] = '';
     $settings['field_suffix'] = '';
     $settings['thousand_separator'] = '';
-    $settings['hide_condition'] = '';
+    $settings['decimal_separator'] = '.';
+    $settings['decimal_places'] = '';
+    $settings['hide_condition'] = 'never';
     $settings['hide_value'] = '';
     return $settings;
   }
@@ -42,17 +44,32 @@ class DefaultTripalRealTypeFormatter extends TripalFormatterBase {
     $field_prefix = $this->getSetting('field_prefix');
     $field_suffix = $this->getSetting('field_suffix');
     $thousand_separator = $this->getSetting('thousand_separator');
-    $decimal_places = $this->getSetting('decimal_places') ?? 2;
-    $hide_condition = $this->getSetting('hide_condition') ?? '';
+    $decimal_separator = $this->getSetting('decimal_separator') ?? '.';
+    $decimal_places = $this->getSetting('decimal_places') ?? '';
+    $hide_condition = $this->getSetting('hide_condition') ?? 'never';
     $hide_value = $this->getSetting('hide_value') ?? '';
-
+#dpm($this->getSettings(), 'CPF1 settings');
     foreach($items as $delta => $item) {
       $value = $item->get("value")->getValue() ?? '';
       $hide = ((($hide_condition == '') and !$value)
            or (($hide_condition == 'if_value') and ($value == $hide_value)));
+#print "\nCP3 hide_condition \"$hide_condition\" hide_value ";var_dump($hide_value);
+#print "CP4 value ";var_dump($value);
+#print "CP5 hide ";var_dump($hide);
       if (!$hide) {
-        if (strlen($value) and strlen($thousand_separator)) {
-          $value = number_format(floatval($value), $decimal_places, '.', $thousand_separator);
+        if (strlen($value) && (strlen($thousand_separator) || strlen($decimal_places))) {
+          // If the decimal places setting is not specified (i.e. default),
+          // then we need a value for number_format(), so find the actual
+          // number of decimal places in the current value.
+          if (!$decimal_places) {
+            $decimal_places = 0;
+            if (preg_match('/' . preg_quote($decial_separator, '/') . '(.*)$/', $value, $matches)) {
+              $decimal_places = strlen($matches[1]);
+            }
+#dpm($decimal_places, "CPF2 DP from \"$value\""); //;;;
+          }
+#dpm($value, "CPF3 value, decimal_places=$decimal_places");
+          $value = number_format(floatval($value), $decimal_places, $decimal_separator, $thousand_separator);
         }
         $elements[$delta] = [
           "#markup" => $field_prefix . $value . $field_suffix,
@@ -71,16 +88,14 @@ class DefaultTripalRealTypeFormatter extends TripalFormatterBase {
 
     $form['field_prefix'] = [
       '#title' => $this->t('Text to display before the field value'),
-      '#description' => $this->t('Enter text here that will be displayed before the'
-                     . ' field value, or leave blank for no additional text'),
+      '#description' => $this->t('Enter text here that will be displayed before the field value, or leave blank for no additional text'),
       '#type' => 'textfield',
       '#default_value' => $this->getSetting('field_prefix'),
       '#required' => FALSE,
     ];
     $form['field_suffix'] = [
       '#title' => $this->t('Text to display after the field value'),
-      '#description' => $this->t('Enter text here that will be displayed after the'
-                     . ' field value, or leave blank for no additional text'),
+      '#description' => $this->t('Enter text here that will be displayed after the field value, or leave blank for no additional text'),
       '#type' => 'textfield',
       '#default_value' => $this->getSetting('field_suffix'),
       '#required' => FALSE,
@@ -90,6 +105,13 @@ class DefaultTripalRealTypeFormatter extends TripalFormatterBase {
       '#description' => $this->t('Character to display every three digits'),
       '#type' => 'textfield',
       '#default_value' => $this->getSetting('thousand_separator'),
+      '#required' => FALSE,
+    ];
+    $form['decimal_separator'] = [
+      '#title' => $this->t('Decimal Separator'),
+      '#description' => $this->t('Character to use for the decimal point'),
+      '#type' => 'textfield',
+      '#default_value' => $this->getSetting('decimal_separator'),
       '#required' => FALSE,
     ];
     $form['decimal_places'] = [
@@ -125,7 +147,11 @@ class DefaultTripalRealTypeFormatter extends TripalFormatterBase {
    */
   public function settingsSummary() {
     $summary = parent::settingsSummary();
-    $summary[] = $this->t('Set display format');
+    $decimal_places = $this->getSetting('decimal_places');
+    if (!strlen($decimal_places)) {
+      $decimal_places = $this->t('not specified');
+    }
+    $summary[] = $this->t('Places: @decimal_places', ['@decimal_places' => $decimal_places]);
     return $summary;
   }
 
