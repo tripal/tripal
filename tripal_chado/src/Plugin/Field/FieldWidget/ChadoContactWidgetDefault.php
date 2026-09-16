93eaf70c80911dda73bec59417c1db5609efae8d
Ethem Halil İbrahim Kadı <293729393+EthemKD@users.noreply.github.com>
2026-09-17 01:04:40 +0300

    fix: use item_vals in contact widget

--- tripal_chado/src/Plugin/Field/FieldWidget/ChadoContactWidgetDefault.php
+++ tripal_chado/src/Plugin/Field/FieldWidget/ChadoContactWidgetDefault.php
@@ -92,7 +92,7 @@ class ChadoContactWidgetDefault extends ChadoWidgetBase {
     // use the cvterm of the field as the default.
     if (array_key_exists('linker_type_id', $property_definitions)) {
 
-      if (empty($item['linker_type_id'])) {
+      if (empty($item_vals['linker_type_id'])) {
         $termIdSpace = $this->getFieldSetting('termIdSpace');
         $termAccession = $this->getFieldSetting('termAccession');
 
@@ -100,12 +100,12 @@ class ChadoContactWidgetDefault extends ChadoWidgetBase {
         $idSpace = $idSpace_manager->loadCollection($termIdSpace);
         $term = $idSpace->getTerm($termAccession);
 
-        $item['linker_type_id'] = $term->getInternalId();
+        $item_vals['linker_type_id'] = $term->getInternalId();
       }
 
       $elements['linker_type_id'] = [
         '#type' => 'value',
-        '#default_value' => $item['linker_type_id'],
+        '#default_value' => $item_vals['linker_type_id'],
       ];
     }
 
